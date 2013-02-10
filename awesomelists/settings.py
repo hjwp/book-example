ee36e1449c2f3ccb704b3e7f76ee6fb407b60a8c
Harry <hjwp2@cantab.net>
2013-02-10 17:06:09 +0000

    Rename mysite project to "awesomelists"

--- awesomelists/settings.py
+++ awesomelists/settings.py
@@ -1,4 +1,4 @@
-# Django settings for mysite project.
+# Django settings for awesomelists project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -97,10 +97,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = 'awesomelists.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'awesomelists.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
--- awesomelists/urls.py
+++ awesomelists/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^mysite/', include('mysite.foo.urls')),
+    # url(r'^$', 'awesomelists.views.home', name='home'),
+    # url(r'^awesomelists/', include('awesomelists.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
