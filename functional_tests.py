0cffd096d5767e6bc25585ac3d4404fc68cd8dce
Harry <hjwp2@cantab.net>
2013-02-25 01:31:02 +0000

    Change FT method name.

--- functional_tests.py
+++ functional_tests.py
@@ -9,8 +9,7 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_todo_list_and_get_a_url_for_it(self):
-
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
@@ -37,6 +36,8 @@ class NewVisitorTest(unittest.TestCase):
         # explanatory text to that effect.
 
         # She visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
 
 
 if __name__ == '__main__':
