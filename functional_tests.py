b57e4c217635dce12563c88268f35a097ee8451a
Harry <hjwp2@cantab.net>
2013-02-27 11:13:36 +0000

    Basic static HTML for front page

--- functional_tests.py
+++ functional_tests.py
@@ -39,10 +39,10 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(
-            row.text == '1: Buy peacock feathers'
-            for row in rows
-        ))
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
+        )
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
