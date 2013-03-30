bc6774e5d74fc6cd3bafc1018209880faec08b16
Harry <hjwp2@cantab.net>
2013-03-30 18:10:53 +0000

    Iterate thru rows in template, only save if necessary.

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+    else:
+        new_item_text = ''
+
     return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
+        'new_item_text': new_item_text,
     })
