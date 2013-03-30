f41bda71e464a2b56fd565b23fc02eddfc32abe6
Harry <hjwp2@cantab.net>
2013-03-30 18:10:53 +0000

    Iterate thru rows in template, only save if necessary.

--- lists/tests.py
+++ lists/tests.py
@@ -10,8 +10,18 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
 
 
