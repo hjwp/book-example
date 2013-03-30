fe3b68f928da8014585454be05e3cfbba4d2e921
Harry <hjwp2@cantab.net>
2013-03-30 18:04:38 +0000

    Add implicitly_wait

--- functional_tests.py
+++ functional_tests.py
@@ -5,6 +5,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
