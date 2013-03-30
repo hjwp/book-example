1f17f49b7137bc015082bd1f538661c8306d2e75
Harry <hjwp2@cantab.net>
2013-03-30 14:51:51 +0000

    tests for a new Item model

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
 
+from lists.models import Item
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -32,4 +33,25 @@ class HomePageTest(TestCase):
             {'new_item_text':  'A new list item'}
         )
         self.assertEqual(response.content.decode(), expected_html)
+
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
 
