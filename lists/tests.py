12ac91cd14e76260fd08773ab437e4facc606e61
Harry <hjwp2@cantab.net>
2013-07-07 11:04:39 +0100

    remove some blank lines from home.html and reorder some imports

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
-from django.http import HttpRequest
 
 from lists.views import home_page
 
