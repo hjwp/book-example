8ac2d44aff0560a6bf8f9edae015e3cba53511de
Harry <hjwp2@cantab.net>
2013-07-13 17:59:25 +0100

    python 3: need to compare raw bytes

--- lists/tests.py
+++ lists/tests.py
@@ -15,6 +15,6 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith('</html>'))
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
