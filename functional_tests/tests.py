acc52d9d06cfafa15ffd09a68a36746f59d083ae
Harry <hjwp2@cantab.net>
2013-07-29 09:02:22 +0100

    Start new FT for layout and styling --ch08l001--
    Conflicts:
    	functional_tests/tests.py

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -115,3 +115,17 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied, they both go back to sleep
 
+
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
