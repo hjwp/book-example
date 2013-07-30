1c80cbbe719278d1850a3a74347ac4deb8f8a085
Harry <hjwp2@cantab.net>
2013-07-30 14:02:38 +0100

    set STATIC_ROOT in settings. --ch07l018--

--- superlists/settings.py
+++ superlists/settings.py
@@ -1,4 +1,5 @@
 # Django settings for superlists project.
+from os import path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -61,7 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = path.abspath(path.join(path.dirname(__file__), '../../static'))
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
