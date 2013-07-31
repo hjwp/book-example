c7a459ff5a83954b45d7ca050bb619762c851c4c
Harry <hjwp2@cantab.net>
2013-07-31 16:02:23 +0100

    Hack Ft setupClass to be able to talk to live server. --ch08l001--

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,23 @@
 from django.contrib.staticfiles.testing import StaticLiveServerCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import sys
 
 class NewVisitorTest(StaticLiveServerCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
