fbc8e2d2f2168ed9e6475b33ce320c2b8621c677
Harry <hjwp2@cantab.net>
2013-07-31 16:03:25 +0100

    use .server_url as part of hack. --ch08l002--

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -36,7 +36,7 @@ class NewVisitorTest(LiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -81,7 +81,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Francis visits the home page.  There is no sign of Edith's
         # list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -107,7 +107,7 @@ class NewVisitorTest(LiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Edith goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_tag_name('input')
