c22054c930616f1229a700168fd8dd7c4ff868a0
Harry <hjwp2@cantab.net>
2013-08-02 12:22:20 +0100

    move database out of code tree, use PROJECT_ROOT. --ch08l003--

--- superlists/settings.py
+++ superlists/settings.py
@@ -1,5 +1,6 @@
 # Django settings for superlists project.
 from os import path
+PROJECT_ROOT = path.abspath(path.join(path.dirname(__file__), '..'))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -13,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'database.sqlite',
+        'NAME': path.join(PROJECT_ROOT, '..', 'database', 'database.sqlite'),
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -62,7 +63,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = path.abspath(path.join(path.dirname(__file__), '../../static'))
+STATIC_ROOT = path.join(PROJECT_ROOT, '../static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
