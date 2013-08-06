5019cba3c4eed87f56f33dd59aeb8a23cc64caeb
Harry <hjwp2@cantab.net>
2013-08-06 11:18:06 +0100

    New ft for item validaiton. --ch11l001--

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -146,3 +146,20 @@ class NewVisitorTest(StaticLiveServerTestCase):
             delta=10
         )
 
+
+    def test_cannot_add_empty_list_items(self):
+        # Edith goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
+
+        # The home page refreshes, and there is an error message saying
+        # that list items cannot be blank
+
+        # She tries again with some text for the item, which now works
+
+        # Perversely, she now decides to submit a second blank list item
+
+        # She receives a similar warning on the list page
+
+        # And she can correct it by filling some text in
+        self.fail('write me!')
+
