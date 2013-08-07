235af5998aa35c09410d9685841a8dcd1b4006bb
Harry <hjwp2@cantab.net>
2013-08-07 15:49:25 +0100

    Change tests dunderinit to import all fts. --ch09l007--

--- functional_tests/tests/__init__.py
+++ functional_tests/tests/__init__.py
@@ -1,1 +1,3 @@
-from .base import *
+from .test_simple_list_creation import NewVisitorTest
+from .test_layout_and_styling import LayoutAndStylingTest
+from .test_list_item_validation import ItemValidationTest
