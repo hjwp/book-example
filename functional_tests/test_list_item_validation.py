8941ad82bc62fae4dbbb178be1a893f366d51f4d
Harry <hjwp2@cantab.net>
2013-08-07 15:54:16 +0100

    Finish implementing validation FT. --ch11l012--

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -18,11 +18,22 @@ class ItemValidationTest(FunctionalTest):
         ))
 
         # She tries again with some text for the item, which now works
-        self.fail('finish this test!')
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
 
         # Perversely, she now decides to submit a second blank list item
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # She receives a similar warning on the list page
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
 
         # And she can correct it by filling some text in
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
 
