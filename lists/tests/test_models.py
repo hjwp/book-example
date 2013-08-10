e7abc23de0ecc2b2057a36a8bef192d328d3cd08
Harry <hjwp2@cantab.net>
2013-08-10 17:25:16 +0100

    unit test blank items are not allowed on model. --ch11l018--

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from lists.models import Item, List
 
@@ -30,4 +31,12 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
+
+
+
+    def test_cannot_save_empty_list_items(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text='')
+        with self.assertRaises(ValidationError):
+            item.save()
 
