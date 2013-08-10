468668449caf2f0c24224f2f1768a82d8928af98
Harry <hjwp2@cantab.net>
2013-08-10 16:32:06 +0100

    test_views now only view tests. --ch09l010--
    Conflicts:
    	lists/tests/test_views.py

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -103,34 +103,3 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'other list item 1')
         self.assertNotContains(response, 'other list item 2')
 
-
-
-class ListAndItemModelsTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        list = List()
-        list.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list
-        second_item.save()
-
-        saved_lists = List.objects.all()
-        self.assertEqual(saved_lists.count(), 1)
-        self.assertEqual(saved_lists[0], list)
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list)
-
