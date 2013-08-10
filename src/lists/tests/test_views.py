8088a707885dc8c7dc8655b3faa013213f42bea1
Harry <hjwp2@cantab.net>
2013-08-10 16:32:06 +0100

    test_views now only view tests. --ch13l017--

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -72,31 +72,3 @@ class NewItemTest(TestCase):
         )
 
         self.assertRedirects(response, f"/lists/{correct_list.id}/")
-
-
-class ListAndItemModelsTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        mylist = List()
-        mylist.save()
-        first_item = Item()
-        first_item.text = "The first (ever) list item"
-        first_item.list = mylist
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = "Item the second"
-        second_item.list = mylist
-        second_item.save()
-
-        saved_list = List.objects.get()
-        self.assertEqual(saved_list, mylist)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "The first (ever) list item")
-        self.assertEqual(first_saved_item.list, mylist)
-        self.assertEqual(second_saved_item.text, "Item the second")
-        self.assertEqual(second_saved_item.list, mylist)
