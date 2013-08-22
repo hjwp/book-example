149211b106938b494c2fc3321222ab770a1f93c8
Harry <hjwp2@cantab.net>
2013-08-22 20:46:24 +0100

    Remove old add_item url. --ch10l023--

--- lists/urls.py
+++ lists/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
     url(r'^new$', 'lists.views.new_list', name='new_list'),
 )
