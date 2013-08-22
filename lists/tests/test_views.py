3253d9c71a97f8617eef9cffe39a3bf164266ecc
Harry <hjwp2@cantab.net>
2013-08-22 19:57:00 +0100

    Fix html escaping in tests. --ch11l029--

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.utils.html import escape
 from lists.models import Item, List
 
 
@@ -30,8 +31,7 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'item_text': ''})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
-        expected_error = "You can't have an empty list item"
-        print(response.content.decode())
+        expected_error = escape("You can't have an empty list item")
         self.assertContains(response, expected_error)
 
 
