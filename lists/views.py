c5055c9f2d9d61ae4e149bdba2d80d0dee8bc547
Harry <hjwp2@cantab.net>
2013-08-22 20:45:27 +0100

    Adjust view_list to handle post, delete add_item view. --ch09l022--
    Conflicts:
    	lists/views.py
    
    Conflicts:
    	lists/views.py

--- lists/views.py
+++ lists/views.py
@@ -17,13 +17,10 @@ def new_list(request):
     return redirect('/lists/%d/' % (list_.id,))
 
 
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
-
-
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/%d/' % (list_.id,))
     return render(request, 'list.html', {'list': list_})
 
