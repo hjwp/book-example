3993b41387da8a05a760d815ec7743f0e75e665c
Harry <hjwp2@cantab.net>
2013-08-22 19:35:15 +0100

    Unit test validation of new items. --ch09l014--
    Conflicts:
    	lists/tests/test_views.py

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -42,6 +42,14 @@ class NewListTest(TestCase):
         self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
 
 
+    def test_validation_errors_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(Item.objects.all().count(), 0)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error =  "You can't have an empty list item"
+        self.assertContains(response, expected_error)
+
+
 
 class NewItemTest(TestCase):
 
