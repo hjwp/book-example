8a8b97a3e0faecd4a9f259b9bb8c54a571d72cc2
Harry <hjwp2@cantab.net>
2013-08-22 20:55:50 +0100

    add validation to view_list. ugly though. --ch09l025--

--- lists/views.py
+++ lists/views.py
@@ -19,8 +19,13 @@ def new_list(request):
 
 def view_list(request, list_id):
     list = List.objects.get(id=list_id)
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'], list=list)
-        return redirect('/lists/%d/' % (list.id,))
-    return render(request, 'list.html', {'list': list})
+    error = None
 
+    if request.method == 'POST':
+        try:
+            Item.objects.create(text=request.POST['item_text'], list=list)
+            return redirect('/lists/%d/' % (list.id,))
+        except ValidationError:
+            error = "You can't have an empty list item"
+
+    return render(request, 'list.html', {'list': list, "error": error})
