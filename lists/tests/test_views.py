bcf0dfe68f6542f50ceafd9ebd1e650094b62626
Harry <hjwp2@cantab.net>
2013-08-22 20:55:18 +0100

    New test for validation on existing lists. --ch11l034--

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -98,3 +98,15 @@ class ListViewTest(TestCase):
         )
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
 
+
+    def test_validation_errors_end_up_on_lists_page(self):
+        list_ = List.objects.create()
+        response = self.client.post(
+            '/lists/%d/' % (list_.id,),
+            data={'item_text': ''}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
