548f0a12d2ca95d70c3006663567e117a4b3f553
Harry <hjwp2@cantab.net>
2013-08-25 19:29:08 +0100

    new ft for duplicate items. --ch12l001--

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -31,3 +31,18 @@ class ItemValidationTest(FunctionalTest):
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
 
+
+    def test_cannot_add_duplicate_items(self):
+        # Edith goes to the home page and starts a new list
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Buy wellies\n')
+        self.check_for_row_in_list_table('1: Buy wellies')
+
+        # She accidentally tries to enter a duplicate item
+        self.get_item_input_box().send_keys('Buy wellies\n')
+
+        # She sees a helpful error message
+        self.check_for_row_in_list_table('1: Buy wellies')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You've already got this in your list")
+
