7e0e34936947e6aa2812030b004c6b4da82a834c
Harry <hjwp2@cantab.net>
2013-08-29 14:04:44 +0100

    Use a constant for the empty list error. --ch11l011--

--- lists/forms.py
+++ lists/forms.py
@@ -2,6 +2,8 @@ from django import forms
 
 from lists.models import Item
 
+EMPTY_ITEM_ERROR = "You can't have an empty list item"
+
 class ItemForm(forms.models.ModelForm):
 
     class Meta:
@@ -14,6 +16,6 @@ class ItemForm(forms.models.ModelForm):
             }),
         }
         error_messages = {
-            'text': {'required': "You can't have an empty list item"}
+            'text': {'required': EMPTY_ITEM_ERROR}
         }
 
