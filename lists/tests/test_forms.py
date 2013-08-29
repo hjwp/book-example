d04aec820d95a4268cfb29ba6f99fe0cf9994df3
Harry <hjwp2@cantab.net>
2013-08-29 14:05:29 +0100

    use error constant in forms test. --ch11l012--

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from lists.forms import ItemForm
+from lists.forms import EMPTY_ITEM_ERROR, ItemForm
 
 
 class ItemFormTest(TestCase):
@@ -14,7 +14,5 @@ class ItemFormTest(TestCase):
     def test_form_validation_for_blank_items(self):
         form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
-        self.assertEqual(
-            form.errors['text'],
-            ["You can't have an empty list item"]
-        )
+        self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
+
