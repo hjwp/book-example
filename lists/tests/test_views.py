402d6f608cc0edb06e62638a29c6909ed03ec6ea
Harry <hjwp2@cantab.net>
2013-08-29 14:24:08 +0100

    New unit tests that home page view uses form. --ch11l013--

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.utils.html import escape
+
+from lists.forms import ItemForm
 from lists.models import Item, List
 
 
@@ -8,6 +10,11 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 
 
