b14fbda77be6571126793feb8338bf485a8d47bd
Harry <hjwp2@cantab.net>
2013-08-29 19:47:31 +0100

    Rename input name item_text to text in views and tests. --ch11l022--

--- lists/views.py
+++ lists/views.py
@@ -10,7 +10,7 @@ def home_page(request):
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['text'], list=list_)
     try:
         item.full_clean()
         item.save()
@@ -27,8 +27,7 @@ def view_list(request, list_id):
 
     if request.method == 'POST':
         try:
-
-            item = Item(text=request.POST['item_text'], list=list_)
+            item = Item(text=request.POST['text'], list=list_)
             item.full_clean()
             item.save()
             return redirect(list_)
