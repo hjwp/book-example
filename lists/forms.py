fbbc7611b816497d96d5f53e80660bdab30c874c
Harry <hjwp2@cantab.net>
2013-08-29 14:03:21 +0100

    Add init for form to set error message. --ch11l010--

--- lists/forms.py
+++ lists/forms.py
@@ -13,3 +13,10 @@ class ItemForm(forms.models.ModelForm):
                 'class': 'form-control input-lg',
             }),
         }
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        empty_error = "You can't have an empty list item"
+        self.fields['text'].error_messages['required'] = empty_error
+
