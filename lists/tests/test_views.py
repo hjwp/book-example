c82a3a0954b8a48d5678e6232f3011557a3fe290
Harry <hjwp2@cantab.net>
2013-08-29 13:22:22 +0100

    New unit test for dupe items in views. --ch09l037--
    Conflicts:
    	lists/tests/test_views.py

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -109,9 +109,8 @@ class ListViewTest(TestCase):
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
 
 
-    def test_validation_errors_end_up_on_lists_page(self):
+    def test_empty_item_validation_errors_end_up_on_lists_page(self):
         listey = List.objects.create()
-
         response = self.client.post(
             '/lists/%d/' % (listey.id,),
             data={'item_text': ''}
@@ -120,3 +119,18 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
         expected_error =  escape("You can't have an empty list item")
         self.assertContains(response, expected_error)
+
+
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            '/lists/%d/' % (list1.id,),
+            data={'item_text': 'textey'}
+        )
+
+        self.assertEqual(Item.objects.all().count(), 1)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error =  escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+
