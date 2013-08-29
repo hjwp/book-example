1092cefb63d5ed0bfdaa5c87e51dd45a2197ce9f
Harry <hjwp2@cantab.net>
2013-08-29 13:50:20 +0100

    start writing test for form empty item validation. --ch11l008--

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -9,3 +9,9 @@ class ItemFormTest(TestCase):
         form = ItemForm()
         self.assertIn('placeholder="Enter a to-do item"', form.as_p())
         self.assertIn('class="form-control input-lg"', form.as_p())
+
+
+    def test_form_validation_for_blank_items(self):
+        form = ItemForm(data={'text': ''})
+        form.save()
+
