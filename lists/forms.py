e86078e7f5b2229f4b27418461d036860f7ec16f
Harry <hjwp2@cantab.net>
2013-08-29 13:49:11 +0100

    Form now uses modelform, custom placeholder.

--- lists/forms.py
+++ lists/forms.py
@@ -1,4 +1,14 @@
 from django import forms
 
-class ItemForm(forms.Form):
-    item_text = forms.CharField()
+from lists.models import Item
+
+class ItemForm(forms.models.ModelForm):
+
+    class Meta:
+        model = Item
+        fields = ('text',)
+        widgets = {
+            'text': forms.fields.TextInput(
+                attrs={'placeholder': 'Enter a to-do item'}
+            ),
+        }
