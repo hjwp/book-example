48ae56d5a1514b2f15580f2760f57eb07192edad
Harry <hjwp2@cantab.net>
2013-08-29 13:51:13 +0100

    better test for form empty item validation. --ch11l009--

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -13,5 +13,8 @@ class ItemFormTest(TestCase):
 
     def test_form_validation_for_blank_items(self):
         form = ItemForm(data={'text': ''})
-        form.save()
-
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors['text'],
+            ["You can't have an empty list item"]
+        )
