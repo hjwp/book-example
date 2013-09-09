79450981b160c5eae4748b051a73b26292fe0e1d
Harry <hjwp2@cantab.net>
2013-09-09 19:11:51 +0100

    use form in new_list view. --ch11l024--

--- lists/views.py
+++ lists/views.py
@@ -9,16 +9,14 @@ def home_page(request):
 
 
 def new_list(request):
-    list_ = List.objects.create()
-    item = Item(text=request.POST['text'], list=list_)
-    try:
-        item.full_clean()
-        item.save()
-    except ValidationError:
-        list_.delete()
-        error = "You can't have an empty list item"
-        return render(request, 'home.html', {"error": error})
-    return redirect(list_)
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        Item.objects.create(text=request.POST['text'], list=list_)
+        return redirect(list_)
+    else:
+        return render(request, 'home.html', {"form": form})
+
 
 
 def view_list(request, list_id):
