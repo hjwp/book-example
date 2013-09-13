66f95ee4c77ac3eea01510f07bc554eb815500cc
Harry <hjwp2@cantab.net>
2013-09-13 14:32:04 +0100

    placeholder for ExistingListItemForm --ch09l071--

--- lists/forms.py
+++ lists/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 from lists.models import Item
 
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 EMPTY_LIST_ERROR = "You can't have an empty list item"
 
 class ItemForm(forms.models.ModelForm):
@@ -19,3 +20,6 @@ class ItemForm(forms.models.ModelForm):
                 attrs={'placeholder': 'Enter a to-do item'}
             ),
         }
+
+class ExistingListItemForm(forms.Form):
+    pass
