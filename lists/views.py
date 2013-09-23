f9b34a6201d52f644cb396b219df4b5bdb778642
Harry <hjwp2@cantab.net>
2013-09-23 11:03:14 +0100

    use dict .get to handle case where there is no POST. --ch05l010--

--- lists/views.py
+++ lists/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, 'home.html', {
-        'new_item_text': request.POST['item_text'],
+        'new_item_text': request.POST.get('item_text', ''),
     })
