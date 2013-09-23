f48fc9691521fc573faf5fb87d41206e83b46790
Harry <hjwp2@cantab.net>
2013-09-23 10:50:46 +0100

    New test that we can save a POST request. --ch05l005--

--- lists/tests.py
+++ lists/tests.py
@@ -7,3 +7,7 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+
