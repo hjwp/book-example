47b22d1ec6adb794ddc0a4cfd34af0f014a5c5c9
Harry <hjwp2@cantab.net>
2013-09-23 11:02:21 +0100

    view now pushes POST param to template in all cases. --ch05l009--

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        return HttpResponse(request.POST['item_text'])
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST['item_text'],
+    })
