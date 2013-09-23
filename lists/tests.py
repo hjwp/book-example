ff46acdb09c5fc55ef2f8f1596616916b90c83fa
Harry <hjwp2@cantab.net>
2013-09-23 10:50:46 +0100

    New test that we can save a POST request. --ch05l005--

--- lists/tests.py
+++ lists/tests.py
@@ -17,3 +17,14 @@ class HomePageTest(TestCase):
         response = home_page(request)
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
+
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+
