d3d645db1576730bb9d8424e1739c79f1c82c9b2
Harry <hjwp2@cantab.net>
2013-09-23 10:58:38 +0100

    Unit test checks template rendered with new item text. --ch05l007--

--- lists/tests.py
+++ lists/tests.py
@@ -27,4 +27,9 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text':  'A new list item'}
+        )
+        self.assertEqual(response.content.decode(), expected_html)
 
