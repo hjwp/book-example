b4ceef6c5bd1b5ca870c00b1e7ff10fde04fd713
Harry <hjwp2@cantab.net>
2013-09-23 10:58:38 +0100

    Unit test checks home template after post. --ch05l007--

--- lists/tests.py
+++ lists/tests.py
@@ -10,4 +10,5 @@ class HomePageTest(TestCase):
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
 
