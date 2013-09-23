8c611fc244200f041d4343ee1a2dbb6c5edda388
Harry <hjwp2@cantab.net>
2013-09-23 10:57:47 +0100

    Deliberately silly response to POST in view. --ch05l006--

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
+    if request.method == 'POST':
+        return HttpResponse(request.POST['item_text'])
     return render(request, 'home.html')
