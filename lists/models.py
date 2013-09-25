66fa25dcaace3d2871a5a8b90906fb7a2adee4c7
Harry <hjwp2@cantab.net>
2013-09-25 18:02:33 +0100

    Implement get_absolute_url on List. --ch10l026-5--

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+
 
 class List(models.Model):
-    pass
+
+    def get_absolute_url(self):
+        return reverse('view_list', args=[self.id])
+
+
 
 class Item(models.Model):
     text = models.TextField()
     list = models.ForeignKey(List)
+
