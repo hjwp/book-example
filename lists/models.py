9c3764360c3d0f524fc94029bb9203dff3e428cc
Harry <hjwp2@cantab.net>
2013-09-25 18:02:33 +0100

    Implement get_absolute_url on List. --ch09l026-5--

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.shortcuts import resolve_url
+
 
 class List(models.Model):
-    pass
+
+    def get_absolute_url(self):
+        return resolve_url('view_list', self.id)
+
+
 
 class Item(models.Model):
     text = models.TextField()
@@ -11,3 +17,5 @@ class Item(models.Model):
     def save(self, *args, **kwargs):
         self.full_clean()
         super().save(*args, **kwargs)
+
+
