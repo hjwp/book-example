033a76c0586ea4779ddef469090f82a1245ab86e
Harry <hjwp2@cantab.net>
2013-09-25 18:02:14 +0100

    Unit test for get_absolute_url on List. --ch10l026-4--

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -40,3 +40,7 @@ class ListAndItemModelsTest(TestCase):
             item.save()
             item.full_clean()
 
+
+    def test_get_absolute_url(self):
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
