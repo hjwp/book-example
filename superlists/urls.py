ebd180d37401772365659ae12f09ced2ca8506f9
Harry <hjwp2@cantab.net>
2013-09-27 10:35:42 +0100

    update urls.py entry to use HomePage CBV. --ch31l002--

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
+from lists.views import HomePageView
 
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^$', HomePageView.as_view(), name='home'),
     url(r'^lists/', include('lists.urls')),
     # url(r'^admin/', include(admin.site.urls)),
 )
