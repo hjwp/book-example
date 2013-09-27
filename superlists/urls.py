a982580d921c5db70c7499268d858ca48f703083
Harry <hjwp2@cantab.net>
2013-09-27 10:35:42 +0100

    update urls.py entry to use HomePage CBV. --ch31l002--

--- superlists/urls.py
+++ superlists/urls.py
@@ -3,7 +3,7 @@ from lists import views as list_views
 from lists import urls as list_urls
 
 urlpatterns = [
-    url(r'^$', list_views.home_page, name='home'),
+    url(r'^$', list_views.HomePageView.as_view(), name='home'),
     url(r'^lists/', include(list_urls)),
 ]
 
