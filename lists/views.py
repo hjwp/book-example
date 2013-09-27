0644fc9eb2fbed52eefe1db6ca43996fedd4df9e
Harry <hjwp2@cantab.net>
2013-09-27 10:32:27 +0100

    Simple home page view based on FormView. --ch21l001--

--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import redirect, render
+from django.views.generic import FormView
 
 from lists.forms import ExistingListItemForm, ItemForm
 from lists.models import Item, List
 
 
-def home_page(request):
-    return render(request, 'home.html', {'form': ItemForm()})
+class HomePageView(FormView):
+    template_name = 'home.html'
+    form_class = ItemForm
 
 
 def new_list(request):
