89200518a27d0bbe6d00711a4f61e0358ed4d202
Harry <hjwp2@cantab.net>
2013-10-05 16:10:58 +0100

    First cut FT. --ch10l001--

--- functional_tests/tests/test_list_item_validation.py
+++ functional_tests/tests/test_list_item_validation.py
@@ -45,3 +45,18 @@ class ItemValidationTest(FunctionalTest):
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You've already got this in your list")
 
+
+    def test_error_messages_are_cleared_on_input(self):
+        # Edith starts a new list in a way that causes a validation error:
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.browser.find_element_by_css_selector('.error')
+        self.assertTrue(error.is_displayed())
+
+        # She starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+
+        # She is pleased to see that the error message disappears
+        error = self.browser.find_element_by_css_selector('.error')
+        self.assertFalse(error.is_displayed())
+
