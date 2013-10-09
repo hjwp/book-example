fc94076f6cbb775bec3c9cc888c769c2b4c025a6
Harry <hjwp2@cantab.net>
2013-10-09 23:01:37 +0100

    new unique url for viewing alist. --ch06l034--

--- superlists/urls.py
+++ superlists/urls.py
@@ -6,11 +6,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
-        name='view_list'
-    ),
+    url(r'^lists/(.+)/$', 'lists.views.view_list', name='view_list'),
     url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
-    # url(r'^superlists/', include('superlists.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
