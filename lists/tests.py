ab0961384b0aa0e37ed56f840a41ffdb4a53d804
Harry <hjwp2@cantab.net>
2013-10-09 23:41:40 +0100

    remove unneccesary old test that home page shows all items. --ch07l012--

--- lists/tests.py
+++ lists/tests.py
@@ -9,16 +9,6 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/')
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
-
-
     def test_can_save_a_POST_request(self):
         self.client.post('/', data={'item_text': 'A new list item'})
 
