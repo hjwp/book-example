efc2eff28ee35a5192f7cb2e0faf3401ab515143
Harry <hjwp2@cantab.net>
2013-10-09 20:54:34 +0100

    add new unit test class ListViewTest. --ch07l009--

--- lists/tests.py
+++ lists/tests.py
@@ -39,6 +39,19 @@ class HomePageTest(TestCase):
 
 
 
+class ListViewTest(TestCase):
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
+
+
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
