aa4ab2c49c3d1e1e4bd0c4fec64a811230a051c3
Harry <hjwp2@cantab.net>
2013-10-09 21:03:51 +0100

    new_list view that uses new template. --ch07l014--

--- lists/views.py
+++ lists/views.py
@@ -12,5 +12,5 @@ def home_page(request):
 
 def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
 
