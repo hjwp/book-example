e9d8a18ffd04afac3a8fc619905408c9d8d5c4cf
Harry <hjwp2@cantab.net>
2013-10-09 20:52:46 +0100

    change test to check for a different redirect. --ch07l007--

--- lists/tests.py
+++ lists/tests.py
@@ -30,7 +30,7 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
 
     def test_only_saves_items_when_necessary(self):
