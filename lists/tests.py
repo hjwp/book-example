534d430d07d520a371a8e25f28177d6b861cf667
Harry <hjwp2@cantab.net>
2013-10-09 21:02:39 +0100

    new test that we use the list.html template. --ch07l013--

--- lists/tests.py
+++ lists/tests.py
@@ -31,6 +31,11 @@ class HomePageTest(TestCase):
 
 class ListViewTest(TestCase):
 
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+
     def test_displays_all_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
