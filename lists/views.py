23f8276bac57e01c5de9d46c443ff8dbd76387b4
Harry <hjwp2@cantab.net>
2013-10-09 20:59:03 +0100

    redirect to different URL. ch06l008--

--- lists/views.py
+++ lists/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import redirect, render
 from lists.models import Item
 
 # Create your views here.
+
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
 
     items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
+
