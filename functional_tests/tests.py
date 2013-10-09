311d743df6c6c9e5ca1a5c1fe4d03bf1a9eca66f
Harry <hjwp2@cantab.net>
2013-10-09 15:22:11 +0100

    Add skip to new FT. --ch10l001-1--

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,6 +2,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import sys
+from unittest import skip
 
 class NewVisitorTest(StaticLiveServerCase):
 
@@ -130,6 +131,7 @@ class NewVisitorTest(StaticLiveServerCase):
         )
 
 
+    @skip
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
