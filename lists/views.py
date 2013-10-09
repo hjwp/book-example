52cadb0c06ac1a47a97b9a3bd908248fb76c3e65
Harry <hjwp2@cantab.net>
2013-10-09 21:41:02 +0100

    view now handles creating a new list object. --ch07l032--

--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
     return render(request, 'home.html')
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
 
 
