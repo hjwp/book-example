3cb3639fe9402ccab904d2726c6b5eba8a0b1ba0
Harry <hjwp2@cantab.net>
2013-10-09 21:24:54 +0100

    new view for new_list.

--- lists/views.py
+++ lists/views.py
@@ -4,10 +4,12 @@ from lists.models import Item
 # Create your views here.
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
 
 
 def view_list(request):
