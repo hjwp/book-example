6db8925e31b46ba3f587e9948b8bc752f42d8db1
Harry <hjwp2@cantab.net>
2013-10-09 20:28:38 +0100

    use self.live_server_url. --ch06l002--

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -23,7 +23,7 @@ class NewVisitorTest(LiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
