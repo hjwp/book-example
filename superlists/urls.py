ced5aa7f737f39b8c84a6f4c003a8cd0546cd6b3
Harry <hjwp2@cantab.net>
2013-10-09 20:59:49 +0100

    add new URL for only list in world. --ch06l011--

--- superlists/urls.py
+++ superlists/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
+        name='view_list'
+    ),
 
     # url(r'^admin/', include(admin.site.urls)),
 )
