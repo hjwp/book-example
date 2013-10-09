90ac20613042a486c52dd73c7b51729eaa26a13a
Harry <hjwp2@cantab.net>
2013-10-09 15:22:11 +0100

    Add skip to new FT. --ch09l001-1--

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,6 +2,7 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import sys
+from unittest import skip
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -127,4 +128,22 @@ class NewVisitorTest(LiveServerTestCase):
             512,
             delta=3
         )
+
+
+    @skip
+    def test_cannot_add_empty_list_items(self):
+        # Edith goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
+
+        # The home page refreshes, and there is an error message saying
+        # that list items cannot be blank
+
+        # She tries again with some text for the item, which now works
+
+        # Perversely, she now decides to submit a second blank list item
+
+        # She receives a similar warning on the list page
+
+        # And she can correct it by filling some text in
+        self.fail('write me!')
 
