0bec2103b79428b6ac26c902ed5cf16c06a765b6
Harry <hjwp2@cantab.net>
2013-10-10 00:13:08 +0100

    Move tests to new class NewListTest. --ch06l021-1--

--- lists/tests.py
+++ lists/tests.py
@@ -20,7 +20,16 @@ class HomePageTest(TestCase):
         self.assertEqual(response.content.decode(), expected_html)
 
 
-    def test_home_page_can_save_a_POST_request(self):
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+
+
+class NewListTest(TestCase):
+
+    def test_saving_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = 'A new list item'
@@ -32,7 +41,7 @@ class HomePageTest(TestCase):
         self.assertEqual(new_item.text, 'A new list item')
 
 
-    def test_home_page_redirects_after_POST(self):
+    def test_redirects_after_POST(self):
         request = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = 'A new list item'
@@ -41,12 +50,6 @@ class HomePageTest(TestCase):
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
 
 
 
