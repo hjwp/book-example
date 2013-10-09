62cc2cb558cb76ade01aa8a9f948f4fa9958dd9e
Harry <hjwp2@cantab.net>
2013-10-09 21:25:52 +0100

    new url for new list view. --ch06l022--

--- superlists/urls.py
+++ superlists/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
         name='view_list'
     ),
-
+    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
     # url(r'^admin/', include(admin.site.urls)),
 )
