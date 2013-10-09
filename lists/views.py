38987e775016395bbf859aeb35929523765c14c6
Harry <hjwp2@cantab.net>
2013-10-09 20:59:03 +0100

    redirect to different URL. --ch07l008--

--- lists/views.py
+++ lists/views.py
@@ -4,7 +4,8 @@ from lists.models import Item
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
 
     items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
+
