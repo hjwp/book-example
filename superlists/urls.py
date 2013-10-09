409f43105c0dfc9f1232fa5e58772ce335977be4
Harry <hjwp2@cantab.net>
2013-10-09 21:25:52 +0100

    new url for new list view. --ch07l022--

--- superlists/urls.py
+++ superlists/urls.py
@@ -3,6 +3,7 @@ from lists import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
+    url(r'^lists/new$', views.new_list, name='new_list'),
     url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
 ]
 
