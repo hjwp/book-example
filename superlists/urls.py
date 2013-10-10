2863ae3cedd28e9cf171d1c04665769d6f2549c5
Harry <hjwp2@cantab.net>
2013-10-10 01:59:42 +0100

    New url for add item view. --ch06l039--

--- superlists/urls.py
+++ superlists/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', views.home_page, name='home'),
     url(r'^lists/new$', views.new_list, name='new_list'),
     url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
     # url(r'^admin/', include(admin.site.urls)),
 ]
 
