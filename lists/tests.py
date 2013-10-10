c6e1f0f21e8abbe63a8809c95c5b8defc41e02da
Harry <hjwp2@cantab.net>
2013-10-10 02:03:17 +0100

    Test we pass the list object in context to list template. --ch06l041--

--- lists/tests.py
+++ lists/tests.py
@@ -81,6 +81,13 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
 
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
+        self.assertEqual(response.context['list'], correct_list)
+
+
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
         Item.objects.create(text='itemey 1', list=correct_list)
