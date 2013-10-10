d1a70a91589bc97c48999ec9005b03b04a25f5fe
Harry <hjwp2@cantab.net>
2013-10-10 01:59:42 +0100

    New url for add item view. --ch06l039--

--- superlists/urls.py
+++ superlists/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^lists/(\d+)/new_item$', 'lists.views.add_item', name='add_item'),
     url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
