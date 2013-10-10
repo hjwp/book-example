5e25fce58b487300fb78e4b096b448e1a99bb228
Harry <hjwp2@cantab.net>
2013-10-10 01:58:01 +0100

    New tests for our final view, add item. --ch07l037--

--- lists/tests.py
+++ lists/tests.py
@@ -27,6 +27,36 @@ class NewListTest(TestCase):
 
 
 
+class NewItemTest(TestCase):
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
+
+
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
