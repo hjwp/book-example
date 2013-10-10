37a90ac44392f03ff1f18b8e3b79740bdca66501
Harry <hjwp2@cantab.net>
2013-10-10 02:01:48 +0100

    New add_item view for existing lists. --ch06l040--

--- lists/views.py
+++ lists/views.py
@@ -13,6 +13,12 @@ def new_list(request):
     return redirect('/lists/%d/' % (list_.id,))
 
 
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/%d/' % (list_.id,))
+
+
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     items = Item.objects.filter(list=list_)
