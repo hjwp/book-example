08050672c941444f9dbb5a533447f1868bd11338
Harry <hjwp2@cantab.net>
2013-10-10 01:58:52 +0100

    Switch url to using back-d capture group. --ch06l038--

--- superlists/urls.py
+++ superlists/urls.py
@@ -5,7 +5,7 @@ from lists import views
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
     url(r'^lists/new$', views.new_list, name='new_list'),
-    url(r'^lists/(.+)/$', views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
     # url(r'^admin/', include(admin.site.urls)),
 ]
 
