02de9a6ae26e8010584331abaac5437b80a5b1d2
Harry <hjwp2@cantab.net>
2013-10-10 01:14:59 +0100

    adjust list view tests to look for unique url. --ch06l033-1--
    Conflicts:
    	lists/tests.py

--- lists/tests.py
+++ lists/tests.py
@@ -44,20 +44,26 @@ class NewListTest(TestCase):
 
 class ListViewTest(TestCase):
 
-    def test_displays_all_items(self):
+    def test_uses_list_template(self):
         list_ = List.objects.create()
-        Item.objects.create(text='itemey 1', list=list_)
-        Item.objects.create(text='itemey 2', list=list_)
+        response = self.client.get('/lists/%d/' % (list_.id,))
+        self.assertTemplateUsed(response, 'list.html')
 
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other list item 1', list=other_list)
+        Item.objects.create(text='other list item 2', list=other_list)
+
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
-
-
-    def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
-        self.assertTemplateUsed(response, 'list.html')
+        self.assertNotContains(response, 'other list item 1')
+        self.assertNotContains(response, 'other list item 2')
 
 
 
