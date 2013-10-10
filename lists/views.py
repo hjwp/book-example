b55c9ab8157ad8d38a56d5dda9b836ad56af1127
Harry <hjwp2@cantab.net>
2013-10-10 23:52:44 +0100

    use form properly in final view. --ch11l031--

--- lists/views.py
+++ lists/views.py
@@ -18,22 +18,13 @@ def new_list(request):
         return render(request, 'home.html', {"form": form})
 
 
-
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    error = None
-
-    if request.method == 'POST':
-        try:
-            item = Item(text=request.POST['text'], list=list_)
-            item.full_clean()
-            item.save()
-            return redirect(list_)
-        except ValidationError:
-            error = "You can't have an empty list item"
-
     form = ItemForm()
-    return render(request, 'list.html', {
-        'list': list_, "form": form, "error": error
-    })
+    if request.method == 'POST':
+        form = ItemForm(data=request.POST)
+        if form.is_valid():
+            Item.objects.create(text=request.POST['text'], list=list_)
+            return redirect(list_)
+    return render(request, 'list.html', {'list': list_, "form": form})
 
