73b4ffc5b8119ce862d034e0ee2558d973102740
Harry <hjwp2@cantab.net>
2013-10-10 01:58:52 +0100

    Switch url to using back-d capture group. --ch06l038--

--- superlists/urls.py
+++ superlists/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/(.+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
     url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
