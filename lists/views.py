6427793dc18af1a50744805c258939ab6ddf80b7
Harry <hjwp2@cantab.net>
2013-10-11 01:11:15 +0100

    Hackish implementation in view. --ch11l015--
    Conflicts:
    	lists/views.py

--- lists/views.py
+++ lists/views.py
@@ -23,6 +23,10 @@ def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     form = ItemForm(data=request.POST or None)
     if form.is_valid():
-        form.save(for_list=list_)
-        return redirect(list_)
+        try:
+            form.save(for_list=list_)
+            return redirect(list_)
+        except ValidationError:
+            form.errors.update({'text': "You've already got this in your list"})
     return render(request, 'list.html', {'list': list_, "form": form})
+
