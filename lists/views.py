a1d80a20bc891da7d4dc3dc1f7d773f7c2b55cfb
Harry <hjwp2@cantab.net>
2013-10-11 01:55:10 +0100

    use new ExistingListItemForm in view. --ch12l051--

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import redirect, render
 
-from lists.forms import ItemForm
+from lists.forms import ExistingListItemForm, ItemForm
 from lists.models import List
-
-# Create your views here.
 
 def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
@@ -21,11 +19,11 @@ def new_list(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    form = ItemForm()
+    form = ExistingListItemForm(for_list=list_)
     if request.method == 'POST':
-        form = ItemForm(data=request.POST)
+        form = ExistingListItemForm(for_list=list_, data=request.POST)
         if form.is_valid():
-            form.save(for_list=list_)
+            form.save()
             return redirect(list_)
     return render(request, 'list.html', {'list': list_, "form": form})
 
