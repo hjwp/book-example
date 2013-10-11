4750380a65d2569b1060c420db7b0cde63dfece7
Harry <hjwp2@cantab.net>
2013-10-11 01:06:11 +0100

    new view test for dupe items. --ch13l014--

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -142,3 +142,17 @@ class ListViewTest(TestCase):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
+
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            f'/lists/{list1.id}/',
+            data={'text': 'textey'}
+        )
+
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
+
