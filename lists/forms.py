7089e7317642503b32b32a495530e194388635db
Harry <hjwp2@cantab.net>
2013-10-15 00:22:52 +0100

    Use validate_unique on instance to catch dupes. --ch11l018--

--- lists/forms.py
+++ lists/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from lists.models import Item
 
@@ -29,5 +30,16 @@ class ItemForm(forms.models.ModelForm):
 
 
 class ExistingListItemForm(ItemForm):
+
     def __init__(self, for_list, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.instance.list = for_list
+
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError:
+            self._update_errors({'text': [DUPLICATE_ITEM_ERROR]})
+
+
