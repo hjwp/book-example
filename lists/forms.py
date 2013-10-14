2c41d89af6c4063ae1f03c458471a41c6dd99e95
Harry <hjwp2@cantab.net>
2013-10-15 00:03:40 +0100

    ExistingListItemForm now inherits from ItemForm. --ch09l072--

--- lists/forms.py
+++ lists/forms.py
@@ -26,6 +26,6 @@ class ItemForm(forms.models.ModelForm):
         return super().save()
 
 
-class ExistingListItemForm(forms.models.ModelForm):
+class ExistingListItemForm(ItemForm):
     def __init__(self, for_list, *args, **kwargs):
         super().__init__(*args, **kwargs)
