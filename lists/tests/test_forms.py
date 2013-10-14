da1e42c93a31199e898b9d49cc747c748817cd1b
Harry <hjwp2@cantab.net>
2013-10-14 22:57:35 +0100

    Proper unit test for form save. --ch11l034--

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from lists.forms import EMPTY_ITEM_ERROR, ItemForm
+from lists.models import Item, List
 
 
 class ItemFormTest(TestCase):
@@ -18,6 +19,10 @@ class ItemFormTest(TestCase):
 
 
     def test_form_save_handles_saving_to_a_list(self):
+        list_ = List.objects.create()
         form = ItemForm(data={'text': 'do me'})
-        new_item = form.save()
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list, list_)
 
