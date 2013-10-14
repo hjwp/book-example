e4f84cd1941e389b792f1c1d39f0fb69c7e47830
Harry <hjwp2@cantab.net>
2013-10-14 22:58:21 +0100

    Custom save method on form to save item to a list. --ch10l029--

--- lists/forms.py
+++ lists/forms.py
@@ -20,3 +20,9 @@ class ItemForm(forms.models.ModelForm):
                 'class': 'form-control input-lg',
             }),
         }
+
+
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
+
