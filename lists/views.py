777829869d16a89db56df1df81a40bff66ad694c
Harry <hjwp2@cantab.net>
2013-10-14 23:06:56 +0100

    use form.save in views. --ch10l030--

--- lists/views.py
+++ lists/views.py
@@ -13,7 +13,7 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'home.html', {"form": form})
@@ -25,6 +25,6 @@ def view_list(request, list_id):
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
     return render(request, 'list.html', {'list': list_, "form": form})
