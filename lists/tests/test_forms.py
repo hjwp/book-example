940e66d3427d7d8fd1232600773f5032334e6b43
Harry <hjwp2@cantab.net>
2013-10-15 00:33:14 +0100

    New unit test for ExistingListItemForm save. --ch12l053--

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -53,3 +53,10 @@ class ExistingListItemFormTest(TestCase):
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])
 
+
+    def test_form_save(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={'text': 'hi'})
+        new_item = form.save()
+        self.assertEqual(new_item, Item.objects.all()[0])
+
