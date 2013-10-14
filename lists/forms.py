c96e701c793410b2f3234255077f43431bd36778
Harry <hjwp2@cantab.net>
2013-10-14 22:58:21 +0100

    Custom save method on form to save item to a list. --ch11l035--

--- lists/forms.py
+++ lists/forms.py
@@ -19,3 +19,8 @@ class ItemForm(forms.models.ModelForm):
             'text': {'required': EMPTY_ITEM_ERROR}
         }
 
+
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
+
