492d68d343d5918c64ca1f4711e9d97451419fab
Harry <hjwp2@cantab.net>
2013-10-19 10:57:32 +0100

    unit tests and for navigator watch function, and start to implement. no login function yet

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
 
-    var initialize = function (navigator){
+    var initialize = function (navigator, loggedInUser, urls){
         $('#id_login').on('click', function () {
             navigator.id.request();
         });
+
+        navigator.id.watch({
+            loggedInUser: loggedInUser,
+            onlogin: onLogin,
+            onlogout: onLogout
+        });
     };
+
+    var onLogin = function () {};
+    var onLogout = function () {};
 
     window.Superlists = {
         Accounts: {
-            initialize: initialize
+            initialize: initialize,
+            onLogin: onLogin,
+            onLogout: onLogout
         }
     };
 
