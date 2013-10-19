4eec3c6287f688e41a612650837a51b4bde88153
Harry <hjwp2@cantab.net>
2013-10-19 17:54:11 +0100

    Back to sinon test, using module. next is ajax

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -1,30 +1,18 @@
 $(document).ready(function() {
 
-    var urls;
-    var csrfToken;
-
     var initialize = function (navigator, user, token, urls_){
-        urls = urls_;
-        csrfToken = token;
         $('#id_login').on('click', function () {
             navigator.id.request();
         });
 
         navigator.id.watch({
             loggedInUser: user,
-            onlogin: function (assertion) {
-                $.post(
-                    urls.login,
-                    { assertion: assertion, csrfmiddlewaretoken: token }
-                ).done( Superlists.Accounts.refreshPage );
-            },
         });
     };
 
     window.Superlists = {
         Accounts: {
-            initialize: initialize,
-            refreshPage: function () { console.log('who called me?'); }//window.location.reload(); }
+            initialize: initialize
         }
     };
 
