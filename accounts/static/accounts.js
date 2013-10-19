a7358a8941b19ecdfd4423d4552c23d3544a87d9
Harry <hjwp2@cantab.net>
2013-10-19 13:46:28 +0100

    revert first stab at testing solution

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-    var loginUrl;
 
     var initialize = function (navigator, loggedInUser, urls){
         $('#id_login').on('click', function () {
             navigator.id.request();
         });
-        loginUrl = urls.login;
 
         navigator.id.watch({
             loggedInUser: loggedInUser,
@@ -14,22 +12,13 @@ $(document).ready(function() {
         });
     };
 
-    var onLogin = function (assertion) {
-        $.post(
-            loginUrl, 
-            {assertion: assertion, csrfmiddlewaretoken: 'csrf token'}
-        ).done(
-            Superlists.Accounts.onLoginDone
-        );
-    };
-    var onLoginDone = function () {};
+    var onLogin = function () {};
     var onLogout = function () {};
 
     window.Superlists = {
         Accounts: {
             initialize: initialize,
             onLogin: onLogin,
-            onLoginDone: onLoginDone,
             onLogout: onLogout
         }
     };
