9753b4a9bda23cf0652a939bd4527bbfa5e513f5
Harry <hjwp2@cantab.net>
2013-10-20 13:46:03 +0100

    add login and logout posts + callbacks. client side done!

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -1,12 +1,24 @@
 $(document).ready(function() {
 
-    var initialize = function (navigator, user, token, urls_){
+    var initialize = function (navigator, user, token, urls) {
         $('#id_login').on('click', function () {
             navigator.id.request();
         });
 
         navigator.id.watch({
             loggedInUser: user,
+            onlogin: function (assertion) {
+                $.post(
+                    urls.login,
+                    { assertion: assertion, csrfmiddlewaretoken: token }
+                )
+                .done(function () { window.location.reload(); })
+                .fail(function () { navigator.id.logout(); } );
+            },
+            onlogout: function (assertion) {
+                $.post(urls.logout)
+                .always(function () { window.location.reload(); });
+            }
         });
     };
 
