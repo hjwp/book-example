96b67759f106cc9f49827c8a9aa915a162137ae6
Harry <hjwp2@cantab.net>
2013-10-21 11:06:22 +0100

    tweaks to login view tests

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.http import HttpRequest
 from django.test import TestCase
 from unittest.mock import patch
+
+User = get_user_model()
 
 from accounts.views import login
 
@@ -13,14 +16,16 @@ class LoginViewTest(TestCase):
     ):
         mock_authenticate.return_value = None
         self.client.post('/accounts/login', {'assertion': 'assert this'})
-        mock_authenticate.assert_called_once_with('assert this')
+        mock_authenticate.assert_called_once_with(assertion='assert this')
 
 
     @patch('accounts.views.authenticate')
-    def test_returns_OK(
+    def test_returns_OK_when_user_found(
         self, mock_authenticate
     ):
-        mock_authenticate.return_value = None
+        user = User.objects.create(email='a@b.com')
+        user.backend = '' # required for auth_login to work
+        mock_authenticate.return_value = user
         response = self.client.post('/accounts/login', {'assertion': 'a'})
         self.assertEqual(response.content.decode(), 'OK')
 
--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import login as auth_login
 from django.http import HttpResponse
 
 def login(request):
-    user = authenticate(request.POST['assertion'])
+    user = authenticate(assertion=request.POST['assertion'])
     if user:
         auth_login(request, user)
     return HttpResponse('OK')
