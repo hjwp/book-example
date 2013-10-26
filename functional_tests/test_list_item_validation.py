f879e2819ab43d5da711ac00707d4f690db24c55
Harry <hjwp2@cantab.net>
2013-10-26 16:50:29 +0100

    Use helper function in validation fts. --ch14l003--

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -62,7 +62,7 @@ class ItemValidationTest(FunctionalTest):
 
         # She sees a helpful error message
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             "You've already got this in your list"
         ))
 
@@ -77,7 +77,7 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
 
         self.wait_for(lambda: self.assertTrue(
-            self.browser.find_element_by_css_selector('.has-error').is_displayed()
+            self.get_error_element().is_displayed()
         ))
 
         # She starts typing in the input box to clear the error
@@ -85,6 +85,6 @@ class ItemValidationTest(FunctionalTest):
 
         # She is pleased to see that the error message disappears
         self.wait_for(lambda: self.assertFalse(
-            self.browser.find_element_by_css_selector('.has-error').is_displayed()
+            self.get_error_element().is_displayed()
         ))
 
