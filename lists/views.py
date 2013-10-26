7a8286bce7c484c94281b2f8f07c919b96c93b9f
Harry <hjwp2@cantab.net>
2013-10-26 14:50:47 +0100

    Use form in home_page view. --ch11l014--

--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,13 @@
 from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
+
+from lists.forms import ItemForm
 from lists.models import Item, List
 
 # Create your views here.
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'form': ItemForm()})
 
 
 def new_list(request):
