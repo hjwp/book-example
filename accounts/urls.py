f2f1db894443c6c6c5301dc40c89d40c07729e53
Harry <hjwp2@cantab.net>
2013-10-28 08:41:59 +0000

    logout url.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^login$', 'accounts.views.login', name='login'),
+    url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
 )
