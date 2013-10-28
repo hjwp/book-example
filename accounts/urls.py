4555a08d4b052e642c2b669094bbd165a42569ac
Harry <hjwp2@cantab.net>
2013-10-28 20:32:57 +0000

    SPIKE - add view,url,template for my lists to accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^login$', 'accounts.views.login', name='login'),
     url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
+    url(r'^(?P<email>.+@.+)/$', 'accounts.views.me', name='me'),
 )
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,18 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import get_user_model
+User = get_user_model()
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def login(request):
     user = authenticate(assertion=request.POST['assertion'])
     if user:
         auth_login(request, user)
     return HttpResponse('OK')
+
+
+def me(request, email):
+    user = User.objects.get(email=email)
+    return render(request, 'my_lists.html', {'owner': user})
+
