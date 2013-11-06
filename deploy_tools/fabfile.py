fa12110d3a4cd7d95b1f145774a46a8d59a358d4
Harry <hjwp2@cantab.net>
2013-11-06 19:55:29 +0000

    Deploy: Add error logging to gunicorn, switch off fake migration

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -58,9 +58,6 @@ def _update_static_files(source_folder):
 
 
 def _update_database(source_folder):
-    run('cd %s && ../virtualenv/bin/python3 manage.py syncdb' % (source_folder,))
-    # one-off fake database migration. remove me before next deploy
-    run('cd %s && ../virtualenv/bin/python3 manage.py migrate lists --fake 0001' % (
-        source_folder,
-    ))
+    run('cd %s && ../virtualenv/bin/python3 manage.py syncdb --noinput' % (source_folder,))
     run('cd %s && ../virtualenv/bin/python3 manage.py migrate' % (source_folder,))
+
