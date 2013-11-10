8fe788daba7e17aaa1e8e67ccad2d29963f777da
Harry <hjwp2@cantab.net>
2013-11-10 20:10:26 +0000

    switch to using a redirect. --ch05l028--

--- lists/views.py
+++ lists/views.py
@@ -1,13 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from lists.models import Item
 
 def home_page(request):
     if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-    else:
-        new_item_text = ''
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
 
-    return render(request, 'home.html', {
-        'new_item_text': new_item_text,
-    })
+    return render(request, 'home.html')
