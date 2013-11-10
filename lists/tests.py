4554a417aaeaa1eabaf9dcb5613fc158fa2ea5cf
Harry <hjwp2@cantab.net>
2013-11-10 20:15:01 +0000

    add test that home page displays all list items

--- lists/tests.py
+++ lists/tests.py
@@ -20,6 +20,17 @@ class HomePageTest(TestCase):
         self.assertEqual(response.content.decode(), expected_html)
 
 
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
+
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
