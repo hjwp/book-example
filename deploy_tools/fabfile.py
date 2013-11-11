a399da0b9e28bd7e499ab1f4e912ed287532fba9
Harry <hjwp2@cantab.net>
2013-11-11 07:57:31 +0000

    use syncdb --migrate in fabfile

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -58,6 +58,5 @@ def _update_static_files(source_folder):
 
 
 def _update_database(source_folder):
-    run('cd %s && ../virtualenv/bin/python3 manage.py syncdb --noinput' % (source_folder,))
-    run('cd %s && ../virtualenv/bin/python3 manage.py migrate' % (source_folder,))
+    run('cd %s && ../virtualenv/bin/python3 manage.py syncdb --migrate --noinput' % (source_folder,))
 
