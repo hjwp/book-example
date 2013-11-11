e9050e55ccab921a6ae60c6d05f9f988d668ecfb
Harry <hjwp2@cantab.net>
2013-11-11 19:07:07 +0000

    Fix fabfile logic error - need to append key import no matter what

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -40,7 +40,7 @@ def _update_settings(source_folder, site_name):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
         append(secret_key_file, "SECRET_KEY = '%s'" % (key,))
-        append(settings_path, 'from .secret_key import SECRET_KEY')
+    append(settings_path, 'from .secret_key import SECRET_KEY')
 
 def _update_virtualenv(source_folder):
     virtualenv_folder = path.join(source_folder, '../virtualenv')
