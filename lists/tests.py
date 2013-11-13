be8359114bf22604d78e75999c5835bcb0933e43
Harry <hjwp2@cantab.net>
2013-11-13 11:26:57 +0000

    unit test for view function itself

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -8,3 +10,10 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
