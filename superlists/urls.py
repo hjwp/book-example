6acd3e2a19ea89de771199bcf557e05d7e640580
Harry <hjwp2@cantab.net>
2013-11-13 11:17:30 +0000

    comment out admin urls

--- superlists/urls.py
+++ superlists/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'superlists.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 ]
+
