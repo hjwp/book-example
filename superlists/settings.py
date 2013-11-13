0e073227fc505bd1d63b297be52e4ec71bc4eb2c
Harry <hjwp2@cantab.net>
2013-11-13 17:43:34 +0000

    Adjust database path to ../database in settings. --ch08l003--

--- superlists/settings.py
+++ superlists/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
     }
 }
 
