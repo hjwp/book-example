272634c9dbf6055774ba42565779f8b30b5041d6
Harry <hjwp2@cantab.net>
2013-11-14 12:26:48 +0000

    Strip down lists/urls to just its 3. --ch07l046--

--- lists/urls.py
+++ lists/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from lists import views
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
-    path('lists/new', views.new_list, name='new_list'),
-    path('lists/<list_id>/', views.view_list, name='view_list'),
-    path('lists/<list_id>/add_item', views.add_item, name='add_item'),
+    path('new', views.new_list, name='new_list'),
+    path('<list_id>/', views.view_list, name='view_list'),
+    path('<list_id>/add_item', views.add_item, name='add_item'),
 ]
 
