558c08321eaa4828925cb8fcf03e8aed47bb4f1b
Harry <hjwp2@cantab.net>
2013-11-14 12:18:33 +0000

    cut superlists urls down to just home, include and disabled admin

--- superlists/urls.py
+++ superlists/urls.py
@@ -3,8 +3,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+    url(r'^lists/', include('lists.urls')),
     # url(r'^admin/', include(admin.site.urls)),
 )
