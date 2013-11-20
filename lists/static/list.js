0a71699444bb74cae24435ef119220ff43a8acf2
Harry <hjwp2@cantab.net>
2013-11-20 18:03:24 +0000

    Use jQuery onload and namespacing boilerplate.

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,3 +1,5 @@
-$('input').on('keypress', function () {
-    $('.has-error').hide();
+jQuery(document).ready(function ($) {
+    $('input').on('keypress', function () {
+        $('.has-error').hide();
+    });
 });
