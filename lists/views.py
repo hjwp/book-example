027bc8c64dfe697a47e68cd2d020ec60b6551d80
Harry <hjwp2@cantab.net>
2013-11-27 18:18:52 +0000

    Also refactor view_list redirect for DRY. --ch11l036-7--

--- lists/views.py
+++ lists/views.py
@@ -25,10 +25,11 @@ def view_list(request, list_id):
 
     if request.method == 'POST':
         try:
+
             item = Item(text=request.POST['item_text'], list=list_)
             item.full_clean()
             item.save()
-            return redirect(f'/lists/{list_.id}/')
+            return redirect(list_)
         except ValidationError:
             error = "You can't have an empty list item"
 
