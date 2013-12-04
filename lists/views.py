388f7ec411a6ee4f33217fcd41a0396898c14f7d
Harry <hjwp2@cantab.net>
2013-12-04 16:57:26 +0000

    view layer tests, placeholder view + url

--- lists/views.py
+++ lists/views.py
@@ -33,3 +33,8 @@ def my_lists(request, email):
     owner = User.objects.get(email=email)
     return render(request, 'my_lists.html', {'owner': owner})
 
+
+def share_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    return redirect(list_)
+
