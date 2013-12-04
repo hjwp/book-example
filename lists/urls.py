5e9f04f281c8522de7414cfec8fcb8f3864b3f20
Harry <hjwp2@cantab.net>
2013-12-04 16:57:26 +0000

    view layer tests, placeholder view + url

--- lists/urls.py
+++ lists/urls.py
@@ -4,6 +4,7 @@ from lists import views
 urlpatterns = [
     url(r'^new$', views.new_list, name='new_list'),
     url(r'^(\d+)/$', views.view_list, name='view_list'),
+    url(r'^(\d+)/share$', views.share_list, name='share_list'),
     url(r'^users/(.+)/$', views.my_lists, name='my_lists'),
 ]
 
