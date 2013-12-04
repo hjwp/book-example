9eb47fdb3fc2cff9bdab097f665a665101c90b1b
Harry <hjwp2@cantab.net>
2013-12-04 16:57:26 +0000

    view layer tests, placeholder view + url

--- lists/urls.py
+++ lists/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('',
     url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
     url(r'^new$', 'lists.views.new_list', name='new_list'),
     url(r'^users/(.+)/$', 'lists.views.my_lists', name='my_lists'),
+    url(r'^(\d+)/share$', 'lists.views.share_list', name='share_list'),
 )
