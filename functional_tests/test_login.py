218201a384854dc10d0069c2a7b66ccfe41a86c0
Harry <hjwp2@cantab.net>
2013-12-28 17:53:39 +0000

    tweak ids of login/logout links. --ch15l017--

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -28,7 +28,7 @@ class LoginTest(FunctionalTest):
         # Edith goes to the awesome superlists site
         # and notices a "Sign in" link for the first time.
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('login').click()
+        self.browser.find_element_by_id('id_login').click()
 
         # A Persona login box appears
         self.switch_to_new_window('Mozilla Persona')
@@ -44,7 +44,7 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('To-Do')
 
         # She can see that she is logged in
-        self.wait_for_element_with_id('logout')
+        self.wait_for_element_with_id('id_logout')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn('edith@mockmyid.com', navbar.text)
 
