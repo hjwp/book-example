20567084557756e04bfc1d7354e7303edc4e2dc3
Harry <hjwp2@cantab.net>
2013-12-28 17:47:54 +0000

    REVERT all of spike, but not FT. --ch15l016--
    Revert "SPIKE: add logging to stderr. "
    
    This reverts commit 6ff2f67e7d8d6bbb941383b2aadbd1427b1fc78f.
    
    Revert "SPIKE add accounts/urls.py. "
    
    This reverts commit 5205c89592e3890573732e1214399b1353232b36.
    
    Revert "SPIKE add accounts urls to global urls. "
    
    This reverts commit 249e338e63537700e76dc1cb484000aa01a395c1.
    
    Revert "SPIKE add new auth backend to settings.py. "
    
    This reverts commit 101906e53dd0d374747b62eb10a0eaf408b0a9c9.
    
    Revert "SPIKE add logout view. "
    
    This reverts commit 4cc16dd7d7dcc4649984fa3fe89d137f285c44da.
    
    Revert "SPIKE custom auth user manager thingie. "
    
    This reverts commit 09b99a92f4294b834be4fdb242cbfe223654257c.
    
    Revert "SPIKE custom user model. "
    
    This reverts commit ee1572d9d22fa8a09583709142d207c7c4de42c8.
    
    Revert "SPIKE authentication module. "
    
    This reverts commit 18d1cc0820e017487fa00341564687d234ff25d8.
    
    Revert "SPIKE accounts/views.py login view function. "
    
    This reverts commit 2cffe1f235f2367ffbd25341c39661b1eb7cf5e5.
    
    Revert "SPIKE create accounts app. "
    
    This reverts commit f798c4ce354842f1c666b93df9249314158a873b.
    
    Revert "SPIKE - client-side, div for login/logout buttons. "
    
    This reverts commit b808fdb53544a602d4bb8957edab3a62ce642844.
    
    Revert "SPIKE - client-side js stuff. "
    
    This reverts commit 7d810785f5cf827757380daea1de01e77562c030.
    
    Conflicts:
    	lists/templates/base.html
    
    Conflicts:
    	superlists/settings.py

--- accounts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('',
-    url(r'^login$', 'accounts.views.login', name='login'),
-    url(r'^logout$', 'accounts.views.logout', name='logout'),
-)
