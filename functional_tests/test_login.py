65260558e0467738baa356e013e21598addd2797
Harry <hjwp2@cantab.net>
2013-12-28 17:04:46 +0000

    switch_to_new_window helper fn. --ch15l014--

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,6 +1,21 @@
+import time
+
 from .base import FunctionalTest
 
+
 class LoginTest(FunctionalTest):
+
+    def switch_to_new_window(self, text_in_title):
+        retries = 60
+        while retries > 0:
+            for handle in self.browser.window_handles:
+                self.browser.switch_to_window(handle)
+                if text_in_title in self.browser.title:
+                    return
+            retries -= 1
+            time.sleep(0.5)
+        self.fail('could not find window')
+
 
     def test_login_with_persona(self):
         # Edith goes to the awesome superlists site
