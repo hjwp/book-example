b0ea3b94f63f003f2b96a12f0d04583afa917434
Harry <hjwp2@cantab.net>
2013-12-28 16:40:01 +0000

    SPIKE add accounts urls to global urls. --ch15l008--

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from lists import views as list_views
 from lists import urls as list_urls
+from accounts import urls as account_urls
 
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
+    url(r'^accounts/', include(account_urls)),
     # url(r'^admin/', include(admin.site.urls)),
 ]
 
