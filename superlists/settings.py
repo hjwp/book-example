efbc497bd5c90078efaa41f7bbe99510241ed38e
Harry <hjwp2@cantab.net>
2013-12-28 16:40:36 +0000

    SPIKE REINTRODUCED: add logging to stderr. --ch15l021-1--

--- superlists/settings.py
+++ superlists/settings.py
@@ -88,3 +88,20 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'superlists', 'static'),
 )
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
+
