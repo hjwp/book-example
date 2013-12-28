e6e9c4347a48566e729cd67edda6284bebb6a02d
Harry <hjwp2@cantab.net>
2013-12-28 15:43:55 +0000

    SPIKE accounts/views.py login view function. --ch15l003--

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+import sys
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.shortcuts import redirect
 
-# Create your views here.
+def login(request):
+    print('login view', file=sys.stderr)
+    # user = PersonaAuthenticationBackend().authenticate(request.POST['assertion'])
+    user = authenticate(assertion=request.POST['assertion'])
+    if user is not None:
+        auth_login(request, user)
+    return redirect('/')
+
