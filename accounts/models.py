52867b4e825f0a69270a7a61751f48b646321113
Harry <hjwp2@cantab.net>
2013-12-28 15:51:39 +0000

    SPIKE custom user model. --ch15l005--

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+class ListUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(primary_key=True)
+    USERNAME_FIELD = 'email'
+    #REQUIRED_FIELDS = ['email', 'height']
+
+    objects = ListUserManager()
+
+    @property
+    def is_staff(self):
+        return self.email == 'harry.percival@gmail.com'
+
+    @property
+    def is_active(self):
+        return True
+
