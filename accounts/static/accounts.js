f8925c4671e25ed47d040def9834fa6f9eb58718
Harry <hjwp2@cantab.net>
2013-12-29 11:43:21 +0000

    accounts.js with console.log in initialize. --ch15l028--

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -1,5 +1,9 @@
+var initialize = function (navigator) {
+    console.log(navigator);
+};
+
 window.Superlists = {
     Accounts: {
-        initialize: function () {}
+        initialize: initialize
     }
 };
