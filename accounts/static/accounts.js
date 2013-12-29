b175a8fa76dd9fcdb6ec41ffe8873a86c63bd323
Harry <hjwp2@cantab.net>
2013-12-29 13:09:24 +0000

    call navigator watch in initialize. --ch15l039--

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -4,6 +4,8 @@ var initialize = function (navigator) {
     $('#id_login').on('click', function () {
         navigator.id.request();
     });
+
+    navigator.id.watch();
 };
 
 window.Superlists = {
