76996ad3df65e969a63efecad263263685a54672
Harry <hjwp2@cantab.net>
2013-12-29 15:00:38 +0000

    Pass user to watch. --ch15l044--

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -1,11 +1,13 @@
 /*global $ */
 
-var initialize = function (navigator) {
+var initialize = function (navigator, user, token, urls) {
     $('#id_login').on('click', function () {
         navigator.id.request();
     });
 
-    navigator.id.watch({});
+    navigator.id.watch({
+        loggedInUser: user
+    });
 };
 
 window.Superlists = {
