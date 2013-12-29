a8834226c6747f17ec011bdfb7625d32c49be327
Harry <hjwp2@cantab.net>
2013-12-29 09:28:51 +0000

    Add new static files dir to settings.py. --ch15l021--

--- superlists/settings.py
+++ superlists/settings.py
@@ -102,3 +102,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'superlists', 'static'),
+)
+
