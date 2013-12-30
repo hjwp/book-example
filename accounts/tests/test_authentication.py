48091e7be8cfca6009a6a53b40c47ffd6e51362b
Harry <hjwp2@cantab.net>
2013-12-30 16:55:06 +0000

    new test should return none if request fails. --ch16l020--

--- accounts/tests/test_authentication.py
+++ accounts/tests/test_authentication.py
@@ -15,3 +15,13 @@ class AuthenticateTest(TestCase):
             PERSONA_VERIFY_URL,
             data={'assertion': 'an assertion', 'audience': DOMAIN}
         )
+
+
+    @patch('accounts.authentication.requests.post')
+    def test_returns_none_if_response_errors(self, mock_post):
+        mock_post.return_value.ok = False
+        backend = PersonaAuthenticationBackend()
+
+        user = backend.authenticate('an assertion')
+        self.assertIsNone(user)
+
