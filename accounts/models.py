2e684bb23906e2a838d1c703899001e3e707f923
Harry <hjwp2@cantab.net>
2013-12-30 18:35:22 +0000

    Email becomes primary key field. --ch16l044--

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.utils import timezone
 
 class User(models.Model):
-    email = models.EmailField()
+    email = models.EmailField(primary_key=True)
     last_login = models.DateTimeField(default=timezone.now)
     REQUIRED_FIELDS = ()
     USERNAME_FIELD = 'email'
+
