87db3cf7e56a46ae78e4fd0cbc6d5a1d1410ac63
Harry <hjwp2@cantab.net>
2013-12-30 18:40:26 +0000

    Add stupid last_login field to pristine 1-field user model. pah. --ch16l042--

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
 class User(models.Model):
     email = models.EmailField()
+    last_login = models.DateTimeField(default=timezone.now)
     REQUIRED_FIELDS = ()
     USERNAME_FIELD = 'email'
