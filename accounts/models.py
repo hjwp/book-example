c8af86b2d63270963e3df1af635ab3909cf1036f
Harry <hjwp2@cantab.net>
2013-12-30 18:29:44 +0000

    Four annoying required attributes that django wants. --ch16l027--

--- accounts/models.py
+++ accounts/models.py
@@ -3,3 +3,8 @@ from django.db import models
 class User(models.Model):
     email = models.EmailField()
 
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    is_anonymous = False
+    is_authenticated = True
+
