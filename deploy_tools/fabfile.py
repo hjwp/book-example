1557a2dbef2e2a3940f05405373eddcdc176b599
Harry <hjwp2@cantab.net>
2014-01-02 12:27:45 +0000

    stop using path.join in fabfile, it wont work from windows

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,6 +1,5 @@
 from fabric.contrib.files import append, exists, sed
 from fabric.api import env, local, run
-from os import path
 import random
 
 
@@ -9,7 +8,7 @@ SITES_FOLDER = '/home/harry/sites'
 
 def deploy():
     _create_directory_structure_if_necessary(env.host)
-    source_folder = path.join(SITES_FOLDER, env.host, 'source')
+    source_folder = '%s/%s/source' % (SITES_FOLDER, env.host)
     _get_latest_source(source_folder)
     _update_settings(source_folder, env.host)
     _update_virtualenv(source_folder)
@@ -18,13 +17,11 @@ def deploy():
 
 
 def _create_directory_structure_if_necessary(site_name):
-    base_folder = path.join(SITES_FOLDER, site_name)
-    run('mkdir -p %s' % (base_folder))
     for subfolder in ('database', 'static', 'virtualenv', 'source'):
-        run('mkdir -p %s/%s' % (base_folder, subfolder))
+        run('mkdir -p %s/%s/%s' % (SITES_FOLDER, site_name, subfolder))
 
 def _get_latest_source(source_folder):
-    if exists(path.join(source_folder, '.git')):
+    if exists(source_folder + '/.git'):
         run('cd %s && git fetch' % (source_folder,))
     else:
         run('git clone %s %s' % (REPO_URL, source_folder))
@@ -32,10 +29,10 @@ def _get_latest_source(source_folder):
     run('cd %s && git reset --hard %s' % (source_folder, current_commit))
 
 def _update_settings(source_folder, site_name):
-    settings_path = path.join(source_folder, 'superlists/settings.py')
+    settings_path = source_folder + '/superlists/settings.py'
     sed(settings_path, "DEBUG = True", "DEBUG = False")
     append(settings_path, 'ALLOWED_HOSTS = ["%s"]' % (site_name,))
-    secret_key_file = path.join(source_folder, 'superlists/secret_key.py')
+    secret_key_file = source_folder + '/superlists/secret_key.py'
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
@@ -43,8 +40,8 @@ def _update_settings(source_folder, site_name):
     append(settings_path, 'from .secret_key import SECRET_KEY')
 
 def _update_virtualenv(source_folder):
-    virtualenv_folder = path.join(source_folder, '../virtualenv')
-    if not exists(path.join(virtualenv_folder, 'bin', 'pip')):
+    virtualenv_folder = source_folder + '/../virtualenv'
+    if not exists(virtualenv_folder + '/bin/pip'):
         run('virtualenv --python=python3.3 %s' % (virtualenv_folder,))
     run('%s/bin/pip install -r %s/requirements.txt' % (
             virtualenv_folder, source_folder
