078231c2967353a91ae27d2a112efe44a4fdd6e9
Harry <hjwp2@cantab.net>
2014-01-03 17:00:10 +0000

    amend wait_for_element_with_id to show page text on fail. --ch16l052--

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -20,7 +20,10 @@ class LoginTest(FunctionalTest):
 
     def wait_for_element_with_id(self, element_id):
         WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id)
+            lambda b: b.find_element_by_id(element_id),
+            'Could not find element with id {}. Page text was:\n{}'.format(
+                element_id, self.browser.find_element_by_tag_name('body').text
+            )
         )
 
 
