49b8da01fbe908114f2447073b18f737fdd581e3
Harry <hjwp2@cantab.net>
2014-01-03 17:05:57 +0000

    add logout url based on std django one. --ch16l053--

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.contrib.auth.views import logout
 from accounts import views
 
 urlpatterns = [
     url(r'^login$', views.persona_login, name='persona_login'),
+    url(r'^logout$', logout, {'next_page': '/'}, name='logout'),
 ]
 
