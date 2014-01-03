1557bed39323751467d97d5d679d9f09a91f656f
Harry <hjwp2@cantab.net>
2014-01-03 16:59:21 +0000

    helper wait_to_be_logged_in ornot fns. --ch16l050--

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -24,6 +24,17 @@ class LoginTest(FunctionalTest):
         )
 
 
+    def wait_to_be_logged_in(self):
+        self.wait_for_element_with_id('id_logout')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn('edith@mockmyid.com', navbar.text)
+
+    def wait_to_be_logged_out(self):
+        self.wait_for_element_with_id('id_login')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn('edith@mockmyid.com', navbar.text)
+
+
     def test_login_with_persona(self):
         # Edith goes to the awesome superlists site
         # and notices a "Sign in" link for the first time.
