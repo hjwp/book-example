8c8d3026c380145dda349e7d2e660c624f15e7a6
Harry <hjwp2@cantab.net>
2014-01-17 11:40:51 +0000

    Placeholder ft method with checks for login/logout. --ch17l004--

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -22,3 +22,14 @@ class MyListsTest(FunctionalTest):
             path='/',
         ))
 
+
+    def test_logged_in_users_lists_are_saved_as_my_lists(self):
+        email = 'edith@example.com'
+        self.browser.get(self.server_url)
+        self.assert_logged_out(email)
+
+        # Edith is a logged-in user
+        self.create_pre_authenticated_session(email)
+        self.browser.get(self.server_url)
+        self.assert_logged_in(email)
+
