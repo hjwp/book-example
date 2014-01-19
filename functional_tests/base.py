7af171f5e583bd47f9d5ed0e4cb90a5bff146797
Harry <hjwp2@cantab.net>
2014-01-19 16:37:58 +0000

    move various login waits out of test_login towards base.py. --ch17l002-1--

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,6 +1,8 @@
 from django.contrib.staticfiles.testing import StaticLiveServerCase
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 import sys
+
 
 class FunctionalTest(StaticLiveServerCase):
 
@@ -35,4 +37,24 @@ class FunctionalTest(StaticLiveServerCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
+
+
+    def wait_for_element_with_id(self, element_id):
+        WebDriverWait(self.browser, timeout=30).until(
+            lambda b: b.find_element_by_id(element_id),
+            'Could not find element with id {}. Page text was {}'.format(
+                element_id, self.browser.find_element_by_tag_name('body').text
+            )
+        )
+
+
+    def wait_to_be_logged_in(self):
+        self.wait_for_element_with_id('id_logout')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn('edith@mockmyid.com', navbar.text)
+
+    def wait_to_be_logged_out(self):
+        self.wait_for_element_with_id('id_login')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn('edith@mockmyid.com', navbar.text)
 
