43268d82d6a1a75d761f19c15509557b0c44d12e
Harry <hjwp2@cantab.net>
2014-01-19 16:41:46 +0000

    move wait for login functions into base and tweak. --ch17l002-2--

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -48,13 +48,14 @@ class FunctionalTest(StaticLiveServerCase):
         )
 
 
-    def wait_to_be_logged_in(self):
+    def wait_to_be_logged_in(self, email):
         self.wait_for_element_with_id('id_logout')
         navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn('edith@mockmyid.com', navbar.text)
+        self.assertIn(email, navbar.text)
 
-    def wait_to_be_logged_out(self):
+
+    def wait_to_be_logged_out(self, email):
         self.wait_for_element_with_id('id_login')
         navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn('edith@mockmyid.com', navbar.text)
+        self.assertNotIn(email, navbar.text)
 
