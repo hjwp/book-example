b33f13b1f41e56b01cca4a826d247ca6ede4f31f
Harry <hjwp2@cantab.net>
2014-01-21 22:34:15 +0000

    add user to other view test. --ch19l013--

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -166,6 +166,7 @@ class ListViewTest(TestCase):
 class MyListsTest(TestCase):
 
     def test_my_lists_url_renders_my_lists_template(self):
+        User.objects.create(email='a@b.com')
         response = self.client.get('/lists/users/a@b.com/')
         self.assertTemplateUsed(response, 'my_lists.html')
 
