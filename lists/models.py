965ecdb34c6b800da5fbe37d5e7c71c1ecae7c77
Harry <hjwp2@cantab.net>
2014-01-21 22:52:59 +0000

    lists have a .name attribute. --ch19l025--

--- lists/models.py
+++ lists/models.py
@@ -10,6 +10,11 @@ class List(models.Model):
         return reverse('view_list', args=[self.id])
 
 
+    @property
+    def name(self):
+        return self.item_set.first().text
+
+
 
 class Item(models.Model):
     text = models.TextField(default='')
