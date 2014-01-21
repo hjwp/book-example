3c755d318da96558c2b470a5ab929eb2f089762d
Harry <hjwp2@cantab.net>
2014-01-21 21:13:53 +0000

    test for my lists url and template. --ch19l003--

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -159,3 +159,11 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
 
+
+
+class MyListsTest(TestCase):
+
+    def test_my_lists_url_renders_my_lists_template(self):
+        response = self.client.get('/lists/users/a@b.com/')
+        self.assertTemplateUsed(response, 'my_lists.html')
+
