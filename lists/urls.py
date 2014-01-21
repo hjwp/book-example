5fbe0f65a41c68067ef966ac64945c9fef7daaa3
Harry <hjwp2@cantab.net>
2014-01-21 21:14:34 +0000

    URL for my lists. --ch18l004--

--- lists/urls.py
+++ lists/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
     url(r'^new$', 'lists.views.new_list', name='new_list'),
+    url(r'^users/(.+)/$', 'lists.views.my_lists', name='my_lists'),
 )
