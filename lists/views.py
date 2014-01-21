433cd5f53c19e9bc083694b15025582fd2f00cbd
Harry <hjwp2@cantab.net>
2014-01-21 22:32:34 +0000

    view passes owner to my_lists template. --ch19l012--

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 from lists.forms import ExistingListItemForm, ItemForm
 from lists.models import List
+
 
 def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
@@ -29,5 +32,6 @@ def view_list(request, list_id):
 
 
 def my_lists(request, email):
-    return render(request, 'my_lists.html')
+    owner = User.objects.get(email=email)
+    return render(request, 'my_lists.html', {'owner': owner})
 
