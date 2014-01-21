f5186a4cae65c33618e5fb491a6718c2b2f17221
Harry <hjwp2@cantab.net>
2014-01-21 22:44:11 +0000

    Migration for list owner. --ch18l022--

---
+++ lists/migrations/0006_list_owner.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('lists', '0005_list_item_unique_together'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='list',
+            name='owner',
+            field=models.ForeignKey(blank=True, null=True, to=settings.AUTH_USER_MODEL),
+        ),
+    ]
