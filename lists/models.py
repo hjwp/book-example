c61d70363a7576d48c1fbeebcf06e30511cc7641
Harry <hjwp2@cantab.net>
2014-01-22 18:57:27 +0000

    implement shared_with at model layer

--- lists/models.py
+++ lists/models.py
@@ -5,6 +5,9 @@ from django.db import models
 
 class List(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
+    shared_with = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='shared_lists'
+    )
 
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
