ecfdffda4a4b8d8baf902c49301fd81ce908ed64
Harry <hjwp2@cantab.net>
2014-01-22 18:33:00 +0000

    ListPage page object... --ch21l006--

--- functional_tests/home_and_list_pages.py
+++ functional_tests/home_and_list_pages.py
@@ -22,3 +22,22 @@ class HomePage(object):
         list_page.wait_for_new_item_in_list(item_text, 1)
         return list_page
 
+
+
+class ListPage(object):
+
+    def __init__(self, test):
+        self.test = test
+
+    def get_list_table_rows(self):
+        return self.test.browser.find_elements_by_css_selector(
+            '#id_list_table tr'
+        )
+
+    def wait_for_new_item_in_list(self, item_text, position):
+        expected_row = '{}: {}'.format(position, item_text)
+        self.test.wait_for(lambda: self.test.assertIn(
+            expected_row,
+            [row.text for row in self.get_list_table_rows()]
+        ))
+
