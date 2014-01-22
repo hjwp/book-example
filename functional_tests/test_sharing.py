5b8a7a76f26d875cbb96f413e10fcb556119936b
Harry <hjwp2@cantab.net>
2014-01-22 18:35:03 +0000

    continue using list_page object. --ch21l008--

--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -27,8 +27,13 @@ class SharingTest(FunctionalTest):
         list_page = HomePage(self).start_new_list('Get help')
 
         # She notices a "Share this list" option
-        share_box = self.browser.find_element_by_css_selector('input[name=email]')
+        share_box = list_page.get_share_box()
         self.assertEqual(
             share_box.get_attribute('placeholder'),
             'your-friend@example.com'
         )
+
+        # She shares her list.
+        # The page updates to say that it's shared with Oniciferous:
+        list_page.share_list_with('oniciferous@example.com')
+
