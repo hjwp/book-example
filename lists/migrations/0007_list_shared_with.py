34522865280e6f6c24a529be788747ef72129f57
Harry <hjwp2@cantab.net>
2014-01-22 18:57:27 +0000

    implement shared_with at model layer

---
+++ lists/migrations/0007_list_shared_with.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('lists', '0006_list_owner'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='list',
+            name='shared_with',
+            field=models.ManyToManyField(related_name='shared_lists', to=settings.AUTH_USER_MODEL),
+            preserve_default=True,
+        ),
+    ]
