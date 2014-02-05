03659989a1c63bfa56111a798bf69ca0af819780
Harry <hjwp2@cantab.net>
2014-02-05 23:06:25 +0000

    Extract user from fab command line rather than hard-coding.

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -4,11 +4,11 @@ import random
 
 
 REPO_URL = 'https://github.com/hjwp/book-example.git'
-SITES_FOLDER = '/home/harry/sites'
 
 def deploy():
-    _create_directory_structure_if_necessary(env.host)
-    source_folder = '%s/%s/source' % (SITES_FOLDER, env.host)
+    site_folder = '/home/%s/sites/%s' % (env.user, env.host)
+    source_folder = site_folder + '/source'
+    _create_directory_structure_if_necessary(site_folder)
     _get_latest_source(source_folder)
     _update_settings(source_folder, env.host)
     _update_virtualenv(source_folder)
@@ -16,9 +16,9 @@ def deploy():
     _update_database(source_folder)
 
 
-def _create_directory_structure_if_necessary(site_name):
+def _create_directory_structure_if_necessary(site_folder):
     for subfolder in ('database', 'static', 'virtualenv', 'source'):
-        run('mkdir -p %s/%s/%s' % (SITES_FOLDER, site_name, subfolder))
+        run('mkdir -p %s/%s' % (site_folder, subfolder))
 
 def _get_latest_source(source_folder):
     if exists(source_folder + '/.git'):
