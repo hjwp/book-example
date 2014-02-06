240efac6d93b03530d62a8e193a0f2c3a3c1d818
Harry <hjwp2@cantab.net>
2014-02-06 19:40:26 +0000

    use full_clean in model unit test. --ch10l012-2--

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -40,4 +40,5 @@ class ListAndItemModelsTest(TestCase):
         item = Item(list=list_, text='')
         with self.assertRaises(ValidationError):
             item.save()
+            item.full_clean()
 
