03ef7f490cf8fd0c25c87fbfa633049e4c4e0f88
Harry <hjwp2@cantab.net>
2014-02-06 21:17:19 +0000

    additional test that invalid input isnt saved for new lists. --ch11l030-1--

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -35,6 +35,12 @@ class NewListTest(TestCase):
         self.assertContains(response, expected_error)
 
 
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
+
+
 
 class NewItemTest(TestCase):
 
