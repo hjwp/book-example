32c3fba6cc94bbc88fc06eb424043c8e7473397b
Harry <hjwp2@cantab.net>
2014-02-06 20:47:43 +0000

    Delete add_item view. --ch11l032-2--

--- lists/views.py
+++ lists/views.py
@@ -19,12 +19,6 @@ def new_list(request):
     return redirect(f'/lists/{list_.id}/')
 
 
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect(f'/lists/{list_.id}/')
-
-
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     if request.method == 'POST':
