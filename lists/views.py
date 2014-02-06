c60cf84d6d5404cd5469e4124825fa571f9ff4c3
Harry <hjwp2@cantab.net>
2014-02-06 20:47:22 +0000

    add ability to save items to view_list. --ch11l032-1--

--- lists/views.py
+++ lists/views.py
@@ -27,5 +27,8 @@ def add_item(request, list_id):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect(f'/lists/{list_.id}/')
     return render(request, 'list.html', {'list': list_})
 
