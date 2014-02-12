8929e5118f7e1698715b1715ef051fe7d271d66f
Harry <hjwp2@cantab.net>
2014-02-12 15:57:01 +0000

    remove unused imports. --ch11l037--

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,7 @@
-from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
 
 from lists.forms import ItemForm
-from lists.models import Item, List
+from lists.models import List
 
 def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
