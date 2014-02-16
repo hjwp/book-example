a7ad8086d8b8078009e6037faa947def5a7b3b20
Harry <hjwp2@cantab.net>
2014-02-16 21:26:27 +0000

    simplify item model tests. --ch13l010--

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -5,33 +5,17 @@ from lists.models import Item, List
 
 class ListAndItemModelsTest(TestCase):
 
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
+    def test_default_text(self):
+        item = Item()
+        self.assertEqual(item.text, '')
 
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
 
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
-
+    def test_item_is_related_to_list(self):
+        list_ = List.objects.create()
+        item = Item()
+        item.list = list_
+        item.save()
+        self.assertIn(item, list_.item_set.all())
 
     def test_get_absolute_url(self):
         list_ = List.objects.create()
