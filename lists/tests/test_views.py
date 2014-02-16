aacc1fb31f8ee04b7b4659ee79ae63f935e1439c
Harry <hjwp2@cantab.net>
2014-02-16 20:39:56 +0000

    update old test with correct expected html. --ch11l016-1--

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -20,7 +20,7 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
+        expected_html = render_to_string('home.html', {'form': ItemForm()})
         self.assertMultiLineEqual(response.content.decode(), expected_html)
 
 
