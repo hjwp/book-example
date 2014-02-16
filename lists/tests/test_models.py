7bab6584eab9b26f5d226aa11744cbb002d558d0
Harry <hjwp2@cantab.net>
2014-02-16 21:30:25 +0000

    move model tests out into two classes. --ch12l011--

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from lists.models import Item, List
 
 
-class ListAndItemModelsTest(TestCase):
+class ItemModelTest(TestCase):
 
     def test_default_text(self):
         item = Item()
@@ -19,17 +19,13 @@ class ListAndItemModelsTest(TestCase):
         self.assertIn(item, list_.item_set.all())
 
 
-    def test_get_absolute_url(self):
-        list_ = List.objects.create()
-        self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
-
-
     def test_cannot_save_empty_list_items(self):
         list_ = List.objects.create()
         item = Item(list=list_, text='')
         with self.assertRaises(ValidationError):
             item.save()
             item.full_clean()
+
 
 
     def test_duplicate_items_are_invalid(self):
@@ -62,4 +58,12 @@ class ListAndItemModelsTest(TestCase):
     def test_string_representation(self):
         item = Item(text='some text')
         self.assertEqual(str(item), 'some text')
+
+
+
+class ListModelTest(TestCase):
+
+    def test_get_absolute_url(self):
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
 
