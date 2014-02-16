48daae37f28e7d37765c82b7876e59de5975a603
Harry <hjwp2@cantab.net>
2014-02-16 22:32:08 +0000

    revert experiment. --ch13l013--

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -31,8 +31,7 @@ class ItemModelTest(TestCase):
         Item.objects.create(list=list_, text='bla')
         with self.assertRaises(ValidationError):
             item = Item(list=list_, text='bla')
-            # item.full_clean()
-            item.save()
+            item.full_clean()
 
 
     def test_CAN_save_same_item_to_different_lists(self):
