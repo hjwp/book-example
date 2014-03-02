32eb689b68689dccf6af2c3b12eac9184551ae61
Harry <hjwp2@cantab.net>
2014-03-02 10:08:09 +0000

    two more tests for whether user gets logged in. --ch16l012--

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, SESSION_KEY
 from django.test import TestCase
 from unittest.mock import patch
 User = get_user_model()
@@ -24,4 +24,24 @@ class LoginViewTest(TestCase):
         mock_authenticate.return_value = user
         response = self.client.post('/accounts/login', {'assertion': 'a'})
         self.assertEqual(response.content.decode(), 'OK')
+
+
+    @patch('accounts.views.authenticate')
+    def test_gets_logged_in_session_if_authenticate_returns_a_user(
+        self, mock_authenticate
+    ):
+        user = User.objects.create(email='a@b.com')
+        user.backend = ''  # required for auth_login to work
+        mock_authenticate.return_value = user
+        self.client.post('/accounts/login', {'assertion': 'a'})
+        self.assertEqual(self.client.session[SESSION_KEY], user.pk)
+
+
+    @patch('accounts.views.authenticate')
+    def test_does_not_get_logged_in_if_authenticate_returns_None(
+        self, mock_authenticate
+    ):
+        mock_authenticate.return_value = None
+        self.client.post('/accounts/login', {'assertion': 'a'})
+        self.assertNotIn(SESSION_KEY, self.client.session)
 
