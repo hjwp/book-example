2f228118672af820c93076e866491826e31e413b
Harry <hjwp2@cantab.net>
2014-03-02 23:42:27 +0000

    use DOMAIN from settings in actual auth. --ch17l013--

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -1,9 +1,9 @@
 import requests
+from django.conf import settings
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
 PERSONA_VERIFY_URL = 'https://verifier.login.persona.org/verify'
-DOMAIN = 'localhost'
 
 
 class PersonaAuthenticationBackend(object):
@@ -11,7 +11,7 @@ class PersonaAuthenticationBackend(object):
     def authenticate(self, assertion):
         response = requests.post(
             PERSONA_VERIFY_URL,
-            data={'assertion': assertion, 'audience': DOMAIN}
+            data={'assertion': assertion, 'audience': settings.DOMAIN}
         )
         if response.ok and response.json()['status'] == 'okay':
             email = response.json()['email']
