d645dc5bbd5aa4bf627e29ffef27ea775bbc94e4
Harry <hjwp2@cantab.net>
2014-03-02 10:04:36 +0000

    login view now calls authenticate correctly. --ch16l010--

--- accounts/views.py
+++ accounts/views.py
@@ -2,5 +2,6 @@ from django.contrib.auth import authenticate
 from django.http import HttpResponse
 
 def persona_login(request):
+    authenticate(assertion=request.POST['assertion'])
     return HttpResponse()
 
