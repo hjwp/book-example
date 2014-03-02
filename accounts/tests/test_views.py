626f0c6b6952baee4642ba3bfd06fcc1333abf14
Harry <hjwp2@cantab.net>
2014-03-02 10:06:22 +0000

    new test returns OK when user found. --ch16l011--

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from unittest.mock import patch
+User = get_user_model()
 
 
 class LoginViewTest(TestCase):
@@ -11,4 +13,15 @@ class LoginViewTest(TestCase):
         mock_authenticate.return_value = None
         self.client.post('/accounts/login', {'assertion': 'assert this'})
         mock_authenticate.assert_called_once_with(assertion='assert this')
+
+
+    @patch('accounts.views.authenticate')
+    def test_returns_OK_when_user_found(
+        self, mock_authenticate
+    ):
+        user = User.objects.create(email='a@b.com')
+        user.backend = ''  # required for auth_login to work
+        mock_authenticate.return_value = user
+        response = self.client.post('/accounts/login', {'assertion': 'a'})
+        self.assertEqual(response.content.decode(), 'OK')
 
