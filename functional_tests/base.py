e3dc48d2b0f61a810c776deaa0caf8aadfff5ba9
Harry <hjwp2@cantab.net>
2014-03-02 21:41:47 +0000

    base FT runner now stores server hostname if running against server. --ch17l017--

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,6 +1,7 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 import sys
+from .server_tools import reset_database
 
 
 class FunctionalTest(StaticLiveServerTestCase):
@@ -9,20 +10,25 @@ class FunctionalTest(StaticLiveServerTestCase):
     def setUpClass(cls):
         for arg in sys.argv:
             if 'liveserver' in arg:
-                cls.server_url = 'http://' + arg.split('=')[1]
+                cls.server_host = arg.split('=')[1]
+                cls.server_url = 'http://' + cls.server_host
                 cls.against_staging = True
                 return
         super().setUpClass()
         cls.against_staging = False
         cls.server_url = cls.live_server_url
 
+
     @classmethod
     def tearDownClass(cls):
-        if cls.server_url == cls.live_server_url:
+        if not cls.against_staging:
             super().tearDownClass()
 
 
     def setUp(self):
+        if self.against_staging:
+            reset_database(self.server_host)
+
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
