6f159a11f7ea22f32b068697c428b4b0bc74e2b1
Harry <hjwp2@cantab.net>
2014-03-02 21:44:13 +0000

    create_session managemnet command. --ch17l015--

---
+++ functional_tests/management/commands/create_session.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
+User = get_user_model()
+from django.contrib.sessions.backends.db import SessionStore
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('email')
+
+    def handle(self, *args, **options):
+        session_key = create_pre_authenticated_session(options['email'])
+        self.stdout.write(session_key)
+
+
+def create_pre_authenticated_session(email):
+    user = User.objects.create(email=email)
+    session = SessionStore()
+    session[SESSION_KEY] = user.pk
+    session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
+    session.save()
+    return session.session_key
+
