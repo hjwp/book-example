3d66282dd9238a736d0ce70fa7e94da43d1eba92
Harry <hjwp2@cantab.net>
2014-03-03 01:12:08 +0000

    add handlers for both apps to LOGGING. --ch17l022--

--- superlists/settings.py
+++ superlists/settings.py
@@ -128,6 +128,12 @@ LOGGING = {
         'django': {
             'handlers': ['console'],
         },
+        'accounts': {
+            'handlers': ['console'],
+        },
+        'lists': {
+            'handlers': ['console'],
+        },
     },
     'root': {'level': 'INFO'},
 }
