56501487ef3cb13b3006432de3b76a740dc7f8b2
Harry <hjwp2@cantab.net>
2014-03-03 01:12:34 +0000

    unit test logging in auth. --ch17l023--

--- accounts/tests/test_authentication.py
+++ accounts/tests/test_authentication.py
@@ -1,3 +1,4 @@
+import logging
 from unittest.mock import patch
 from django.conf import settings
 from django.contrib.auth import get_user_model
@@ -52,6 +53,22 @@ class AuthenticateTest(TestCase):
         found_user = self.backend.authenticate('an assertion')
         new_user = User.objects.get(email='a@b.com')
         self.assertEqual(found_user, new_user)
+
+
+    def test_logs_non_okay_responses_from_persona(self, mock_post):
+        response_json = {
+            'status': 'not okay', 'reason': 'eg, audience mismatch'
+        }
+        mock_post.return_value.ok = True
+        mock_post.return_value.json.return_value = response_json
+
+        logger = logging.getLogger('accounts.authentication')
+        with patch.object(logger, 'warning') as mock_log_warning:
+            self.backend.authenticate('an assertion')
+
+        mock_log_warning.assert_called_once_with(
+            'Persona says no. Json was: {}'.format(response_json)
+        )
 
 
 
