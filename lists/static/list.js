c98aef01c12d14d8716b617425dc490e159313c0
Harry <hjwp2@cantab.net>
2014-03-09 12:36:08 +0000

    Revert temporarily break js test. --ch21l020--
    This reverts commit 7eb6cee8529de10af282072a6ae33bc933446440.

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,6 +1,6 @@
 window.Superlists = {};
 window.Superlists.initialize = function () {
   $('input[name="text"]').on('keypress', function () {
-    // $('.has-error').hide();
+    $('.has-error').hide();
   });
 };
