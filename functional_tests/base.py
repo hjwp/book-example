add357643e3daa4777307589ae8b9e71f49d8ec3
Harry <hjwp2@cantab.net>
2014-03-09 11:26:07 +0000

    wait_for defined in base.py. --ch20l013--

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,8 +1,10 @@
 import os
 import sys
+import time
 from datetime import datetime
 from django.contrib.staticfiles.testing import StaticLiveServerCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.support.ui import WebDriverWait
 
 from .server_tools import reset_database
@@ -73,6 +75,17 @@ class FunctionalTest(StaticLiveServerCase):
         print('dumping page HTML to', filename)
         with open(filename, 'w') as f:
             f.write(self.browser.page_source)
+
+
+    def wait_for(self, function_with_assertion, timeout=DEFAULT_WAIT):
+        start_time = time.time()
+        while time.time() - start_time < timeout:
+            try:
+                return function_with_assertion()
+            except (AssertionError, WebDriverException):
+                time.sleep(0.1)
+        # one more try, which will raise any errors if they are outstanding
+        return function_with_assertion()
 
 
     def get_item_input_box(self):
