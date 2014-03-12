576a6ed23d725c26365e4fa5ae52efbb576cfa37
Harry <hjwp2@cantab.net>
2014-03-12 21:05:47 +0000

    switch new list view in urls. --ch31l004--

--- lists/urls.py
+++ lists/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from lists import views
 
 urlpatterns = [
-    url(r'^new$', views.new_list, name='new_list'),
+    url(r'^new$', views.NewListView.as_view(), name='new_list'),
     url(r'^(\d+)/$', views.view_list, name='view_list'),
 ]
 
