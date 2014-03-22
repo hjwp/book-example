6cc9b70f6b5dcda7a25117cf7b7cce490b1994ee
Harry <hjwp2@cantab.net>
2014-03-22 14:05:25 +0000

    Attempt saving list owner in view. --ch19l015--

--- lists/views.py
+++ lists/views.py
@@ -13,7 +13,9 @@ def home_page(request):
 def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
-        list_ = List.objects.create()
+        list_ = List()
+        list_.owner = request.user
+        list_.save()
         form.save(for_list=list_)
         return redirect(list_)
     else:
