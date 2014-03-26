a4c45d1f2ac6f33766bf2fbe7a0d4d7d12cde7a7
Harry <hjwp2@cantab.net>
2014-03-26 21:48:30 +0000

    placeholder test that form save returns the new list. --ch20l038-1--

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -60,6 +60,15 @@ class NewListFormTest(unittest.TestCase):
         )
 
 
+    @patch('lists.forms.List.create_new')
+    def test_save_returns_new_list_object(self, mock_List_create_new):
+        user = Mock(is_authenticated=True)
+        form = NewListForm(data={'text': 'new item text'})
+        form.is_valid()
+        response = form.save(owner=user)
+        self.assertEqual(response, mock_List_create_new.return_value)
+
+
 
 class ExistingListItemFormTest(TestCase):
 
