3564ce2bf2e4244edce305f6383d64d9bd6539bb
Harry <hjwp2@cantab.net>
2014-03-26 20:14:37 +0000

    fix old view so that it conditionally saves user. --ch19l031--

--- lists/views.py
+++ lists/views.py
@@ -14,7 +14,8 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List()
-        list_.owner = request.user
+        if request.user.is_authenticated():
+            list_.owner = request.user
         list_.save()
         form.save(for_list=list_)
         return redirect(list_)
