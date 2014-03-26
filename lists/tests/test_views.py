44ef2a92d9dd59967546b3cc1b47f153228cbea2
Harry <hjwp2@cantab.net>
2014-03-26 18:50:29 +0000

    test that we render home template if form invalid. --ch19l017--

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -115,6 +115,21 @@ class NewListViewUnitTest(unittest.TestCase):
         mock_redirect.assert_called_once_with(mock_form.save.return_value)
 
 
+    @patch('lists.views.render')
+    def test_renders_home_template_with_form_if_form_invalid(
+        self, mock_render, mockNewListForm
+    ):
+        mock_form = mockNewListForm.return_value
+        mock_form.is_valid.return_value = False
+
+        response = new_list2(self.request)
+
+        self.assertEqual(response, mock_render.return_value)
+        mock_render.assert_called_once_with(
+            self.request, 'home.html', {'form': mock_form}
+        )
+
+
 
 class ListViewTest(TestCase):
 
