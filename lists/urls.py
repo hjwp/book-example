ae59b7226eb59806b5643038e570ac21baedce0b
Harry <hjwp2@cantab.net>
2014-03-26 21:42:33 +0000

    switch out new list function in urls. --ch20l032--

--- lists/urls.py
+++ lists/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from lists import views
 
 urlpatterns = [
-    url(r'^new$', views.new_list, name='new_list'),
+    url(r'^new$', views.new_list2, name='new_list'),
     url(r'^(\d+)/$', views.view_list, name='view_list'),
     url(r'^users/(.+)/$', views.my_lists, name='my_lists'),
 ]
