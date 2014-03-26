ded29140734e7a2ec3a5857b79d06ab6e998fad4
Harry <hjwp2@cantab.net>
2014-03-26 18:42:04 +0000

    import NewListForm in views.py. --ch19l011--

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import get_user_model
 
-from lists.forms import ExistingListItemForm, ItemForm
+from lists.forms import ExistingListItemForm, ItemForm, NewListForm
 from lists.models import List
 User = get_user_model()
 
