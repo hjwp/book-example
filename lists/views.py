a997e0b4d831f585065da60017d0d60ad3be9a2a
Harry <hjwp2@cantab.net>
2014-03-27 09:49:59 +0000

    delete old new_list from views.py. --ch19l047--

--- lists/views.py
+++ lists/views.py
@@ -11,19 +11,6 @@ def home_page(request):
 
 
 def new_list(request):
-    form = ItemForm(data=request.POST)
-    if form.is_valid():
-        list_ = List()
-        if request.user.is_authenticated():
-            list_.owner = request.user
-        list_.save()
-        form.save(for_list=list_)
-        return redirect(list_)
-    else:
-        return render(request, 'home.html', {"form": form})
-
-
-def new_list2(request):
     form = NewListForm(data=request.POST)
     if form.is_valid():
         list_ = form.save(owner=request.user)
