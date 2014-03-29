f2c1fd4626625066cbcd9b747e4f2afa9341ede2
Harry <hjwp2@cantab.net>
2014-03-29 17:03:40 +0000

    switch to using StaticLiveServerTestCase. --ch07l008--

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
