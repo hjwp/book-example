83ae7920e704a3fee3da6a7b1446b72caa5794ae
Harry <hjwp2@cantab.net>
2014-04-01 01:21:56 +0100

    Migration for user model.

---
+++ accounts/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('email', models.EmailField(serialize=False, primary_key=True, max_length=75)),
+                ('last_login', models.DateTimeField(default=django.utils.timezone.now)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
