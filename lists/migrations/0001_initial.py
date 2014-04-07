873eff31c77ec6aa9a7c2947a7165e395271f61a
Harry <hjwp2@cantab.net>
2014-04-07 10:29:02 +0100

    fixup key ordering in migration 1

--- lists/migrations/0001_initial.py
+++ lists/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Item',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
             ],
             options={
             },
