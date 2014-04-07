b3aa4d62b1d3bd69373d88d537c90a18768000f7
Harry <hjwp2@cantab.net>
2014-04-07 10:12:37 +0100

    add textfield with default and migration for it.

---
+++ lists/migrations/0002_item_text.py
@@ -0,0 +1,18 @@
+# encoding: utf8
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lists', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='item',
+            name='text',
+            field=models.TextField(default=''),
+            preserve_default=True,
+        ),
+    ]
