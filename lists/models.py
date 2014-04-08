84b0035bfbcccee64dc5369249b4561e72fe982a
Harry <hjwp2@cantab.net>
2014-04-08 15:59:04 +0100

    now using a foreign key to List object, default=None

--- lists/models.py
+++ lists/models.py
@@ -6,5 +6,5 @@ class List(models.Model):
 
 class Item(models.Model):
     text = models.TextField(default='')
-    list = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
 
