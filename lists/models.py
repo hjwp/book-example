28cda934968c90012a43747814f56fb493e753d8
Harry <hjwp2@cantab.net>
2014-04-19 15:40:07 +0100

    placeholder for List.create_new. --ch20l024--

--- lists/models.py
+++ lists/models.py
@@ -7,6 +7,9 @@ class List(models.Model):
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
 
+    def create_new():
+        pass
+
 
 
 class Item(models.Model):
