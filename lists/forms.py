847798c97bb04ea2055e241c081b4b9fc21d25e7
Harry <hjwp2@cantab.net>
2014-04-19 17:00:29 +0100

    return list from form save. --ch19l039-1--

--- lists/forms.py
+++ lists/forms.py
@@ -32,9 +32,9 @@ class NewListForm(ItemForm):
 
     def save(self, owner):
         if owner.is_authenticated():
-            List.create_new(first_item_text=self.cleaned_data['text'], owner=owner)
+            return List.create_new(first_item_text=self.cleaned_data['text'], owner=owner)
         else:
-            List.create_new(first_item_text=self.cleaned_data['text'])
+            return List.create_new(first_item_text=self.cleaned_data['text'])
 
 
 
