ccd0ad25830bba8e1fa7a838e0963ce020472ed3
Harry <hjwp2@cantab.net>
2014-05-05 19:52:41 +0100

    add dunderfuture import in migration

--- lists/migrations/0005_list_item_unique_together.py
+++ lists/migrations/0005_list_item_unique_together.py
@@ -1,6 +1,7 @@
 # encoding: utf8
-from django.db import models, migrations
+from __future__ import unicode_literals
 
+from django.db import models, migrations
 
 class Migration(migrations.Migration):
 
