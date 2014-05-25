1efda25a909e0418a3022f11712e38cb32ae4c8f
Harry <hjwp2@cantab.net>
2014-05-25 14:33:32 +0100

    get_user returns None if no user. --ch16l035--

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -13,5 +13,8 @@ class PasswordlessAuthenticationBackend(object):
 
 
     def get_user(self, email):
-        return User.objects.get(email=email)
+        try:
+            return User.objects.get(email=email)
+        except User.DoesNotExist:
+            return None
 
