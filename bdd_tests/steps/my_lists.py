ce47f36d066c0a87e0b79d6e2eb394d7a8c1a0a6
Harry <hjwp2@cantab.net>
2014-09-10 12:00:21 +0100

    auto-generated steps

---
+++ bdd_tests/steps/my_lists.py
@@ -0,0 +1,39 @@
+@given('I am a logged-in user')
+def step_impl(context):
+    assert False
+
+@when('I create a list with first item "Reticulate Splines"')
+def step_impl(context):
+    assert False
+
+@when('I add an item "Immanentize Eschaton"')
+def step_impl(context):
+    assert False
+
+@when('I create a list with first item "Buy milk"')
+def step_impl(context):
+    assert False
+
+@then('I will see a link to "My lists"')
+def step_impl(context):
+    assert False
+
+@when('I click the link to "My lists"')
+def step_impl(context):
+    assert False
+
+@then('I will see a link to "Reticulate Splines"')
+def step_impl(context):
+    assert False
+
+@then('I will see a link to "Buy milk"')
+def step_impl(context):
+    assert False
+
+@when('I click the link to "Reticulate Splines"')
+def step_impl(context):
+    assert False
+
+@then('I will be on the "Reticulate Splines" list page')
+def step_impl(context):
+    assert False
