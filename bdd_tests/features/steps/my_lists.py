9ab0d0f4885c90fd27a14a15ad1a34ec592c4a02
Harry <hjwp2@cantab.net>
2014-09-10 12:22:23 +0100

    first step, given am logged in

--- bdd_tests/features/steps/my_lists.py
+++ bdd_tests/features/steps/my_lists.py
@@ -1,6 +1,21 @@
+from behave import given, when, then
+from functional_tests.management.commands.create_session import create_pre_authenticated_session
+from django.conf import settings
+HOME = "http://localhost:8081"
+
+
 @given('I am a logged-in user')
-def step_impl(context):
-    assert False
+def given_i_am_logged_in(context):
+    session_key = create_pre_authenticated_session(email='edith@example.com')
+    ## to set a cookie we need to first visit the domain.
+    ## 404 pages load the quickest!
+    context.browser.get(context.server_url + "/404_no_such_url/")
+    context.browser.add_cookie(dict(
+        name=settings.SESSION_COOKIE_NAME,
+        value=session_key,
+        path='/',
+    ))
+
 
 @when('I create a list with first item "Reticulate Splines"')
 def step_impl(context):
