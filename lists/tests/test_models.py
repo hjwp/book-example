afb152485b65e45edbd189c040afcedc667d41de
syndg <syndbe@gmail.com>
2014-10-31 21:27:58 +0200

    PEP8 fixes in test_models.py

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import get_user_model
-User = get_user_model()
 from django.core.exceptions import ValidationError
 from django.test import TestCase
+
 from lists.models import Item, List
+
+User = get_user_model()
 
 
 class ItemModelTest(TestCase):
@@ -11,14 +13,12 @@ class ItemModelTest(TestCase):
         item = Item()
         self.assertEqual(item.text, '')
 
-
     def test_item_is_related_to_list(self):
         list_ = List.objects.create()
         item = Item()
         item.list = list_
         item.save()
         self.assertIn(item, list_.item_set.all())
-
 
     def test_cannot_save_empty_list_items(self):
         list_ = List.objects.create()
@@ -27,8 +27,6 @@ class ItemModelTest(TestCase):
             item.save()
             item.full_clean()
 
-
-
     def test_duplicate_items_are_invalid(self):
         list_ = List.objects.create()
         Item.objects.create(list=list_, text='bla')
@@ -36,14 +34,12 @@ class ItemModelTest(TestCase):
             item = Item(list=list_, text='bla')
             item.full_clean()
 
-
     def test_CAN_save_same_item_to_different_lists(self):
         list1 = List.objects.create()
         list2 = List.objects.create()
         Item.objects.create(list=list1, text='bla')
         item = Item(list=list2, text='bla')
         item.full_clean()  # should not raise
-
 
     def test_list_ordering(self):
         list1 = List.objects.create()
@@ -55,11 +51,9 @@ class ItemModelTest(TestCase):
             [item1, item2, item3]
         )
 
-
     def test_string_representation(self):
         item = Item(text='some text')
         self.assertEqual(str(item), 'some text')
-
 
 
 class ListModelTest(TestCase):
@@ -68,21 +62,17 @@ class ListModelTest(TestCase):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
 
-
     def test_lists_can_have_owners(self):
         List(owner=User())  # should not raise
 
-
     def test_list_owner_is_optional(self):
         List().full_clean()  # should not raise
-
 
     def test_list_name_is_first_item_text(self):
         list_ = List.objects.create()
         Item.objects.create(list=list_, text='first item')
         Item.objects.create(list=list_, text='second item')
         self.assertEqual(list_.name, 'first item')
-
 
     def test_create_new_creates_list_and_first_item(self):
         List.create_new(first_item_text='new item text')
@@ -91,19 +81,16 @@ class ListModelTest(TestCase):
         new_list = List.objects.first()
         self.assertEqual(new_item.list, new_list)
 
-
     def test_create_new_optionally_saves_owner(self):
         user = User.objects.create()
         List.create_new(first_item_text='new item text', owner=user)
         new_list = List.objects.first()
         self.assertEqual(new_list.owner, user)
 
-
     def test_create_returns_new_list_object(self):
         returned = List.create_new(first_item_text='new item text')
         new_list = List.objects.first()
         self.assertEqual(returned, new_list)
-
 
     def test_can_share_with_another_user(self):
         list_ = List.objects.create()
@@ -111,4 +98,3 @@ class ListModelTest(TestCase):
         list_.shared_with.add('a@b.com')
         list_in_db = List.objects.get(id=list_.id)
         self.assertIn(user, list_in_db.shared_with.all())
-
