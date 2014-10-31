f4822dce7a714a1826795f15f41ba4ea14f13221
syndg <syndbe@gmail.com>
2014-10-31 21:28:17 +0200

    Replaced percent operator format in test_models.py

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -60,7 +60,7 @@ class ListModelTest(TestCase):
 
     def test_get_absolute_url(self):
         list_ = List.objects.create()
-        self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
+        self.assertEqual(list_.get_absolute_url(), '/lists/{}/'.format(list_.id,))
 
     def test_lists_can_have_owners(self):
         List(owner=User())  # should not raise
