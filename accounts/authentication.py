319c8b93931b128d22cbdb9c5cfabfcd953380d3
syndg <syndbe@gmail.com>
2014-10-31 21:00:32 +0200

    Removed explicit object subclass in PersonaAuthenticationBackend.

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 PERSONA_VERIFY_URL = 'https://verifier.login.persona.org/verify'
 
 
-class PersonaAuthenticationBackend(object):
+class PersonaAuthenticationBackend:
 
     def authenticate(self, assertion):
         response = requests.post(
