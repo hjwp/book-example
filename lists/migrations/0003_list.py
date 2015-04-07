5c7f7ea49bf441b342ff989cdd9c30496c87c3d6
Harry <hjwp2@cantab.net>
2015-04-07 08:46:24 +0100

    Tweaks to migrations for 1.8

--- lists/migrations/0003_list.py
+++ lists/migrations/0003_list.py
@@ -16,8 +16,5 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
     ]
