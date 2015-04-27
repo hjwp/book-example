63c954522f7840ba8848b814ff29db7ae011d530
Harry <hjwp2@cantab.net>
2015-04-27 21:50:12 +0100

    Cut down superlists/urls.py

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,12 +1,10 @@
-from django.conf.urls import url
-
-from lists import views
+from django.conf.urls import include, url
+from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home'),
-    url(r'^lists/new$', views.new_list, name='new_list'),
-    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
-    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+    url(r'^$', list_views.home_page, name='home'),
+    url(r'^lists/', include(list_urls)),
     # url(r'^admin/', include(admin.site.urls)),
 ]
 
