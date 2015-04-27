160e83891c4cdeaa5d0b67dfb250de992c24d6d0
Harry <hjwp2@cantab.net>
2015-04-27 21:13:57 +0100

    urls.py updates for 1.8

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
+
+from lists import views
 
 urlpatterns = [
-    # Examples:
-    url(r'^$', 'lists.views.home_page', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.home_page, name='home'),
     # url(r'^admin/', include(admin.site.urls)),
 ]
 
