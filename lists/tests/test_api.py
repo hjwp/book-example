c1bc2153d09d3131b13180eddf81e0d02448b8e6
Harry <hjwp2@cantab.net>
2016-04-21 18:06:52 +0100

    return item dicts for a list GET

--- lists/tests/test_api.py
+++ lists/tests/test_api.py
@@ -1,6 +1,5 @@
 import json
 from django.test import TestCase
-
 from lists.models import List, Item
 
 
@@ -12,4 +11,20 @@ class APIGetListItemsTest(TestCase):
         response = self.client.get(self.base_url.format(list_.id))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response['content-type'], 'application/json')
+
+
+    def test_get_returns_items_for_correct_list(self):
+        other_list = List.objects.create()
+        Item.objects.create(list=other_list, text='item 1')
+        our_list = List.objects.create()
+        item1 = Item.objects.create(list=our_list, text='item 1')
+        item2 = Item.objects.create(list=our_list, text='item 2')
+        response = self.client.get(self.base_url.format(our_list.id))
+        self.assertEqual(
+            json.loads(response.content.decode('utf8')),
+            [
+                {'id': item1.id, 'text': item1.text},
+                {'id': item2.id, 'text': item2.text},
+            ]
+        )
 
