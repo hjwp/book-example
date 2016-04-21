0d3e956ba00480a93399472ad96d2d4f501e1fbe
Harry <hjwp2@cantab.net>
2016-04-21 18:06:52 +0100

    test and implementation for get

--- lists/api.py
+++ lists/api.py
@@ -1,4 +1,16 @@
+import json
 from django.http import HttpResponse
+from lists.models import List
+
 
 def list(request, list_id):
-    return HttpResponse(content_type='application/json')
+    list_ = List.objects.get(id=list_id)
+    item_dicts = [
+        {'id': item.id, 'text': item.text}
+        for item in list_.item_set.all()
+    ]
+    return HttpResponse(
+        json.dumps(item_dicts),
+        content_type='application/json'
+    )
+
--- lists/tests/test_api.py
+++ lists/tests/test_api.py
@@ -15,3 +15,15 @@ class APIGetListItemsTest(TestCase):
         self.assertEqual(response['content-type'], 'application/json')
 
 
+    def test_getting_items(self):
+        list_ = List.objects.create()
+        item1 = Item.objects.create(list=list_, text='item 1')
+        item2 = Item.objects.create(list=list_, text='item 2')
+        response = self.client.get(self.base_url.format(list_.id))
+        self.assertEqual(
+            json.loads(response.content.decode('utf8')),
+            [
+                {'id': item1.id, 'text': item1.text},
+                {'id': item2.id, 'text': item2.text},
+            ]
+        )
