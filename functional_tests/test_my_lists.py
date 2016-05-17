6b1d1220bc28ed1e54c134a181ad3d5e7a32f6bb
Harry <hjwp2@cantab.net>
2016-05-17 09:32:56 +0100

    tweaks to my lists FT, check for email

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -12,8 +12,8 @@ class MyListsTest(FunctionalTest):
         else:
             session_key = create_pre_authenticated_session(email)
 
-        ## to set a cookie we need to first visit the domain.
-        ## 404 pages load the quickest!
+        # to set a cookie we need to first visit the domain.
+        # 404 pages load the quickest!
         self.browser.get(self.server_url + "/404_no_such_url/")
         self.browser.add_cookie(dict(
             name=settings.SESSION_COOKIE_NAME,
@@ -24,30 +24,39 @@ class MyListsTest(FunctionalTest):
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
         # Edith is a logged-in user
-        self.create_pre_authenticated_session('edith@example.com')
+        email = 'edith@example.com'
+        self.create_pre_authenticated_session(email)
 
         # She goes to the home page and starts a list
         self.browser.get(self.server_url)
-        self.get_item_input_box().send_keys('Reticulate splines\n')
-        self.get_item_input_box().send_keys('Immanentize eschaton\n')
+        self.get_item_input_box().send_keys('First list 1st item\n')
+        self.get_item_input_box().send_keys('First list 2nd item\n')
         first_list_url = self.browser.current_url
 
         # She notices a "My lists" link, for the first time.
         self.browser.find_element_by_link_text('My lists').click()
 
+        # When she clicks through to it, she sees the page's title
+        # contains her email
+        header = self.browser.find_element_by_tag_name('h1')
+        self.assertIn(email, header.text)
+
         # She sees that her list is in there, named according to its
         # first list item
-        self.browser.find_element_by_link_text('Reticulate splines').click()
+        self.browser.find_element_by_link_text('First list 1st item').click()
+
+        # clicking on it takes her back to the first list page
         self.assertEqual(self.browser.current_url, first_list_url)
+
 
         # She decides to start another list, just to see
         self.browser.get(self.server_url)
-        self.get_item_input_box().send_keys('Click cows\n')
+        self.get_item_input_box().send_keys('Second list 1st item\n')
         second_list_url = self.browser.current_url
 
         # Under "my lists", her new list appears
         self.browser.find_element_by_link_text('My lists').click()
-        self.browser.find_element_by_link_text('Click cows').click()
+        self.browser.find_element_by_link_text('Second list 1st item').click()
         self.assertEqual(self.browser.current_url, second_list_url)
 
         # She logs out.  The "My lists" option disappears
