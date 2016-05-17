9a3b14d56d930fc4d2d2bd64fb951aab0714d71f
Harry <hjwp2@cantab.net>
2016-05-17 09:32:56 +0100

    tweaks to my lists FT, check for email

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -24,38 +24,46 @@ class MyListsTest(FunctionalTest):
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
         # Edith is a logged-in user
-        self.create_pre_authenticated_session('edith@example.com')
+        email = 'edith@example.com'
+        self.create_pre_authenticated_session(email)
 
         # She goes to the home page and starts a list
         self.browser.get(self.live_server_url)
-        self.add_list_item('Reticulate splines')
-        self.add_list_item('Immanentize eschaton')
+        self.add_list_item('First list 1st item')
+        self.add_list_item('First list 2nd item')
         first_list_url = self.browser.current_url
 
         # She notices a "My lists" link, for the first time.
         self.browser.find_element_by_link_text('My lists').click()
 
+        # When she clicks through to it, she sees the page's title
+        # contains her email
+        self.wait_for(lambda: self.assertIn(
+            email,
+            self.browser.find_element_by_tag_name('h1').text
+        ))
+
         # She sees that her list is in there, named according to its
         # first list item
         self.wait_for(
-            lambda: self.browser.find_element_by_link_text('Reticulate splines')
+            lambda: self.browser.find_element_by_link_text('First list 1st item')
         )
-        self.browser.find_element_by_link_text('Reticulate splines').click()
+        self.browser.find_element_by_link_text('First list 1st item').click()
         self.wait_for(
             lambda: self.assertEqual(self.browser.current_url, first_list_url)
         )
 
         # She decides to start another list, just to see
         self.browser.get(self.live_server_url)
-        self.add_list_item('Click cows')
+        self.add_list_item('Second list 1st item')
         second_list_url = self.browser.current_url
 
         # Under "my lists", her new list appears
         self.browser.find_element_by_link_text('My lists').click()
         self.wait_for(
-            lambda: self.browser.find_element_by_link_text('Click cows')
+            lambda: self.browser.find_element_by_link_text('Second list 1st item')
         )
-        self.browser.find_element_by_link_text('Click cows').click()
+        self.browser.find_element_by_link_text('Second list 1st item').click()
         self.wait_for(
             lambda: self.assertEqual(self.browser.current_url, second_list_url)
         )
