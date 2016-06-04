d027d7b5c7bb5f6e82a7307d50ea25ace04938f7
Harry <hjwp2@cantab.net>
2016-06-04 11:19:28 +0100

    SPIKE google smtp settings. --ch16l007--

--- superlists/settings.py
+++ superlists/settings.py
@@ -118,3 +118,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'obeythetestinggoat@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
