7758dc6d8a4cda401b74f8cc59d913a47bd4d566
Harry <hjwp2@cantab.net>
2016-06-04 11:23:08 +0100

    SPIKE: user model. --ch15l009--

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
+
 
 class Token(models.Model):
     email = models.EmailField()
     uid = models.CharField(max_length=255)
+
+
+
+class ListUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(primary_key=True)
+    USERNAME_FIELD = 'email'
+    #REQUIRED_FIELDS = ['email', 'height']
+
+    objects = ListUserManager()
+
+    @property
+    def is_staff(self):
+        return self.email == 'harry.percival@example.com'
+
+    @property
+    def is_active(self):
+        return True
 
