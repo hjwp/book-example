0bd887f48b20a668005a4f32784845951140311c
Harry <hjwp2@cantab.net>
2016-06-04 14:56:36 +0100

    reintroduce custom LOGGING. --ch15l021-1--

--- superlists/settings.py
+++ superlists/settings.py
@@ -101,3 +101,20 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
