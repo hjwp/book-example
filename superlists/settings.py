9dcb914e7bbd4f489b152b3ed8dd49f43860be9a
Harry <hjwp2@cantab.net>
2016-06-04 14:56:36 +0100

    SPIKE: Add logging for errors. --ch16l017--

--- superlists/settings.py
+++ superlists/settings.py
@@ -132,3 +132,19 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
