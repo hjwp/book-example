ebc80f29368c2984a1c1acb6423ff19962335a96
Harry <hjwp2@cantab.net>
2016-06-04 12:16:26 +0100

    SPIKE urls entries for login and logout. --ch16l014--

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,6 @@ from accounts import views
 
 urlpatterns = [
     url(r'^send_email$', views.send_login_email, name='send_login_email'),
+    url(r'^login$', views.login, name='login'),
+    url(r'^logout$', views.logout, name='logout'),
 ]
