7344006721d848c9383662fb70afc6f412acfecd
Harry <hjwp2@cantab.net>
2016-06-07 02:21:36 +0100

    placeholder send_login_email view. --ch17l004--

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.shortcuts import redirect
 
-# Create your views here.
+def send_login_email(request):
+    return redirect('/')
+
