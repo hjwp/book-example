17188b914cf98a4dcd6ab187251287f879479063
Harry <hjwp2@cantab.net>
2016-06-27 17:20:17 +0100

    switch to using a real mock + patch. --ch17l007--

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-import accounts.views
+from unittest.mock import patch
 
 
 class SendLoginEmailViewTest(TestCase):
@@ -11,25 +11,15 @@ class SendLoginEmailViewTest(TestCase):
         self.assertRedirects(response, '/')
 
 
-    def test_sends_mail_to_address_from_post(self):
-        self.send_mail_called = False
-
-        def fake_send_mail(subject, body, from_email, to_list):
-            self.send_mail_called = True
-            self.subject = subject
-            self.body = body
-            self.from_email = from_email
-            self.to_list = to_list
-
-        accounts.views.send_mail = fake_send_mail
-
+    @patch('accounts.views.send_mail')
+    def test_sends_mail_to_address_from_post(self, mock_send_mail):
         self.client.post('/accounts/send_login_email', data={
             'email': 'edith@example.com'
         })
 
-
-        self.assertTrue(self.send_mail_called)
-        self.assertEqual(self.subject, 'Your login link for Superlists')
-        self.assertEqual(self.from_email, 'noreply@superlists')
-        self.assertEqual(self.to_list, ['edith@example.com'])
+        self.assertEqual(mock_send_mail.called, True)
+        (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+        self.assertEqual(subject, 'Your login link for Superlists')
+        self.assertEqual(from_email, 'noreply@superlists')
+        self.assertEqual(to_list, ['edith@example.com'])
 
