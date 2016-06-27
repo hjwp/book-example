c28e69093c41afc65b2546f1d60f75cae363e9bc
Harry <hjwp2@cantab.net>
2016-06-27 17:26:10 +0100

    add a debug print to see send_mail is mocked. --ch16l009--

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.core.mail import send_mail
 
 def send_login_email(request):
     email = request.POST['email']
+    print(type(send_mail))
     send_mail(
         'Your login link for Superlists',
         'body text tbc',
