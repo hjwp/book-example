0a89f170bcdc296250dd1c698e3f758f2ade3cb8
Harry <hjwp2@cantab.net>
2016-06-27 17:26:32 +0100

    Revert debug print. --ch17l010--
    This reverts commit 07ab6536cacb832ae11557c168e6c6cc470520f6.

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import redirect
 
 def send_login_email(request):
     email = request.POST['email']
-    print(type(send_mail))
     send_mail(
         'Your login link for Superlists',
         'body text tbc',
