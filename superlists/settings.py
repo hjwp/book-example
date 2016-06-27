de276bcb245de450be1fb9714a13809af72324de
Harry <hjwp2@cantab.net>
2016-06-27 20:02:14 +0100

    add custom user model to settings.py. --ch15l026--

--- superlists/settings.py
+++ superlists/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
 )
+
+AUTH_USER_MODEL = 'accounts.User'
+
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
