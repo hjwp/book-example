dc51268c65f6a72a315e1572e462242a41dc93e9
Harry <hjwp2@cantab.net>
2016-06-27 20:05:50 +0100

    Migrations for custom user model. --ch15l029--

---
+++ accounts/migrations/0001_initial.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('email', models.EmailField(serialize=False, max_length=254, primary_key=True)),
+            ],
+        ),
+    ]
