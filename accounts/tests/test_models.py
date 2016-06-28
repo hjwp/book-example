115d71b9f2767fbe3f81d7a6a63b4480c18347ec
Harry <hjwp2@cantab.net>
2016-06-28 23:13:18 +0100

    unit test for token model. --ch16l030--

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -1,11 +1,21 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-
+from accounts.models import Token
 User = get_user_model()
+
 
 class UserModelTest(TestCase):
 
     def test_user_is_valid_with_email_only(self):
         user = User(email='a@b.com')
         user.full_clean()  # should not raise
+
+
+
+class TokenModelTest(TestCase):
+
+    def test_links_user_with_auto_generated_uid(self):
+        token1 = Token.objects.create(email='a@b.com')
+        token2 = Token.objects.create(email='a@b.com')
+        self.assertNotEqual(token1.uid, token2.uid)
 
