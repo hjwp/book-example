8af090888e8c9b97624e00c4d3b053680c13e75e
Harry <hjwp2@cantab.net>
2016-06-28 23:25:14 +0100

    migration for almost-correct token. --ch16l034--

--- accounts/migrations/0002_token.py
+++ accounts/migrations/0002_token.py
@@ -17,6 +17,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('email', models.EmailField(max_length=254)),
+                ('uid', models.CharField(max_length=40)),
             ],
         ),
     ]
