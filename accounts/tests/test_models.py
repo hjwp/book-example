85c7d30bba82f1fd4fad65944e9adc684de3f8c7
Harry <hjwp2@cantab.net>
2016-06-28 23:13:18 +0100

    unit test for token model. --ch16l030--

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-
+from accounts.models import Token
 User = get_user_model()
+
 
 class UserModelTest(TestCase):
 
@@ -13,4 +14,13 @@ class UserModelTest(TestCase):
     def test_email_is_primary_key(self):
         user = User(email='a@b.com')
         self.assertEqual(user.pk, 'a@b.com')
+
+
+
+class TokenModelTest(TestCase):
+
+    def test_links_user_with_auto_generated_uid(self):
+        token1 = Token.objects.create(email='a@b.com')
+        token2 = Token.objects.create(email='a@b.com')
+        self.assertNotEqual(token1.uid, token2.uid)
 
