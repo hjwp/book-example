759723c56058bb9c75362ae4fe2a66fffb2c1efa
Harry <hjwp2@cantab.net>
2016-06-29 21:41:29 +0100

    add success message. --ch16l014--

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect
 from django.core.mail import send_mail
+from django.contrib import messages
 
 def send_login_email(request):
     email = request.POST['email']
@@ -8,6 +9,10 @@ def send_login_email(request):
         'body text tbc',
         'noreply@superlists',
         [email]
+    )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
     )
     return redirect('/')
 
