19ffecc670c99320e59bfc18f9b13a6e3a76a2ad
Harry <hjwp2@cantab.net>
2016-06-29 16:05:10 +0100

    Basic unit test for the email view should redirect. --ch17l003--

---
+++ accounts/tests/test_views.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+
+class SendLoginEmailViewTest(TestCase):
+
+    def test_redirects_to_home_page(self):
+        response = self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'
+        })
+        self.assertRedirects(response, '/')
+
