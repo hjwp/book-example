a7a59accedb18f1e11f2f15dde41884cbd198216
Harry <hjwp2@cantab.net>
2016-06-30 15:20:27 +0100

    handle case where user doesnt exist. --ch17l028--

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -6,6 +6,8 @@ class PasswordlessAuthenticationBackend(object):
         try:
             token = Token.objects.get(uid=uid)
             return User.objects.get(email=token.email)
+        except User.DoesNotExist:
+            return User.objects.create(email=token.email)
         except Token.DoesNotExist:
             return None
 
