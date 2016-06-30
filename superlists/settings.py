e659a83d2b3f7d44f74d79498d269d552e1f6eba
Harry <hjwp2@cantab.net>
2016-06-30 15:21:14 +0100

    add auth backend to settings. --ch16l036--

--- superlists/settings.py
+++ superlists/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = (
 )
 
 AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = (
+    'accounts.authentication.PasswordlessAuthenticationBackend',
+)
 
 
 MIDDLEWARE_CLASSES = (
