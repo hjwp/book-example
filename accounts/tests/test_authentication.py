6a94b35753adf0aa06285f90e510b31c1f212ca6
Harry <hjwp2@cantab.net>
2016-06-30 17:03:38 +0100

    test for get_user on custom auth backend. --ch17l030--

--- accounts/tests/test_authentication.py
+++ accounts/tests/test_authentication.py
@@ -29,3 +29,21 @@ class AuthenticateTest(TestCase):
         user = PasswordlessAuthenticationBackend().authenticate(token.uid)
         self.assertEqual(user, existing_user)
 
+
+
+class GetUserTest(TestCase):
+
+    def test_gets_user_by_email(self):
+        User.objects.create(email='another@example.com')
+        desired_user = User.objects.create(email='edith@example.com')
+        found_user = PasswordlessAuthenticationBackend().get_user(
+            'edith@example.com'
+        )
+        self.assertEqual(found_user, desired_user)
+
+
+    def test_returns_None_if_no_user_with_that_email(self):
+        self.assertIsNone(
+            PasswordlessAuthenticationBackend().get_user('edith@example.com')
+        )
+
