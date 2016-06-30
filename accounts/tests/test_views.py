4fc0fb0ace5c17868a45ea8f636de5f3e22750ea
Harry <hjwp2@cantab.net>
2016-06-30 18:43:28 +0100

    final test for login, move patch to class level. --ch17l044--

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -59,14 +59,14 @@ class SendLoginEmailViewTest(TestCase):
 
 
 
+@patch('accounts.views.auth')
 class LoginViewTest(TestCase):
 
-    def test_redirects_to_home_page(self):
+    def test_redirects_to_home_page(self, mock_auth):
         response = self.client.get('/accounts/login?token=abcd123')
         self.assertRedirects(response, '/')
 
 
-    @patch('accounts.views.auth')
     def test_calls_authenticate_with_uid_from_get_request(self, mock_auth):
         self.client.get('/accounts/login?token=abcd123')
         self.assertEqual(
@@ -75,11 +75,16 @@ class LoginViewTest(TestCase):
         )
 
 
-    @patch('accounts.views.auth')
     def test_calls_auth_login_with_user_if_there_is_one(self, mock_auth):
         response = self.client.get('/accounts/login?token=abcd123')
         self.assertEqual(
             mock_auth.login.call_args,
             call(response.wsgi_request, mock_auth.authenticate.return_value)
         )
+
+
+    def test_does_not_login_if_user_is_not_authenticated(self, mock_auth):
+        mock_auth.authenticate.return_value = None
+        self.client.get('/accounts/login?token=abcd123')
+        self.assertEqual(mock_auth.login.called, False)
 
