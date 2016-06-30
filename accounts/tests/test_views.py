d226636e8011873274c53cd7f78535cfadebbd13
Harry <hjwp2@cantab.net>
2016-06-30 18:19:05 +0100

    A second mocky test for the login view. --ch17l041--

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -74,3 +74,12 @@ class LoginViewTest(TestCase):
             call(uid='abcd123')
         )
 
+
+    @patch('accounts.views.auth')
+    def test_calls_auth_login_with_user_if_there_is_one(self, mock_auth):
+        response = self.client.get('/accounts/login?token=abcd123')
+        self.assertEqual(
+            mock_auth.login.call_args,
+            call(response.wsgi_request, mock_auth.authenticate.return_value)
+        )
+
