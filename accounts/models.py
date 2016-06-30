08db4143854605715993ace82a99db63a6451f72
Harry <hjwp2@cantab.net>
2016-06-30 20:29:45 +0100

    Fix signal bug. --ch17l048--

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,10 @@
 import uuid
+from django.contrib import auth
 from django.db import models
+
+
+auth.signals.user_logged_in.disconnect(auth.models.update_last_login)
+
 
 class User(models.Model):
     email = models.EmailField(primary_key=True)
