2557d78da514f8bd1c8ddc04a3a2176d885aa44e
Harry <hjwp2@cantab.net>
2016-06-30 20:27:07 +0100

    New unit test for broken auth signal handler. --ch17l047--

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
-from django.contrib.auth import get_user_model
+from django.contrib import auth
 from accounts.models import Token
-User = get_user_model()
+User = auth.get_user_model()
 
 
 class UserModelTest(TestCase):
@@ -14,6 +14,13 @@ class UserModelTest(TestCase):
     def test_email_is_primary_key(self):
         user = User(email='a@b.com')
         self.assertEqual(user.pk, 'a@b.com')
+
+
+    def test_no_problem_with_auth_login(self):
+        user = User.objects.create(email='edith@example.com')
+        user.backend = ''
+        request = self.client.request().wsgi_request
+        auth.login(request, user)  # should not raise
 
 
 
