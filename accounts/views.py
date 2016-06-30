c5b7e55bed6b46367a6fe704ebe8e3c5fcad1107
Harry <hjwp2@cantab.net>
2016-06-30 14:53:44 +0100

    Use token in url in message body. --ch17l023--

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
 
 from accounts.models import Token
@@ -8,9 +9,13 @@ from accounts.models import Token
 def send_login_email(request):
     email = request.POST['email']
     token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    message_body = f'Use this link to log in:\n\n{url}'
     send_mail(
         'Your login link for Superlists',
-        'Use this link to log in',
+        message_body,
         'noreply@superlists',
         [email]
     )
