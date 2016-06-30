21227f82e24dae76ca983c7132c049b4c283c4b4
Harry <hjwp2@cantab.net>
2016-06-30 15:15:58 +0100

    auth backend: handle case where no such token. --ch17l027--

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -3,6 +3,9 @@ from accounts.models import User, Token
 class PasswordlessAuthenticationBackend(object):
 
     def authenticate(self, uid):
-        token = Token.objects.get(uid=uid)
-        return User.objects.get(email=token.email)
+        try:
+            token = Token.objects.get(uid=uid)
+            return User.objects.get(email=token.email)
+        except Token.DoesNotExist:
+            return None
 
