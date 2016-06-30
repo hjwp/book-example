0c077c9d91ccbe9fb73551c336ca8499adc81fde
Harry <hjwp2@cantab.net>
2016-06-30 15:12:18 +0100

    three unit tests for custom auth backend. --ch16l024--

---
+++ accounts/tests/test_authentication.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from accounts.authentication import PasswordlessAuthenticationBackend
+from accounts.models import Token
+User = get_user_model()
+
+
+class AuthenticateTest(TestCase):
+
+    def test_returns_None_if_no_such_token(self):
+        result = PasswordlessAuthenticationBackend().authenticate(
+            'no-such-token'
+        )
+        self.assertIsNone(result)
+
+
+    def test_returns_new_user_with_correct_email_if_token_exists(self):
+        email = 'edith@example.com'
+        token = Token.objects.create(email=email)
+        user = PasswordlessAuthenticationBackend().authenticate(token.uid)
+        new_user = User.objects.get(email=email)
+        self.assertEqual(user, new_user)
+
+
+    def test_returns_existing_user_with_correct_email_if_token_exists(self):
+        email = 'edith@example.com'
+        existing_user = User.objects.create(email=email)
+        token = Token.objects.create(email=email)
+        user = PasswordlessAuthenticationBackend().authenticate(token.uid)
+        self.assertEqual(user, existing_user)
+
