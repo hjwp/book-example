0c27fa1e3e50e58c4cac5e2babe397a2b39afbc7
Harry <hjwp2@cantab.net>
2016-07-11 06:55:09 +0100

    reintroduce gmail smtp settings. --ch17l049--

--- superlists/settings.py
+++ superlists/settings.py
@@ -143,3 +143,9 @@ LOGGING = {
     },
     'root': {'level': 'INFO'},
 }
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'obeythetestinggoat@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_USE_TLS = True
+
