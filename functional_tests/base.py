a2258fa1732f93fff16220b8be56b481a53f9d39
Harry <hjwp2@cantab.net>
2016-08-17 22:42:51 +0100

    helper method for checking login/logout status in ft base. --ch17l002--

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -2,6 +2,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 import sys
 
+
 class FunctionalTest(StaticLiveServerTestCase):
 
     @classmethod
@@ -35,4 +36,16 @@ class FunctionalTest(StaticLiveServerTestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
+
+
+    def assert_logged_in(self, email):
+        self.browser.find_element_by_link_text('Log out')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(email, navbar.text)
+
+
+    def assert_logged_out(self, email):
+        self.browser.find_element_by_name('email')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(email, navbar.text)
 
