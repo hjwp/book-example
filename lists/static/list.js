23f89b413d02eaa9557568535b57c507a1f07782
Harry <hjwp2@cantab.net>
2016-09-23 17:11:43 +0200

    lists.js now exports an initialize function. --ch14l016--

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,8 @@
-$('input[name="text"]').on('keypress', function () {
-  console.log('in keypress handler');
-  $('.has-error').hide();
-});
+var initialize = function () {
+  console.log('initialize called');
+  $('input[name="text"]').on('keypress', function () {
+    console.log('in keypress handler');
+    $('.has-error').hide();
+  });
+};
 console.log('list.js loaded');
