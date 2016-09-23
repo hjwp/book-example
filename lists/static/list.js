f542f5cf99b52142890add339c1e60aee364e188
Harry <hjwp2@cantab.net>
2016-09-23 17:34:34 +0200

    namespace Superlists in lists.js. --ch14l021--

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,4 +1,5 @@
-var initialize = function () {
+window.Superlists = {};
+window.Superlists.initialize = function () {
   $('input[name="text"]').on('keypress', function () {
     $('.has-error').hide();
   });
