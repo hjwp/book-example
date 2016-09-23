5f618957915efe6589228946429ad81c036af897
Harry <hjwp2@cantab.net>
2016-09-23 17:19:33 +0200

    strip out console logs from lists.js. --ch14l018--

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,8 +1,5 @@
 var initialize = function () {
-  console.log('initialize called');
   $('input[name="text"]').on('keypress', function () {
-    console.log('in keypress handler');
     $('.has-error').hide();
   });
 };
-console.log('list.js loaded');
