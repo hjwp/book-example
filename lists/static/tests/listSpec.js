1b9f1b1415b1669931ea8d89b7bb56daaa4f7bde
Harry <hjwp2@cantab.net>
2016-09-25 07:37:46 +0200

    start testing what we actually want

--- lists/static/tests/listSpec.js
+++ lists/static/tests/listSpec.js
@@ -13,15 +13,8 @@ describe("list js", function() {
     $('#testform').remove();
   });
 
-  it("should be able to use jquery to create and hide things", function() {
-    expect( $('.has-error').is(':visible') ).toBe(true);
-    $('.has-error').hide();
-    expect( $('.has-error').is(':visible') ).toBe(false);
-  });
-
-  it("should be able to run the same test twice", function() {
-    expect( $('.has-error').is(':visible') ).toBe(true);
-    $('.has-error').hide();
+  it("should hide errors on keypress", function() {
+    $('#testform input').trigger('keypress');
     expect( $('.has-error').is(':visible') ).toBe(false);
   });
 
