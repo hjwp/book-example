5400d66e34ff3e6ec72d20d14c06b36f21676a15
Harry <hjwp2@cantab.net>
2016-09-25 07:29:35 +0200

    use beforeEach and afterEach, inject fixtures manually, remove from runner html

--- lists/static/tests/listSpec.js
+++ lists/static/tests/listSpec.js
@@ -1,4 +1,17 @@
 describe("list js", function() {
+  beforeEach(function () {
+    var form = $(
+      '<form id="testform">' +
+        '<input name="text" />' +
+        '<div class="has-error"></div>' +
+      '</form>'
+    );
+    $('body').append(form);
+
+  });
+  afterEach(function () {
+    $('#testform').remove();
+  });
 
   it("should be able to use jquery to create and hide things", function() {
     expect( $('.has-error').is(':visible') ).toBe(true);
