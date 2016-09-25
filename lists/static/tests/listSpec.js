8acd3b06ac800ffe1a6488f4909714b2985b33dc
Harry <hjwp2@cantab.net>
2016-09-25 07:14:41 +0200

    dupe the test. now fails

--- lists/static/tests/listSpec.js
+++ lists/static/tests/listSpec.js
@@ -1,8 +1,16 @@
 describe("list js", function() {
+
   it("should be able to use jquery to create and hide things", function() {
     expect( $('.has-error').is(':visible') ).toBe(true);
     $('.has-error').hide();
     expect( $('.has-error').is(':visible') ).toBe(false);
   });
+
+  it("should be able to run the same test twice", function() {
+    expect( $('.has-error').is(':visible') ).toBe(true);
+    $('.has-error').hide();
+    expect( $('.has-error').is(':visible') ).toBe(false);
+  });
+
 });
 
