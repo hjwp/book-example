4b87589019e2edaa8eed3b61e12a92bf8e8cd63e
Harry <hjwp2@cantab.net>
2016-09-26 17:20:23 +0200

    New js tests for Ajax, one written two placeholders

--- lists/static/tests/listSpec.js
+++ lists/static/tests/listSpec.js
@@ -1,4 +1,5 @@
 describe("list js", function() {
+
   beforeEach(function () {
     var form = $(
       '<form id="testform">' +
@@ -7,22 +8,66 @@ describe("list js", function() {
       '</form>'
     );
     $('body').append(form);
+    var table = $('<table id="id_list_table"></table>');
+    $('body').append(table);
+    jasmine.Ajax.install();
 
   });
   afterEach(function () {
     $('#testform').remove();
+    $('#id_list_table').remove();
+    jasmine.Ajax.uninstall();
   });
 
-  it("should hide errors on keypress", function() {
-    window.Superlists.hideErrorsOnInput();
-    $('#testform input').trigger('keypress');
-    expect( $('.has-error').is(':visible') ).toBe(false);
+  describe("hiding errors on input", function() {
+
+    it("should hide errors on keypress", function() {
+      window.Superlists.hideErrorsOnInput();
+      $('#testform input').trigger('keypress');
+      expect( $('.has-error').is(':visible') ).toBe(false);
+    });
+
+    it("should not hide errors unnecessarily", function() {
+      window.Superlists.hideErrorsOnInput();
+      expect( $('.has-error').is(':visible') ).toBe(true);
+    });
+
   });
 
-  it("should not hide errors unnecessarily", function() {
-    window.Superlists.hideErrorsOnInput();
-    expect( $('.has-error').is(':visible') ).toBe(true);
-  });
+  describe("retrieving and adding to existing lists via ajax", function() {
 
+    it("should retrieve items via ajax and fill in lists table on page load", function () {
+      var url = '/listitemsapi/';
+      window.Superlists.startAjax(url);
+
+      expect(jasmine.Ajax.requests.mostRecent().url).toBe(url);
+
+      var rowsJson = JSON.stringify([
+          {'id': 101, 'text': 'item 1 text'},
+          {'id': 102, 'text': 'item 2 text'},
+      ]);
+
+      jasmine.Ajax.requests.mostRecent().respondWith({
+        "status": 200,
+        "contentType": 'application/json',
+        "responseText": rowsJson
+      });
+
+      var rows = $('#id_list_table tr');
+      expect(rows.length).toEqual(2);
+      var row1 = $('#id_list_table tr:first-child td');
+      expect(row1.text()).toEqual('1: item 1 text');
+      var row2 = $('#id_list_table tr:last-child td');
+      expect(row2.text()).toEqual('2: item 2 text');
+    });
+
+
+    it("should intercept form submit and do ajax post", function () {
+    });
+
+
+    it("should repopulate items table after post", function () {
+    });
+
+  });
 });
-
