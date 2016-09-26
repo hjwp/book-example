06ece2c52567021fbf4c252eeea00c9c88c0dc70
Harry <hjwp2@cantab.net>
2016-09-26 17:54:32 +0200

    start on lists js, get on page load now works

--- lists/static/list.js
+++ lists/static/list.js
@@ -5,3 +5,14 @@ window.Superlists.hideErrorsOnInput = function () {
     $('.has-error').hide();
   });
 };
+
+window.Superlists.startAjax = function (url) {
+  $.get(url).done(function (response) {
+    var rows = '';
+    for (var i=0; i<response.length; i++) {
+      var row = response[i];
+      rows += '<tr><td>' + (i+1) + ': ' + row.text + '</td></tr>\n';
+    }
+    $('#id_list_table').html(rows);
+  });
+};
