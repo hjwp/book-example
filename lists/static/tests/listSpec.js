37ab48d0ba581c5bfee6070c800e707061c59b9f
Harry <hjwp2@cantab.net>
2016-09-28 12:36:50 +0200

    test for repopulating table after post

--- lists/static/tests/listSpec.js
+++ lists/static/tests/listSpec.js
@@ -78,8 +78,38 @@ describe("list js", function() {
     });
 
     it("should repopulate items table after post", function () {
+      var url = '/listitemsapi/';
+      window.Superlists.startAjax(url);
+      $('form#testform').submit();
+      jasmine.Ajax.requests.mostRecent().respondWith({
+        "status": 201,
+        "contentType": 'application/json',
+        'responseText': '{}'
+      });
 
+      var getRequest = jasmine.Ajax.requests.mostRecent();
+
+      expect(getRequest.url).toBe(url);
+      expect(getRequest.method).toBe('GET');
+
+      var rowsJson = JSON.stringify([
+          {'id': 101, 'text': 'item 1 text'},
+          {'id': 102, 'text': 'item 2 text'},
+      ]);
+
+      getRequest.respondWith({
+        "status": 200,
+        "contentType": 'application/json',
+        "responseText": rowsJson
+      });
+
+      var rows = $('#id_list_table tr');
+      expect(rows.length).toEqual(2);
+      var row1 = $('#id_list_table tr:first-child td');
+      expect(row1.text()).toEqual('1: item 1 text');
+      var row2 = $('#id_list_table tr:last-child td');
+      expect(row2.text()).toEqual('2: item 2 text');
     });
-
   });
 });
+
