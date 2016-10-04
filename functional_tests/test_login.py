151c034f6a840f29f9f8f9355fd9fbc6a8b2fa08
Harry <hjwp2@cantab.net>
2016-10-05 00:35:59 +0200

    helper function for retrieving emails.  --ch18l010--

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,6 +1,9 @@
+import os
+import poplib
+import re
+import time
 from django.core import mail
 from selenium.webdriver.common.keys import Keys
-import re
 
 from .base import FunctionalTest
 
@@ -9,6 +12,38 @@ SUBJECT = 'Your login link for Superlists'
 
 
 class LoginTest(FunctionalTest):
+
+    def wait_for_email(self, test_email, subject):
+        if not self.staging_server:
+            email = mail.outbox[0]
+            self.assertIn(test_email, email.to)
+            self.assertEqual(email.subject, subject)
+            return email.body
+
+        email_id = None
+        start = time.time()
+        inbox = poplib.POP3_SSL('pop.mail.yahoo.com')
+        try:
+            inbox.user(test_email)
+            inbox.pass_(os.environ['YAHOO_PASSWORD'])
+            while time.time() - start < 60:
+                # get 10 newest messages
+                count, _ = inbox.stat()
+                for i in reversed(range(max(1, count - 10), count + 1)):
+                    print('getting msg', i)
+                    _, lines, __ = inbox.retr(i)
+                    lines = [l.decode('utf8') for l in lines]
+                    print(lines)
+                    if f'Subject: {subject}' in lines:
+                        email_id = i
+                        body = '\n'.join(lines)
+                        return body
+                time.sleep(5)
+        finally:
+            if email_id:
+                inbox.dele(email_id)
+            inbox.quit()
+
 
     def test_can_get_email_link_to_log_in(self):
         # Edith goes to the awesome superlists site
