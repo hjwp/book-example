c18baf1cc6920a529bf85b3eee0e3848aece450b
Harry <hjwp2@cantab.net>
2016-10-05 00:34:30 +0200

    Save self.staging_server hostname in FT runner. --ch18l009--

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -25,9 +25,9 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
+        self.staging_server = os.environ.get('STAGING_SERVER')
+        if self.staging_server:
+            self.live_server_url = 'http://' + self.staging_server
 
 
     def tearDown(self):
