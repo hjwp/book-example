57bb30b6449cb5c49cbc939b96a50031c9c6a9d6
Harry <hjwp2@cantab.net>
2016-10-05 14:59:43 +0200

    testing jquery, inject dom fixture

--- lists/static/tests/listSpec.js
+++ lists/static/tests/listSpec.js
@@ -1,6 +1,28 @@
-describe("list js", function() {
-  it("should have working maths", function() {
-    expect(1 + 1).toEqual(2);
+describe('my test runner', function () {
+
+  beforeEach(function () {
+    var htmlFixture = $('<div id="myfixture" class="has-error">Error!</div>');
+    $('body').append(htmlFixture);
   });
+
+  afterEach(function () {
+    $('#myfixture').remove();
+  });
+
+  it('should be able to check element visibility', function () {
+    expect( $('.has-error').length ).toEqual(1);
+    expect( $('.has-error').is(':visible') ).toBe(true);
+    $('.has-error').hide();
+    expect( $('.has-error').is(':visible') ).toBe(false);
+  });
+
+  it('should be able to check element visibility twice', function () {
+    expect( $('.has-error').length ).toEqual(1);
+    expect( $('.has-error').is(':visible') ).toBe(true);
+    $('.has-error').hide();
+    expect( $('.has-error').is(':visible') ).toBe(false);
+
+  });
+
 });
 
