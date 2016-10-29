330b1a77ff49efbf18b95ecd855e9d633b2c84b2
Harry <hjwp2@cantab.net>
2016-10-29 22:34:06 +0100

    just do the get part

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,6 +1,10 @@
 window.Superlists = {};
-window.Superlists.initialize = function () {
+window.Superlists.initialize = function (url) {
   $('input[name="text"]').on('keypress', function () {
     $('.has-error').hide();
   });
+
+  $.get(url);
+
 };
+
