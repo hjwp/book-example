9d0b1be28a426ee6fe6183aa6746db535dd708e2
Harry <hjwp2@cantab.net>
2016-10-30 12:36:15 +0000

    helper function to get items via Ajax

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,18 +1,23 @@
 window.Superlists = {};
+
+window.Superlists.updateItems = function (url) {
+  $.get(url).done(function (response) {
+    var rows = '';
+    for (var i=0; i<response.length; i++) {
+      var item = response[i];
+      rows += '\n<tr><td>' + (i+1) + ': ' + item.text + '</td></tr>';
+    }
+    $('#id_list_table').html(rows);
+  });
+};
+
 window.Superlists.initialize = function (url) {
   $('input[name="text"]').on('keypress', function () {
     $('.has-error').hide();
   });
 
   if (url) {
-    $.get(url).done(function (response) {
-      var rows = '';
-      for (var i=0; i<response.length; i++) {
-        var item = response[i];
-        rows += '\n<tr><td>' + (i+1) + ': ' + item.text + '</td></tr>';
-      }
-      $('#id_list_table').html(rows);
-    });
+    window.Superlists.updateItems(url);
 
     var form = $('#id_item_form');
     form.on('submit', function(event) {
