64a1fc8d55f4c2b4322eb6510bdd2f3753dbc602
Harry <hjwp2@cantab.net>
2016-11-05 20:58:31 +0000

    hacks to get errors working

--- lists/api.py
+++ lists/api.py
@@ -1,6 +1,8 @@
+from django.core.exceptions import ValidationError
 from django.conf.urls import url
 from lists.models import List, Item
 from rest_framework import serializers, generics
+from lists.forms import EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR
 
 
 class ItemSerializer(serializers.ModelSerializer):
@@ -17,9 +19,23 @@ class ListAPIView(generics.ListCreateAPIView):
         list_ = List.objects.get(id=self.kwargs['id'])
         return list_.item_set.all()
 
+
     def perform_create(self, serializer):
         list_ = List.objects.get(id=self.kwargs['id'])
-        Item.objects.create(list=list_, text=serializer.validated_data['text'])
+        item = Item(list=list_, text=serializer.validated_data.get('text'))
+        try:
+            item.full_clean()
+            item.save()
+        except ValidationError as e:
+            if 'text' in e.message_dict:
+                error = EMPTY_ITEM_ERROR
+            if '__all__' in e.message_dict:
+                error = DUPLICATE_ITEM_ERROR
+            raise serializers.ValidationError({'error': error})
+
+
+
+
 
 
 
