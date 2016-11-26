c51d0ef5c1e84f74b0977213572ff2616864fe83
Harry <hjwp2@cantab.net>
2016-11-26 18:59:09 +0000

    copy urls to lists/urls --ch16l044--

---
+++ lists/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from lists import views
+
+urlpatterns = [
+    url(r'^$', views.home_page, name='home'),
+    url(r'^lists/new$', views.new_list, name='new_list'),
+    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+]
+
