8695a090dcfe2f8a1d60d29e17ba5f46165289f0
Harry <hjwp2@cantab.net>
2016-11-30 11:19:35 +0000

    strip down urls.py to just the example entry

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from django.contrib import admin
+from lists import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home_page, name='home'),
 ]
+
