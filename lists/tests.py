8aeff9a76dc8b897769a51f4c16dabfa3f4f502d
Harry <hjwp2@cantab.net>
2016-11-30 11:04:11 +0000

    simplify tests down to just one with django test client. --ch04l010--

--- lists/tests.py
+++ lists/tests.py
@@ -1,24 +1,9 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
-
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-
-        self.assertTemplateUsed(response, 'wrong.html')
+        self.assertTemplateUsed(response, 'home.html')
 
