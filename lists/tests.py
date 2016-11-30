0d30ff645c4b712e97acfa2d9a314aa8bb559bbe
Harry <hjwp2@cantab.net>
2016-11-30 10:49:23 +0000

    add a .strip() to get tests passing

--- lists/tests.py
+++ lists/tests.py
@@ -18,5 +18,5 @@ class HomePageTest(TestCase):
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
 
