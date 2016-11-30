092bc42603cd63fd1bea2441ab3c3a8a54952633
Harry <hjwp2@cantab.net>
2016-11-30 11:17:52 +0000

    First test for resolving root URL and home_page=None

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,10 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-# Create your tests here.
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
