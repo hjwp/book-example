3508f9eac378639b9c2b524bd45c2fb2a07df557
Harry <hjwp2@cantab.net>
2016-11-30 10:54:25 +0000

    use django test client

--- lists/tests.py
+++ lists/tests.py
@@ -13,10 +13,12 @@ class HomePageTest(TestCase):
 
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/')
+
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
 
