24e9e96859aab4fb324e09fd2c3efcc67c39e27c
Harry <hjwp2@cantab.net>
2016-12-01 20:26:38 +0000

    remove old item creation code from home page. --ch07l024--

--- lists/views.py
+++ lists/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import redirect, render
 from lists.models import Item
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 
