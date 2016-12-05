691af1c93109e395d15015709f2ff0faaff8af49
Harry <hjwp2@cantab.net>
2016-12-05 14:51:49 +0000

    Revert spike

--- accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from accounts import views
-
-urlpatterns = [
-    url(r'^send_email$', views.send_login_email, name='send_login_email'),
-    url(r'^login$', views.login, name='login'),
-    url(r'^logout$', views.logout, name='logout'),
-]
