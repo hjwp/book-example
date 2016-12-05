2b10d2053d3d11c846d598a849834c39d473e616
Harry <hjwp2@cantab.net>
2016-12-05 14:51:49 +0000

    Revert spike

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import include, url
 from lists import views as list_views
 from lists import urls as list_urls
-from accounts import urls as accounts_urls
 
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
-    url(r'^accounts/', include(accounts_urls)),
 ]
 
