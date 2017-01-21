b2133202b9b1a5f8ab2a2b4bfa8f65121a4418e1
Harry <hjwp2@cantab.net>
2017-01-21 13:14:54 +0000

    Make a copy of our existing waiter method, change the args. --ch13l010--

--- src/functional_tests/base.py
+++ src/functional_tests/base.py
@@ -18,6 +18,19 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, "id_list_table")
+                rows = table.find_element(By.TAG_NAME, "tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException):
+                if time.time() - start_time > MAX_WAIT:
+                    raise
+                time.sleep(0.5)
+
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
