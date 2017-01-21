7219bb93e065a2c20d10acb2fcb03af5b5181e4d
Harry <hjwp2@cantab.net>
2017-01-21 13:21:55 +0000

    now execute the fn() we were passed in. --ch11l011--

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -25,10 +25,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         start_time = time.time()
         while True:
             try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
+                return fn()
             except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
                     raise e
