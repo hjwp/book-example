3d920a9d3d1ee6c85eae8c4e7d366ec12e209f62
Harry <hjwp2@cantab.net>
2017-01-28 18:11:18 +0000

    ft helper method for finding input box. --ch11l018--

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -51,3 +51,7 @@ class FunctionalTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
+
+    def get_item_input_box(self):
+        return self.browser.find_element_by_id('id_text')
+
