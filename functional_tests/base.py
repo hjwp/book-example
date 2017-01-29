29eecc6bb117272829bd4ac297feea11bee41da6
Harry <hjwp2@cantab.net>
2017-01-29 14:26:48 +0000

    Finally, use our generic wait decorator in self.wait_for. --ch18l008--

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -34,15 +34,9 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.browser.quit()
 
 
+    @wait
     def wait_for(self, fn):
-        start_time = time.time()
-        while True:
-            try:
-                return fn()
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+        return fn()
 
 
     @wait
