922159bf4637f04ed3465584f1fb11a1bdf2a922
Harry <hjwp2@cantab.net>
2017-01-29 14:49:47 +0000

    Fix decorator by adding *args, **kwargs, --ch18l007--

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -8,11 +8,11 @@ MAX_WAIT = 10
 
 
 def wait(fn):
-    def modified_fn():
+    def modified_fn(*args, **kwargs):
         start_time = time.time()
         while True:
             try:
-                return fn()
+                return fn(*args, **kwargs)
             except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
                     raise e
