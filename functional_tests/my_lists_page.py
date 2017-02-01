d4a9aca0e68196a25408eba8da2760d819788511
Harry <hjwp2@cantab.net>
2017-02-01 12:56:52 +0000

    Start a MyListsPage. --ch22l011--

---
+++ functional_tests/my_lists_page.py
@@ -0,0 +1,14 @@
+class MyListsPage(object):
+
+    def __init__(self, test):
+        self.test = test
+
+    def go_to_my_lists_page(self):
+        self.test.browser.get(self.test.live_server_url)
+        self.test.browser.find_element_by_link_text('My lists').click()
+        self.test.wait_for(lambda: self.test.assertEqual(
+            self.test.browser.find_element_by_tag_name('h1').text,
+            'My Lists'
+        ))
+        return self
+
