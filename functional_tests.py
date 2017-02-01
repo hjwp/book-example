24d7733da692143fbc23902334d3e85763cd4396
Harry <hjwp2@cantab.net>
2017-02-01 14:58:30 +0000

    no need for explicit wait any more

--- functional_tests.py
+++ functional_tests.py
@@ -5,7 +5,6 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
