74e0a76a6d17a5a3ed1fcdca9492854b1e00ace6
Harry <hjwp2@cantab.net>
2017-02-12 22:04:53 +0000

    wire up new list detail view in urls.py naively. --ch31l010--

--- lists/urls.py
+++ lists/urls.py
@@ -3,6 +3,6 @@ from lists import views
 
 urlpatterns = [
     url(r'^new$', views.NewListView.as_view(), name='new_list'),
-    url(r'^(\d+)/$', views.view_list, name='view_list'),
+    url(r'^(\d+)/$', views.ViewAndAddToList.as_view(), name='view_list'),
 ]
 
