a9747d0575be380b2a884d6690a2b779fd5bd982
Harry <hjwp2@cantab.net>
2017-02-12 22:06:14 +0000

    Add pk capture to detail view url. --ch31l011--

--- lists/urls.py
+++ lists/urls.py
@@ -3,6 +3,6 @@ from lists import views
 
 urlpatterns = [
     url(r'^new$', views.NewListView.as_view(), name='new_list'),
-    url(r'^(\d+)/$', views.ViewAndAddToList.as_view(), name='view_list'),
+    url(r'^(?P<pk>\d+)/$', views.ViewAndAddToList.as_view(), name='view_list')
 ]
 
