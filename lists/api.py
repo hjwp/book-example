2c59b9fa491daf8142b3316ab1805193e6c761e3
Harry <hjwp2@cantab.net>
2017-02-14 11:12:01 +0000

    api now has post happy path. --ch36l005--

--- lists/api.py
+++ lists/api.py
@@ -5,6 +5,10 @@ from lists.models import List, Item
 
 def list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(list=list_, text=request.POST['text'])
+        return HttpResponse(status=201)
+
     item_dicts = [
         {'id': item.id, 'text': item.text}
         for item in list_.item_set.all()
