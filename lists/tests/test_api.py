4575c452be97ba24dc243d275ca8d378e8aaef16
Harry <hjwp2@cantab.net>
2017-02-14 13:50:54 +0000

    python unit tests for validation/error handling. --ch36l027--

--- lists/tests/test_api.py
+++ lists/tests/test_api.py
@@ -1,6 +1,7 @@
 import json
 from django.test import TestCase
 from lists.models import List, Item
+from lists.forms import DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR
 
 
 class ListAPITest(TestCase):
@@ -38,4 +39,37 @@ class ListAPITest(TestCase):
         self.assertEqual(response.status_code, 201)
         new_item = list_.item_set.get()
         self.assertEqual(new_item.text, 'new item')
+
+
+    def post_empty_input(self):
+        list_ = List.objects.create()
+        return self.client.post(
+            self.base_url.format(list_.id),
+            data={'text': ''}
+        )
+
+
+    def test_for_invalid_input_nothing_saved_to_db(self):
+        self.post_empty_input()
+        self.assertEqual(Item.objects.count(), 0)
+
+
+    def test_for_invalid_input_returns_error_code(self):
+        response = self.post_empty_input()
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            json.loads(response.content.decode('utf8')),
+            {'error': EMPTY_ITEM_ERROR}
+        )
+
+
+    def test_duplicate_items_error(self):
+        list_ = List.objects.create()
+        self.client.post(self.base_url.format(list_.id), data={'text': 'thing'})
+        response = self.client.post(self.base_url.format(list_.id), data={'text': 'thing'})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            json.loads(response.content.decode('utf8')),
+            {'error': DUPLICATE_ITEM_ERROR}
+        )
 
