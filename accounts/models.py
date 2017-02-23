c8cb50c785e61c7bc2ad05d6c0bbdcbeb4d33d6f
Harry <hjwp2@cantab.net>
2017-02-23 11:28:37 +0000

    make email field unique=True. --ch16l028-1--

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
 
     REQUIRED_FIELDS = []
     USERNAME_FIELD = 'email'
