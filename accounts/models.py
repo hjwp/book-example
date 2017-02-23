959b5e03c6111e0de9d1b7da5564039c6c6ce82b
Harry <hjwp2@cantab.net>
2017-02-23 11:31:13 +0000

    now email is primary key. --ch16l028-4--

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    email = models.EmailField(unique=True)
+    email = models.EmailField(primary_key=True)
 
     REQUIRED_FIELDS = []
     USERNAME_FIELD = 'email'
