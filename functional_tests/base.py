7bf704cfc8b9158ce9ea72e76083d8f57646deac
Harry <hjwp2@cantab.net>
2017-03-12 10:26:22 +0000

    Adjust teardown in FT runner to take screenshots etc. --ch21l006--

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,9 +1,11 @@
 import os
+from datetime import datetime
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
+
 from .server_tools import reset_database
 
 MAX_WAIT = 10
@@ -22,6 +24,11 @@ def wait(fn):
     return modified_fn
 
 
+SCREEN_DUMP_LOCATION = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'screendumps'
+)
+
+
 class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
@@ -31,8 +38,23 @@ class FunctionalTest(StaticLiveServerTestCase):
             setattr(self, 'live_server_url', 'http://' + self.staging_server)
             reset_database(self.staging_server)
 
+
     def tearDown(self):
+        if self._test_has_failed():
+            if not os.path.exists(SCREEN_DUMP_LOCATION):
+                os.makedirs(SCREEN_DUMP_LOCATION)
+            for ix, handle in enumerate(self.browser.window_handles):
+                self._windowid = ix
+                self.browser.switch_to_window(handle)
+                self.take_screenshot()
+                self.dump_html()
         self.browser.quit()
+        super().tearDown()
+
+
+    def _test_has_failed(self):
+        # slightly obscure but couldn't find a better way!
+        return any(error for (method, error) in self._outcome.errors)
 
 
     @wait
