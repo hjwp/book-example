3134c69d50c82fdbca3550e221e4dc860fc81c4a
Harry <hjwp2@cantab.net>
2017-03-12 12:07:25 +0000

    a couple of 3.6 bugfixes in fabfile

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -35,20 +35,20 @@ def _update_settings(source_folder, site_name):
     sed(settings_path, "DEBUG = True", "DEBUG = False")
     sed(settings_path,
         'ALLOWED_HOSTS =.+$',
-        f'ALLOWED_HOSTS = [{site_name!r}]'
+        f'ALLOWED_HOSTS = ["{site_name}"]'
     )
     secret_key_file = source_folder + '/superlists/secret_key.py'
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
-        append(secret_key_file, f"SECRET_KEY = {key!r}")
+        append(secret_key_file, f'SECRET_KEY = "{key}"')
     append(settings_path, '\nfrom .secret_key import SECRET_KEY')
 
 
 def _update_virtualenv(source_folder):
     virtualenv_folder = source_folder + '/../virtualenv'
     if not exists(virtualenv_folder + '/bin/pip'):
-        run(f'python -m venv {virtualenv_folder}')
+        run(f'python3.6 -m venv {virtualenv_folder}')
     run(f'{virtualenv_folder}/bin/pip install -r {source_folder}/requirements.txt')
 
 
@@ -62,6 +62,6 @@ def _update_static_files(source_folder):
 def _update_database(source_folder):
     run(
         f'cd {source_folder}'
-        ' && ../virtualenv/bin/python3 manage.py migrate --noinput'
+        ' && ../virtualenv/bin/python manage.py migrate --noinput'
     )
 
