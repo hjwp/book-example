7d2195cc1ad2716a234eb80f16c25d6ecb4791a5
Harry <hjwp2@cantab.net>
2017-03-28 00:42:09 +0100

    main fn calls a series of functions yet to be written. --ch09l001--

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,17 @@
+from fabric.contrib.files import append, exists, sed
+from fabric.api import env, local, run
+import random
+
+REPO_URL = 'https://github.com/hjwp/book-example.git'
+
+
+def deploy():
+    site_folder = f'/home/{env.user}/sites/{env.host}'
+    source_folder = site_folder + '/source'
+    _create_directory_structure_if_necessary(site_folder)
+    _get_latest_source(source_folder)
+    _update_settings(source_folder, env.host)
+    _update_virtualenv(source_folder)
+    _update_static_files(source_folder)
+    _update_database(source_folder)
+
