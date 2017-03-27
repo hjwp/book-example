d7cf30392c012cd20b692d9c826aa77afe2de85d
Harry <hjwp2@cantab.net>
2017-03-28 00:46:21 +0100

    migrate database. --ch09l007--

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -45,3 +45,7 @@ def _create_or_update_dotenv():
 def _update_static_files():
     run('./virtualenv/bin/python manage.py collectstatic --noinput')
 
+
+def _update_database():
+    run('./virtualenv/bin/python manage.py migrate --noinput')
+
