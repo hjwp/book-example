16691f137b0b57ef41a2150f89aadec3b0d4ac3c
Harry <hjwp2@cantab.net>
2017-04-03 23:07:37 +0100

    switch to using new drf api in urls.py --ch37l005--

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import include, url
 from accounts import urls as accounts_urls
 from lists import views as list_views
-from lists import api_urls
 from lists import urls as list_urls
+# from lists.api import urls as api_urls
+from lists.api import router
 
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
     url(r'^accounts/', include(accounts_urls)),
-    url(r'^api/', include(api_urls)),
+    # url(r'^api/', include(api_urls)),
+    url(r'^api/', include(router.urls)),
 ]
 
