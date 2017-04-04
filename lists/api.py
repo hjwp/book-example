8feb6ccd6e3136489c62c3be2cfe72fd20af8f03
Harry <hjwp2@cantab.net>
2017-04-04 17:24:42 +0100

    tidy-up unused stuff in api. --ch37l019--

--- lists/api.py
+++ lists/api.py
@@ -1,8 +1,5 @@
-import json
-from django.http import HttpResponse
 from lists.models import List, Item
 from lists.forms import (
-    ExistingListItemForm,
     EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR
 )
 from rest_framework import routers, serializers, viewsets
@@ -26,6 +23,7 @@ class ItemSerializer(serializers.ModelSerializer):
         ]
 
 
+
 class ListSerializer(serializers.ModelSerializer):
     items = ItemSerializer(many=True, source='item_set')
 
@@ -33,29 +31,6 @@ class ListSerializer(serializers.ModelSerializer):
         model = List
         fields = ('id', 'items',)
 
-
-
-def list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    if request.method == 'POST':
-        form = ExistingListItemForm(for_list=list_, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponse(status=201)
-        else:
-            return HttpResponse(
-                json.dumps({'error': form.errors['text'][0]}),
-                content_type='application/json',
-                status=400
-            )
-    item_dicts = [
-        {'id': item.id, 'text': item.text}
-        for item in list_.item_set.all()
-    ]
-    return HttpResponse(
-        json.dumps(item_dicts),
-        content_type='application/json'
-    )
 
 
 class ListViewSet(viewsets.ModelViewSet):
