6a860057bc76ad80d9525c46a47b5fa33f6e94f1
Harry <hjwp2@cantab.net>
2017-04-30 11:43:21 +0100

    update to 1.11 released

--- superlists/wsgi.py
+++ superlists/wsgi.py
@@ -4,7 +4,7 @@ WSGI config for superlists project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
