a66b962d02edd58a15b1950629f51ca4bda9a1c2
Harry <hjwp2@cantab.net>
2018-01-11 20:26:06 +0000

    fab function for create session (using .env vars). --ch18l019--

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -11,3 +11,17 @@ def reset_database(host):
     with settings(host_string=f'elspeth@{host}'):
         run(f'{manage_dot_py} flush --noinput')
 
+
+def _get_server_env_vars(host):
+    env_lines = run(f'cat ~/sites/{host}/.env').splitlines()
+    return dict(l.split('=') for l in env_lines if l)
+
+
+def create_session_on_server(host, email):
+    manage_dot_py = _get_manage_dot_py(host)
+    with settings(host_string=f'elspeth@{host}'):
+        env_vars = _get_server_env_vars(host)
+        with shell_env(**env_vars):
+            session_key = run(f'{manage_dot_py} create_session {email}')
+            return session_key.strip()
+
