022f3644e163105285877070cc1ccdcd42bd3c5d
Harry <hjwp2@cantab.net>
2018-01-15 17:03:07 +0000

    create dotenv file --ch09l005--

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -30,3 +30,14 @@ def _update_virtualenv():
         run(f'python3.6 -m venv virtualenv')
     run('./virtualenv/bin/pip install -r requirements.txt')
 
+
+def _create_or_update_dotenv():
+    append('.env', 'DJANGO_DEBUG_FALSE=y')
+    append('.env', f'SITENAME={env.host}')
+    current_contents = run('cat .env')
+    if 'DJANGO_SECRET_KEY' not in current_contents:
+        new_secret = ''.join(random.SystemRandom().choices(
+            'abcdefghijklmnopqrstuvwxyz0123456789', k=50
+        ))
+        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+
