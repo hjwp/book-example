d91d7f73a7a9bdd7b46725853fb5cd297f36b4c5
Serge Kuts <sergio.kuts@gmail.com>
2018-03-26 00:37:36 -0400

    Update functional_tests.py
    Fix the text in title and h1 to correspond the template text. Otherwise, the test fails.

--- functional_tests.py
+++ functional_tests.py
@@ -17,9 +17,9 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)
+        self.assertIn('To-Do lists', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
+        self.assertIn('Your To-Do list', header_text)
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
