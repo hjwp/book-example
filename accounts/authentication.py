1204ebb75508268f8d7124ff3c97340187678fe2
Pavel Druzhinin <34269347+paveldroo@users.noreply.github.com>
2021-04-19 14:22:23 +0300

    Update authentication.py
    Custom authentication doesn't work without request param
    See: https://stackoverflow.com/questions/63890340/typeerror-when-django-authenticate-function-tries-to-bind-to-custom-authenticate

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -2,7 +2,7 @@ from accounts.models import User, Token
 
 class PasswordlessAuthenticationBackend(object):
 
-    def authenticate(self, uid):
+    def authenticate(self, request, uid):
         try:
             token = Token.objects.get(uid=uid)
             return User.objects.get(email=token.email)
