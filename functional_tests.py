aa177cf6dd2c8f0dece297c7ba7a160852399f71
Harry <hjwp2@cantab.net>
2023-03-13 17:11:25 +0000

    black

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+browser.get("http://localhost:8000")
 
-assert 'Django' in browser.title
+assert "Django" in browser.title
--- superlists/wsgi.py
+++ superlists/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'superlists.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
 
 application = get_wsgi_application()
