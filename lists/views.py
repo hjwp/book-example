de087d6b09f84a389e707b5391ee30d09a84eead
Harry <hjwp2@cantab.net>
2023-03-13 17:43:23 +0000

    first cut view

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 
 # Create your views here.
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse("<html><title>To-Do lists</title></html>")
