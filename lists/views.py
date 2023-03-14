1f337b6af05dfc9c12e19500d1dfd54b68228422
Harry <hjwp2@cantab.net>
2023-03-14 10:43:01 +0000

    refactor view to use render. --ch04l003--

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse("<html><title>To-Do lists</title></html>")
+    return render(request, "home.html")
