ebb5fba7afa50a9632202518e2a36f778a9fcb8f
Harry <hjwp2@cantab.net>
2023-03-14 10:43:03 +0000

    use django test client

--- lists/tests.py
+++ lists/tests.py
@@ -11,9 +11,11 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get("/")
+
         html = response.content.decode("utf8")
         self.assertTrue(html.startswith("<html>"))
         self.assertIn("<title>To-Do lists</title>", html)
         self.assertTrue(html.strip().endswith("</html>"))
+
+        self.assertTemplateUsed(response, "home.html")
