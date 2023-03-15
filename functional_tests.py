79e82a5532f9c43a219cb075760a34c9370a7ba0
Harry <hjwp2@cantab.net>
2023-03-15 10:20:21 +0000

    Extend FT with better error messages, look for second row. --ch05l014--

--- functional_tests.py
+++ functional_tests.py
@@ -42,13 +42,27 @@ class NewVisitorTest(unittest.TestCase):
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
         # (Edith is very methodical)
-        self.fail("Finish the test!")
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(
+            "1: Buy peacock feathers",
+            [row.text for row in rows],
+        )
+        self.assertIn(
+            "2: Use peacock feathers to make a fly",
+            [row.text for row in rows],
+        )
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
+        self.fail("Finish the test!")
 
         # She visits that URL - her to-do list is still there.
 
