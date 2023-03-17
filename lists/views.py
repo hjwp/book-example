8e62fbd7df149f52635b9425cf05c86579c6572c
Harry <hjwp2@cantab.net>
2023-03-17 10:37:58 +0000

    intoduce if/else, view now works correctly. --ch05l023--

--- lists/views.py
+++ lists/views.py
@@ -3,12 +3,14 @@ from lists.models import Item
 
 
 def home_page(request):
-    item = Item()
-    item.text = request.POST.get("item_text", "")
-    item.save()
+    if request.method == "POST":
+        new_item_text = request.POST["item_text"]
+        Item.objects.create(text=new_item_text)
+    else:
+        new_item_text = ""
 
     return render(
         request,
         "home.html",
-        {"new_item_text": item.text},
+        {"new_item_text": new_item_text},
     )
