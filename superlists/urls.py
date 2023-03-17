c7e3f0f2595a0cf4ba25cd115ba3586ba967b424
Harry <hjwp2@cantab.net>
2023-03-17 12:05:20 +0000

    Cut down superlists/urls.py. --ch07l045--

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,9 +1,7 @@
-from django.urls import path
-from lists import views
+from django.urls import path, include
+from lists import views as list_views
 
 urlpatterns = [
-    path("", views.home_page, name="home"),
-    path("lists/new", views.new_list, name="new_list"),
-    path("lists/<int:list_id>/", views.view_list, name="view_list"),
-    path("lists/<int:list_id>/add_item", views.add_item, name="add_item"),
+    path("", list_views.home_page, name="home"),
+    path("lists/", include("lists.urls")),
 ]
