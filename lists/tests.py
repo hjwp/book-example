1fcdcb6984c4627e1afb735123d35e807fce6e1e
Harry <hjwp2@cantab.net>
2023-03-17 12:05:12 +0000

    Move tests to new class NewListTest. --ch07l021-1--

--- lists/tests.py
+++ lists/tests.py
@@ -7,21 +7,22 @@ class HomePageTest(TestCase):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
 
-    def test_can_save_a_POST_request(self):
-        self.client.post("/", data={"item_text": "A new list item"})
+    def test_only_saves_items_when_necessary(self):
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(), 0)
 
+
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        self.client.post("/lists/new", data={"item_text": "A new list item"})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, "A new list item")
 
     def test_redirects_after_POST(self):
-        response = self.client.post("/", data={"item_text": "A new list item"})
+        response = self.client.post("/lists/new", data={"item_text": "A new list item"})
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response["location"], "/lists/the-only-list-in-the-world/")
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get("/")
-        self.assertEqual(Item.objects.count(), 0)
 
 
 class ListViewTest(TestCase):
