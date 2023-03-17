1425bd643ba31486519a1ccb69bfbcdcecbd7d45
Harry <hjwp2@cantab.net>
2023-03-17 10:36:50 +0000

    little refactor in view use item.text. --ch05l021--

--- lists/views.py
+++ lists/views.py
@@ -10,5 +10,5 @@ def home_page(request):
     return render(
         request,
         "home.html",
-        {"new_item_text": request.POST.get("item_text", "")},
+        {"new_item_text": item.text},
     )
