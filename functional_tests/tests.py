302ef9125877c6b6f49359c5e638305874e16e3e
Harry <hjwp2@cantab.net>
2023-03-17 11:28:27 +0000

    Inherit from LiveServerTestCase. --ch06l001--

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -5,7 +6,7 @@ import time
 import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
