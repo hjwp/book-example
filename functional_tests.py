b0db651fe12005bcdb49ab799daa5ad8f6b12ad2
Harry <hjwp2@cantab.net>
2023-04-24 17:04:01 +0100

    add comments to ft. --ch02l001--

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,27 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
+
+# Edith has heard about a cool new online to-do app.
+# She goes to check out its homepage
 browser.get("http://localhost:8000")
 
-assert "Congratulations!" in browser.title
-print("OK")
+# She notices the page title and header mention to-do lists
+assert "To-Do" in browser.title
+
+# She is invited to enter a to-do item straight away
+
+# She types "Buy peacock feathers" into a text box
+# (Edith's hobby is tying fly-fishing lures)
+
+# When she hits enter, the page updates, and now the page lists
+# "1: Buy peacock feathers" as an item in a to-do list
+
+# There is still a text box inviting her to add another item.
+# She enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+# The page updates again, and now shows both items on her list
+
+# Satisfied, she goes back to sleep
+
+browser.quit()
