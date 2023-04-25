ae6ed24c58e5a825e02367c3933c59bed3e904bd
Harry <hjwp2@cantab.net>
2023-04-25 12:01:44 +0100

    first unit test for our view.  --ch03l003--

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
+from django.http import HttpRequest
+from lists.views import home_page
 
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode("utf8")
+        self.assertIn("<title>To-Do lists</title>", html)
+        self.assertTrue(html.startswith("<html>"))
+        self.assertTrue(html.endswith("</html>"))
