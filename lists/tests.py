ac46138a4a75ce809799eb7f554bcf7bbddf5425
Harry <hjwp2@cantab.net>
2023-04-26 12:40:46 +0100

    merge two unit tests into one that uses test client. --ch03l013--

--- lists/tests.py
+++ lists/tests.py
@@ -1,17 +1,9 @@
 from django.test import TestCase
-from django.http import HttpRequest
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode("utf8")
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.startswith("<html>"))
-        self.assertTrue(html.endswith("</html>"))
-
-    def test_home_page_returns_correct_html_2(self):
         response = self.client.get("/")
         self.assertContains(response, "<title>To-Do lists</title>")
+        self.assertContains(response, "<html>")
+        self.assertContains(response, "</html>")
