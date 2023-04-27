817ad5fbbf06c561cd55387e1f2490ba6502c388
Harry <hjwp2@cantab.net>
2023-04-27 16:14:01 +0100

    remove if branch from view, just pass post to template. --ch05l010--

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home_page(request):
-    if request.method == "POST":
-        return HttpResponse("You submitted: " + request.POST["item_text"])
-    return render(request, "home.html")
+    return render(
+        request,
+        "home.html",
+        {"new_item_text": request.POST["item_text"]},
+    )
