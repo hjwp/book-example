678b100fcc79aaf52556807ffaa49db1a3951447
Harry <hjwp2@cantab.net>
2023-04-27 17:28:47 +0100

    add test that we save to the db in view.  --ch05l025--

--- lists/tests.py
+++ lists/tests.py
@@ -9,6 +9,11 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
         self.assertContains(response, "A new list item")
         self.assertTemplateUsed(response, "home.html")
 
