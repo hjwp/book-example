0f0c95bf69f86fcff628489f74669b4202b06ae8
Harry <hjwp2@cantab.net>
2023-04-27 16:12:28 +0100

    check on template used. --ch05l009--

--- lists/tests.py
+++ lists/tests.py
@@ -9,3 +9,4 @@ class HomePageTest(TestCase):
     def test_can_save_a_POST_request(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
         self.assertContains(response, "A new list item")
+        self.assertTemplateUsed(response, "home.html")
