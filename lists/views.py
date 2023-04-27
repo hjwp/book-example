13990e46561532f12d62dd5d2dafd2c043b7c0ac
Harry <hjwp2@cantab.net>
2023-04-27 17:49:05 +0100

    naive item save for every request in view. --ch05l026--

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from lists.models import Item
 
 
 def home_page(request):
+    item = Item()
+    item.text = request.POST.get("item_text", "")
+    item.save()
+
     return render(
         request,
         "home.html",
