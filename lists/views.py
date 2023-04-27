5a1ba5219c84722fceb11f915629cfa4be58895f
Harry <hjwp2@cantab.net>
2023-04-27 15:24:41 +0100

    deliberately silly return on POST. --ch05l009--

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home_page(request):
+    if request.method == "POST":
+        return HttpResponse("You submitted: " + request.POST["item_text"])
     return render(request, "home.html")
