80a508532fec1b15fc981794186eec2fe5d1ee08
Harry <hjwp2@cantab.net>
2023-04-27 16:35:53 +0100

    use assertIn thanks Jason. --ch05l015--

--- functional_tests.py
+++ functional_tests.py
@@ -37,10 +37,7 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element(By.ID, "id_list_table")
         rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertTrue(
-            any(row.text == "1: Buy peacock feathers" for row in rows),
-            f"New to-do item did not appear in table. Contents were:\n{table.text}",
-        )
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
