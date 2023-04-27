4d4957a84a2f085134d40b052d2a70200e6bf112
Harry <hjwp2@cantab.net>
2023-04-27 15:21:36 +0100

    new unit test for saving POST request. --ch05l006--

--- lists/tests.py
+++ lists/tests.py
@@ -5,3 +5,7 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertContains(response, "A new list item")
