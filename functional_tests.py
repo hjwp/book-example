1b6cf2fcc0a5fcdd891404e5336e064619993ece
Harry <hjwp2@cantab.net>
2023-04-27 16:58:07 +0100

    Extend FT to check for second item. --ch05l017--

--- functional_tests.py
+++ functional_tests.py
@@ -42,9 +42,22 @@ class NewVisitorTest(unittest.TestCase):
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
         # (Edith is very methodical)
-        self.fail("Finish the test!")
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(
+            "2: Use peacock feathers to make a fly",
+            [row.text for row in rows],
+        )
+        self.assertIn(
+            "1: Buy peacock feathers",
+            [row.text for row in rows],
+        )
 
         # Satisfied, she goes back to sleep
 
