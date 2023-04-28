b33406a70fc01c719ae58183cb2a4e993ddbbe42
Harry <hjwp2@cantab.net>
2023-04-28 16:55:14 +0100

    refactor view to use Item.objects.create. --ch05l033--

--- lists/views.py
+++ lists/views.py
@@ -4,9 +4,7 @@ from lists.models import Item
 
 def home_page(request):
     if request.method == "POST":
-        item = Item()
-        item.text = request.POST["item_text"]
-        item.save()
+        Item.objects.create(text=request.POST["item_text"])
         return redirect("/")
 
     return render(
