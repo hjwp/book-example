e65463edccc73d37166e8ebceb3bf4f105ca2ee5
Harry <hjwp2@cantab.net>
2023-04-28 16:59:52 +0100

    split post tests into two.  --ch05l032--

--- lists/tests.py
+++ lists/tests.py
@@ -8,12 +8,13 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, "home.html")
 
     def test_can_save_a_POST_request(self):
-        response = self.client.post("/", data={"item_text": "A new list item"})
-
+        self.client.post("/", data={"item_text": "A new list item"})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, "A new list item")
 
+    def test_redirects_after_POST(self):
+        response = self.client.post("/", data={"item_text": "A new list item"})
         self.assertRedirects(response, "/")
 
     def test_only_saves_items_when_necessary(self):
