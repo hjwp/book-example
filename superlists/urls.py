28d1a1e3b2eb4168f225e6ba9460d09c5924b717
Harry <hjwp2@cantab.net>
2023-06-21 12:54:48 +0100

    switch out urls.py to point at new nonexistent view fn. --ch07l011--

--- superlists/urls.py
+++ superlists/urls.py
@@ -3,5 +3,5 @@ from lists import views
 
 urlpatterns = [
     path("", views.home_page, name="home"),
-    path("lists/the-only-list-in-the-world/", views.home_page, name="view_list"),
+    path("lists/the-only-list-in-the-world/", views.view_list, name="view_list"),
 ]
