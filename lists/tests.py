70b51da201405ecb0824349e2cf151fa8f10d3a7
Harry <hjwp2@cantab.net>
2023-06-21 12:54:20 +0100

    New unit test class ListViewTest, copied one test moved one test. --ch07l010--

--- lists/tests.py
+++ lists/tests.py
@@ -13,15 +13,6 @@ class HomePageTest(TestCase):
         self.assertContains(response, '<form method="POST">')
         self.assertContains(response, '<input name="item_text"')
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
-
-        response = self.client.get("/")
-
-        self.assertContains(response, "itemey 1")
-        self.assertContains(response, "itemey 2")
-
     def test_can_save_a_POST_request(self):
         self.client.post("/", data={"item_text": "A new list item"})
         self.assertEqual(Item.objects.count(), 1)
@@ -35,6 +26,22 @@ class HomePageTest(TestCase):
     def test_only_saves_items_when_necessary(self):
         self.client.get("/")
         self.assertEqual(Item.objects.count(), 0)
+
+
+class ListViewTest(TestCase):
+    def test_renders_input_form(self):
+        response = self.client.get("/lists/the-only-list-in-the-world/")
+        self.assertContains(response, '<form method="POST">')
+        self.assertContains(response, '<input name="item_text"')
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+
+        response = self.client.get("/lists/the-only-list-in-the-world/")
+
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
 
 
 class ItemModelTest(TestCase):
