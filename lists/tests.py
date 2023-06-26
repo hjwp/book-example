12dec0950dccbc4e6196ac8cc93d4c6bdb05fe0b
Harry <hjwp2@cantab.net>
2023-06-26 15:18:49 +0100

    new tests class NewListViewTests, move 3 tests across --ch07l020--

--- lists/tests.py
+++ lists/tests.py
@@ -13,19 +13,21 @@ class HomePageTest(TestCase):
         self.assertContains(response, '<form method="POST" action="/">')
         self.assertContains(response, '<input name="item_text"')
 
-    def test_can_save_a_POST_request(self):
-        self.client.post("/", data={"item_text": "A new list item"})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, "A new list item")
-
-    def test_redirects_after_POST(self):
-        response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
-
     def test_only_saves_items_when_necessary(self):
         self.client.get("/")
         self.assertEqual(Item.objects.count(), 0)
+
+
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        self.client.post("/lists/new", data={"item_text": "A new list item"})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.get()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_POST(self):
+        response = self.client.post("/lists/new", data={"item_text": "A new list item"})
+        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
 
 
 class ListViewTest(TestCase):
