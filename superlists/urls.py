133a56a2c8b4906d821997172940d6e4d3684fa5
Harry <hjwp2@cantab.net>
2023-06-26 15:20:42 +0100

    urls.py entry for /lists/new. --ch07l021--

--- superlists/urls.py
+++ superlists/urls.py
@@ -3,5 +3,6 @@ from lists import views
 
 urlpatterns = [
     path("", views.home_page, name="home"),
+    path("lists/new", views.new_list, name="new_list"),
     path("lists/the-only-list-in-the-world/", views.view_list, name="view_list"),
 ]
