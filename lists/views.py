f2a574ee90df8659bf2fb7687cb9a16be3fd3f99
Harry <hjwp2@cantab.net>
2023-06-26 07:20:14 +0100

    attempt to use nonexistent list.html template in view. --ch07l015--

--- lists/views.py
+++ lists/views.py
@@ -13,4 +13,4 @@ def home_page(request):
 
 def view_list(request):
     items = Item.objects.all()
-    return render(request, "home.html", {"items": items})
+    return render(request, "list.html", {"items": items})
