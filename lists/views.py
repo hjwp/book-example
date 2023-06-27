8fc48bd5cf3c3f4fbf80720bcc8929080bec27e0
Harry <hjwp2@cantab.net>
2023-06-27 21:38:10 +0100

    Pass just our list to the list template. --ch07l052--

--- lists/views.py
+++ lists/views.py
@@ -14,8 +14,7 @@ def new_list(request):
 
 def view_list(request, list_id):
     our_list = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=our_list)
-    return render(request, "list.html", {"items": items})
+    return render(request, "list.html", {"list": our_list})
 
 
 def add_item(request, list_id):
