d33e22852505a72e42a22ff69b85ba5a7eed1f84
Harry <hjwp2@cantab.net>
2023-06-27 21:25:33 +0100

    new urls.py entry for list/add_item. --ch07l046--

--- superlists/urls.py
+++ superlists/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.home_page, name="home"),
     path("lists/new", views.new_list, name="new_list"),
     path("lists/<int:list_id>/", views.view_list, name="view_list"),
+    path("lists/<int:list_id>/add_item", views.add_item, name="add_item"),
 ]
