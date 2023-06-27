b78fe61f0bde071f98825f8d827b96d80caa6bfd
Harry <hjwp2@cantab.net>
2023-06-27 21:17:22 +0100

    use new list id in redirect after post. --ch07l044--

--- lists/views.py
+++ lists/views.py
@@ -9,7 +9,7 @@ def home_page(request):
 def new_list(request):
     nulist = List.objects.create()
     Item.objects.create(text=request.POST["item_text"], list=nulist)
-    return redirect("/lists/the-only-list-in-the-world/")
+    return redirect(f"/lists/{nulist.id}/")
 
 
 def view_list(request, list_id):
