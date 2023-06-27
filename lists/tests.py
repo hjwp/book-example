252ca99c88f0081c657464f197f92dfe28c19e9f
Harry <hjwp2@cantab.net>
2023-06-27 21:23:33 +0100

    new test class for adding items to existing list. --ch07l045--

--- lists/tests.py
+++ lists/tests.py
@@ -41,6 +41,33 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, "other list item")
 
 
+class NewItemTest(TestCase):
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f"/lists/{correct_list.id}/add_item",
+            data={"item_text": "A new item for an existing list"},
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.get()
+        self.assertEqual(new_item.text, "A new item for an existing list")
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f"/lists/{correct_list.id}/add_item",
+            data={"item_text": "A new item for an existing list"},
+        )
+
+        self.assertRedirects(response, f"/lists/{correct_list.id}/")
+
+
 class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
         mylist = List()
