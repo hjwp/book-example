9877685c84a2c81a38bd586c4ff59f0a39829de0
Harry <hjwp2@cantab.net>
2023-06-27 21:46:19 +0100

    lists/urls.py has just existing-list specific urls. --ch07l058--

--- lists/urls.py
+++ lists/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from lists import views
 
 urlpatterns = [
-    path("", views.home_page, name="home"),
-    path("lists/new", views.new_list, name="new_list"),
-    path("lists/<int:list_id>/", views.view_list, name="view_list"),
-    path("lists/<int:list_id>/add_item", views.add_item, name="add_item"),
+    path("new", views.new_list, name="new_list"),
+    path("<int:list_id>/", views.view_list, name="view_list"),
+    path("<int:list_id>/add_item", views.add_item, name="add_item"),
 ]
