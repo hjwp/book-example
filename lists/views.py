4b5537af67b03b355c36e49d33a9a5dc647cd09e
Harry <hjwp2@cantab.net>
2023-06-27 20:51:03 +0100

    fix new_list view, tests pass again hooray. --ch07l039--

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 
-from lists.models import Item
+from lists.models import Item, List
 
 
 def home_page(request):
@@ -8,7 +8,8 @@ def home_page(request):
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST["item_text"])
+    nulist = List.objects.create()
+    Item.objects.create(text=request.POST["item_text"], list=nulist)
     return redirect("/lists/the-only-list-in-the-world/")
 
 
