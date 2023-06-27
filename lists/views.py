95dff862352202a84a2ee4cf46f1bf52909ad7d7
Harry <hjwp2@cantab.net>
2023-06-27 21:29:36 +0100

    actually save new item. --ch07l050--

--- lists/views.py
+++ lists/views.py
@@ -20,4 +20,5 @@ def view_list(request, list_id):
 
 def add_item(request, list_id):
     our_list = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST["item_text"], list=our_list)
     return redirect(f"/lists/{our_list.id}/")
