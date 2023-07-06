6cf8d3b26143d2fc6d8f77bd2c69f1edea5aefff
Harry <hjwp2@cantab.net>
2023-07-07 00:27:39 +0100

    possible alternative version of chapter 5

--- lists/tests.py
+++ lists/tests.py
@@ -5,3 +5,23 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
+
+    def test_post_saves_items(self):
+        self.client.post("/", data={"item_text": "onions"})
+        response1 = self.client.get("/")
+        self.assertContains(response1, "onions")
+
+    def test_multiple_posts_save_all_items(self):
+        self.client.post("/", data={"item_text": "onions"})
+        self.client.post("/", data={"item_text": "carrots"})
+        response = self.client.get("/")
+        self.assertContains(response, "onions")
+        self.assertContains(response, "carrots")
+
+    def test_no_items_by_default(self):
+        response = self.client.get("/")
+        self.assertContains(
+            response,
+            '<table id="id_list_table"></table>',
+            html=True,
+        )
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from lists.models import Item
 
 def home_page(request):
-    return render(request, "home.html")
+    if request.method == "POST":
+        Item.objects.create(text=request.POST["item_text"])
+
+    return render(
+        request,
+        "home.html",
+        {"items": Item.objects.all()},
+    )
