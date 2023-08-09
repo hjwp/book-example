8758d09ece27f5d9b8a6f15dfca0b9d0220df4a2
Harry <hjwp2@cantab.net>
2023-08-09 22:41:56 +0100

    set STATIC_ROOT. --ch08l019--

--- superlists/settings.py
+++ superlists/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
