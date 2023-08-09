6989af87a081c7079c88cf8d118321e398558a90
Harry <hjwp2@cantab.net>
2023-08-09 23:04:30 +0100

    disable django admin app.  --ch08l020--

--- superlists/settings.py
+++ superlists/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
+    # "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
