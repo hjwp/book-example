d6ec9adab439b3070b58824ed53732690fcd5da5
Harry <hjwp2@cantab.net>
2023-08-16 20:46:20 +0100

    staging server env var hack --ch09l001--

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,11 @@
+import os
+import time
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
-import time
 
 MAX_WAIT = 5
 
@@ -11,6 +13,8 @@ MAX_WAIT = 5
 class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        if test_server := os.environ.get("TEST_SERVER"):
+            self.live_server_url = "http://" + test_server
 
     def tearDown(self):
         self.browser.quit()
