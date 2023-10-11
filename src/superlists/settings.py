0d228da5a39869a7747cd24499d2b20acab91ad4
Harry <hjwp2@cantab.net>
2023-10-11 14:13:50 +0100

    change settings.py to use env vars to set DEBUG and SECRET_KEY --ch10l006--

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -10,20 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ql@n$n80=w%05@@(js+0@!jor(e&t)o4ed&6y%**9-e-lo4j_s"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if "DJANGO_DEBUG_FALSE" in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+else:
+    DEBUG = True
+    SECRET_KEY = "insecure-key-for-dev"
 
 ALLOWED_HOSTS = []
 
