45d589265d5c876b1f0389037bd40e049ac0e27e
Harry <hjwp2@cantab.net>
2023-10-11 14:14:42 +0100

    get ALLOWED_HOSTS from env var too. --ch10l008--

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -20,11 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 if "DJANGO_DEBUG_FALSE" in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+    ALLOWED_HOSTS = [os.environ["DJANGO_ALLOWED_HOST"]]
 else:
     DEBUG = True
     SECRET_KEY = "insecure-key-for-dev"
-
-ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = []
 
 
 # Application definition
