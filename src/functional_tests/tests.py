742e308b208990480b1811cbba14bdf573abf6da
Harry <hjwp2@cantab.net>
2023-10-25 18:38:34 +0100

    Create base FT class and a class for each test. --ch13l002--

--- src/functional_tests/tests.py
+++ src/functional_tests/tests.py
@@ -11,7 +11,7 @@ from selenium.webdriver.common.keys import Keys
 MAX_WAIT = 5
 
 
-class NewVisitorTest(StaticLiveServerTestCase):
+class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         test_server = os.environ.get("TEST_SERVER")
@@ -34,6 +34,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
                     raise
                 time.sleep(0.5)
 
+
+class NewVisitorTest(FunctionalTest):
     def test_can_start_a_todo_list(self):
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
@@ -114,6 +116,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Satisfied, they both go back to sleep
 
+
+class LayoutAndStylingTest(FunctionalTest):
     def test_layout_and_styling(self):
         # Edith goes to the home page,
         self.browser.get(self.live_server_url)
@@ -141,6 +145,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
             delta=10,
         )
 
+
+class ItemValidationTest(FunctionalTest):
     @skip
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
