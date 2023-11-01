beca5888980c8934d5f78cf78a4e589349562bc6
Harry <hjwp2@cantab.net>
2023-11-01 12:24:15 +0000

    Move tests for posting to existing list into listviewtest. --ch14l030--

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -55,7 +55,7 @@ class ListViewTest(TestCase):
         response = self.client.get(f"/lists/{mylist.id}/")
         parsed = lxml.html.fromstring(response.content)
         [form] = parsed.cssselect("form[method=POST]")
-        self.assertEqual(form.get("action"), f"/lists/{mylist.id}/add_item")
+        self.assertEqual(form.get("action"), f"/lists/{mylist.id}/")
         inputs = form.cssselect("input")
         self.assertIn("item_text", [input.get("name") for input in inputs])
 
@@ -72,14 +72,12 @@ class ListViewTest(TestCase):
         self.assertContains(response, "itemey 2")
         self.assertNotContains(response, "other list item")
 
-
-class NewItemTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
         self.client.post(
-            f"/lists/{correct_list.id}/add_item",
+            f"/lists/{correct_list.id}/",
             data={"item_text": "A new item for an existing list"},
         )
 
@@ -88,12 +86,12 @@ class NewItemTest(TestCase):
         self.assertEqual(new_item.text, "A new item for an existing list")
         self.assertEqual(new_item.list, correct_list)
 
-    def test_redirects_to_list_view(self):
+    def test_POST_redirects_to_list_view(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
         response = self.client.post(
-            f"/lists/{correct_list.id}/add_item",
+            f"/lists/{correct_list.id}/",
             data={"item_text": "A new item for an existing list"},
         )
 
