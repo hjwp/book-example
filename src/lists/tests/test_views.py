6bb5acd675211aa7af4f943175c92b6075e6862c
Harry <hjwp2@cantab.net>
2023-11-01 12:24:06 +0000

    test view passes validation errors to template. --ch14l006--

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -20,6 +20,13 @@ class NewListTest(TestCase):
         new_list = List.objects.get()
         self.assertRedirects(response, f"/lists/{new_list.id}/")
 
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post("/lists/new", data={"item_text": ""})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+        expected_error = "You can't have an empty list item"
+        self.assertContains(response, expected_error)
+
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
