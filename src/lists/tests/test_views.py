3389e8a132b28d0654c8a5f7476b18c2362f621f
Harry <hjwp2@cantab.net>
2023-11-01 12:24:13 +0000

    additional test that invalid input isnt saved for new lists. --ch14l015--

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -28,6 +28,11 @@ class NewListTest(TestCase):
         expected_error = escape("You can't have an empty list item")
         self.assertContains(response, expected_error)
 
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post("/lists/new", data={"item_text": ""})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
+
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
