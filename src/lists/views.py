337a00875409fdbb76bc5e49c7741723574e0690
Harry <hjwp2@cantab.net>
2023-11-01 12:24:27 +0000

    check validation in view_list. --ch14l037--

--- src/lists/views.py
+++ src/lists/views.py
@@ -22,7 +22,15 @@ def new_list(request):
 
 def view_list(request, list_id):
     our_list = List.objects.get(id=list_id)
+    error = None
+
     if request.method == "POST":
-        Item.objects.create(text=request.POST["item_text"], list=our_list)
-        return redirect(f"/lists/{our_list.id}/")
-    return render(request, "list.html", {"list": our_list})
+        try:
+            item = Item(text=request.POST["item_text"], list=our_list)
+            item.full_clean()
+            item.save()
+            return redirect(f"/lists/{our_list.id}/")
+        except ValidationError:
+            error = "You can't have an empty list item"
+
+    return render(request, "list.html", {"list": our_list, "error": error})
