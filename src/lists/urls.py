2035c5dbcd6da81ec0aa0f51b46c40719b1c7524
Harry <hjwp2@cantab.net>
2023-11-01 12:24:25 +0000

    Remove old add_item url. --ch14l034--

--- src/lists/urls.py
+++ src/lists/urls.py
@@ -4,5 +4,4 @@ from lists import views
 urlpatterns = [
     path("new", views.new_list, name="new_list"),
     path("<int:list_id>/", views.view_list, name="view_list"),
-    path("<int:list_id>/add_item", views.add_item, name="add_item"),
 ]
