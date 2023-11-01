c7a6860d4213ca5123c6c6a0b7ad0dff1322b681
Harry <hjwp2@cantab.net>
2023-11-01 12:24:10 +0000

    render home template with no error. --ch14l011--

--- src/lists/views.py
+++ src/lists/views.py
@@ -13,7 +13,7 @@ def new_list(request):
     try:
         item.full_clean()
     except ValidationError:
-        pass
+        return render(request, "home.html")
     return redirect(f"/lists/{nulist.id}/")
 
 
