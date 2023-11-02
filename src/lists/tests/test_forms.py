1cef98a61be5e4c94575aa2ac326296438784f13
Harry <hjwp2@cantab.net>
2023-11-02 09:54:26 +0000

    Form now uses modelform, custom placeholder.

--- src/lists/tests/test_forms.py
+++ src/lists/tests/test_forms.py
@@ -6,4 +6,4 @@ from lists.forms import ItemForm
 class ItemFormTest(TestCase):
     def test_form_renders_item_text_input(self):
         form = ItemForm()
-        self.fail(form.as_p())
+        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
