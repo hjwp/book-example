ce4a4e27a3e78dd004e2cdcbcb0fdc807da8b4bf
Harry <hjwp2@cantab.net>
2023-11-02 17:21:02 +0000

    refactor fts to use get_item_input_box. --ch11l020--

--- src/functional_tests/test_layout_and_styling.py
+++ src/functional_tests/test_layout_and_styling.py
@@ -13,7 +13,7 @@ class LayoutAndStylingTest(FunctionalTest):
         self.browser.set_window_size(1024, 768)
 
         # She notices the input box is nicely centered
-        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox = self.get_item_input_box()
         self.assertAlmostEqual(
             inputbox.location["x"] + inputbox.size["width"] / 2,
             512,
@@ -25,7 +25,7 @@ class LayoutAndStylingTest(FunctionalTest):
         inputbox.send_keys("testing")
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: testing")
-        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox = self.get_item_input_box()
         self.assertAlmostEqual(
             inputbox.location["x"] + inputbox.size["width"] / 2,
             512,
