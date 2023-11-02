97924299e945265f75492741dbc4cdf3a0e2bee6
Harry <hjwp2@cantab.net>
2023-11-02 17:21:02 +0000

    refactor fts to use get_item_input_box. --ch11l020--

--- src/functional_tests/test_list_item_validation.py
+++ src/functional_tests/test_list_item_validation.py
@@ -9,7 +9,7 @@ class ItemValidationTest(FunctionalTest):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
         self.browser.get(self.live_server_url)
-        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
@@ -21,12 +21,12 @@ class ItemValidationTest(FunctionalTest):
         )
 
         # She tries again with some text for the item, which now works
-        self.browser.find_element(By.ID, "id_new_item").send_keys("Purchase milk")
-        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys("Purchase milk")
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Purchase milk")
 
         # Perversely, she now decides to submit a second blank list item
-        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         # She receives a similar warning on the list page
         self.wait_for(
@@ -37,7 +37,7 @@ class ItemValidationTest(FunctionalTest):
         )
 
         # And she can correct it by filling some text in
-        self.browser.find_element(By.ID, "id_new_item").send_keys("Make tea")
-        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table("1: Purchase milk")
+        self.get_item_input_box().send_keys("Make tea")
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Purchase  milk")
         self.wait_for_row_in_list_table("2: Make tea")
