fd5d7e00a559de68d46fcb2b13df8e2fd6509d48
Harry <hjwp2@cantab.net>
2023-11-02 09:54:28 +0000

    Add bootstrap CSS classes to form widget

--- src/lists/tests/test_forms.py
+++ src/lists/tests/test_forms.py
@@ -4,6 +4,7 @@ from lists.forms import ItemForm
 
 
 class ItemFormTest(TestCase):
-    def test_form_renders_item_text_input(self):
+    def test_form_item_input_has_placeholder_and_css_classes(self):
         form = ItemForm()
         self.assertIn('placeholder="Enter a to-do item"', form.as_p())
+        self.assertIn('class="form-control form-control-lg"', form.as_p())
