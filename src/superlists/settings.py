d96d0590c82eca0308331aa5a69bfe4249274035
Harry <hjwp2@cantab.net>
2024-02-28 15:25:34 +0000

    Add basic logging config. --ch10l011--

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -123,3 +123,14 @@ STATIC_ROOT = BASE_DIR / "static"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "loggers": {
+        "root": {"handlers": ["console"], "level": "INFO"},
+    },
+}
