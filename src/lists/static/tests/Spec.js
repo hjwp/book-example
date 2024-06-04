b82ff08830a680c0978d9cc41315ca0cdaba1154
Harry <hjwp2@cantab.net>
2024-06-04 22:51:43 +0100

    beforeEach and afterEach HTML fixture --ch16l010--

--- src/lists/static/tests/Spec.js
+++ src/lists/static/tests/Spec.js
@@ -1,4 +1,28 @@
 describe("Superlists tests", () => {
+  let testDiv;
+
+  beforeEach(() => {
+    testDiv = document.createElement("div");
+    testDiv.innerHTML = `
+      <form>
+        <input
+          id="id_text"
+          name="text"
+          class="form-control form-control-lg is-invalid"
+          placeholder="Enter a to-do item"
+          value="Value as submitted"
+          aria-describedby="id_text_feedback"
+          required
+        />
+        <div id="id_text_feedback" class="invalid-feedback">An error message</div>
+      </form>
+    `;
+    document.body.appendChild(testDiv);
+  });
+
+  afterEach(() => {
+    testDiv.remove();
+  });
 
   it("smoke test", () => {
     expect(1 + 1).toEqual(2);
