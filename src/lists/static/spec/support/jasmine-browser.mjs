fc947c3e5a654a238eb042a3ba8d3c64b752b936
Harry <hjwp2@cantab.net>
2024-06-16 00:07:24 +0100

    vanialla jasmine browser runner support file config

---
+++ src/lists/static/spec/support/jasmine-browser.mjs
@@ -0,0 +1,30 @@
+export default {
+  srcDir: "src",
+  srcFiles: [
+    "**/*.js"
+  ],
+  specDir: "spec",
+  specFiles: [
+    "**/*[sS]pec.js"
+  ],
+  helpers: [
+    "helpers/**/*.js"
+  ],
+  env: {
+    stopSpecOnExpectationFailure: false,
+    stopOnSpecFailure: false,
+    random: true
+  },
+
+  // For security, listen only to localhost. You can also specify a different
+  // hostname or IP address, or remove the property or set it to "*" to listen
+  // to all network interfaces.
+  listenAddress: "localhost",
+
+  // The hostname that the browser will use to connect to the server.
+  hostname: "localhost",
+
+  browser: {
+    name: "firefox"
+  }
+};
