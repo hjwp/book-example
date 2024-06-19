19911513b51be9208212281ee29261c7fe617252
Harry <hjwp2@cantab.net>
2024-06-19 17:13:48 +0100

    Deliberately break lists.js, make it hide immediately. --ch16l018--

--- src/lists/static/lists.js
+++ src/lists/static/lists.js
@@ -1,9 +1,7 @@
-console.log("lists.js loading");
-var initialize = () => {
-  console.log("initialize called");
-  const textInput = document.querySelector("#id_text");
-  textInput.oninput = () => {
+const initialize = () => {
+  // const textInput = document.querySelector("#id_text");
+  // textInput.oninput = () => {
     const errorMsg = document.querySelector(".invalid-feedback");
     errorMsg.style.display = "none";
-  };
+  // };
 };
