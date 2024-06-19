9b52e5e04e7e9fc289efdebf4726b8d08a5db18d
Harry <hjwp2@cantab.net>
2024-06-19 17:07:29 +0100

    initialize() fn in lists.js --ch16l017--

--- src/lists/static/lists.js
+++ src/lists/static/lists.js
@@ -1,6 +1,9 @@
 console.log("lists.js loading");
-const textInput = document.querySelector("#id_text");
-textInput.oninput = () => {
-  const errorMsg = document.querySelector(".invalid-feedback");
-  errorMsg.style.display = "none";
+var initialize = () => {
+  console.log("initialize called");
+  const textInput = document.querySelector("#id_text");
+  textInput.oninput = () => {
+    const errorMsg = document.querySelector(".invalid-feedback");
+    errorMsg.style.display = "none";
+  };
 };
