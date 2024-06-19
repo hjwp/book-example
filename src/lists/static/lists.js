20f5592ab26cf392995a1dcf1d01a93b960137f8
Harry <hjwp2@cantab.net>
2024-06-19 18:31:37 +0100

    Try to use css class remove in impl. --ch16l029--

--- src/lists/static/lists.js
+++ src/lists/static/lists.js
@@ -1,7 +1,6 @@
 const initialize = (inputSelector, errorSelector) => {
   const textInput = document.querySelector(inputSelector);
   textInput.oninput = () => {
-    const errorMsg = document.querySelector(errorSelector);
-    errorMsg.style.display = "none";
+    textInput.classList.remove("is-invalid");
   };
 };
