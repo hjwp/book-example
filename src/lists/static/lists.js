448b6fa5ad5232f5369d5595547c4546ff465475
Harry <hjwp2@cantab.net>
2024-06-19 17:01:49 +0100

    Add console.logs to lists.js. --ch16l016--

--- src/lists/static/lists.js
+++ src/lists/static/lists.js
@@ -1,5 +1,6 @@
+console.log("lists.js loading");
 const textInput = document.querySelector("#id_text");
 textInput.oninput = () => {
   const errorMsg = document.querySelector(".invalid-feedback");
   errorMsg.style.display = "none";
-}
+};
