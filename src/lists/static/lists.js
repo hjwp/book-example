c423c2e1290653e82cd605e2d4fc8f9c01841e0b
Harry <hjwp2@cantab.net>
2024-06-19 18:07:17 +0100

    Use selectors passed in. --ch16l025--

--- src/lists/static/lists.js
+++ src/lists/static/lists.js
@@ -1,7 +1,7 @@
-const initialize = () => {
-  const textInput = document.querySelector("#id_text");
+const initialize = (inputSelector, errorSelector) => {
+  const textInput = document.querySelector(inputSelector);
   textInput.oninput = () => {
-    const errorMsg = document.querySelector(".invalid-feedback");
+    const errorMsg = document.querySelector(errorSelector);
     errorMsg.style.display = "none";
   };
 };
