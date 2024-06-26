4cd57cbb2bdc513e009e0ac8db65419347e02f0e
Harry <hjwp2@cantab.net>
2024-06-26 16:59:50 +0100

    SPIKE: authentication 'backend'. --ch19l012--

---
+++ src/accounts/authentication.py
@@ -0,0 +1,25 @@
+import sys
+
+from accounts.models import ListUser, Token
+
+from django.contrib.auth.backends import BaseBackend
+
+
+class PasswordlessAuthenticationBackend(BaseBackend):
+    def authenticate(self, request, uid):
+        print("uid", uid, file=sys.stderr)
+        if not Token.objects.filter(uid=uid).exists():
+            print("no token found", file=sys.stderr)
+            return None
+        token = Token.objects.get(uid=uid)
+        print("got token", file=sys.stderr)
+        try:
+            user = ListUser.objects.get(email=token.email)
+            print("got user", file=sys.stderr)
+            return user
+        except ListUser.DoesNotExist:
+            print("new user", file=sys.stderr)
+            return ListUser.objects.create(email=token.email)
+
+    def get_user(self, email):
+        return ListUser.objects.get(email=email)
