ba659eb80cf06cdc699cdeaa1982a639ca9813ed
Harry <hjwp2@cantab.net>
2024-06-26 16:55:02 +0100

    SPIKE: Custom User Manager thingie. --ch18l010--

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -11,6 +11,14 @@ class Token(models.Model):
     uid = models.CharField(max_length=255)
 
 
+class ListUserManager(BaseUserManager):
+    def create_user(self, email):
+        ListUser.objects.create(email=email)
+
+    def create_superuser(self, email, password):
+        self.create_user(email)
+
+
 class ListUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(primary_key=True)
     USERNAME_FIELD = "email"
