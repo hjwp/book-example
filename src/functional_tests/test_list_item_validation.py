7b18e5c85e16fd523f2e3a9e0d2c81ac56c9588c
Csanad Beres <qed.csberes@gmail.com>
2024-06-26 16:33:53 +0200

    add FT preventing duplicate items --ch15l001--

--- src/functional_tests/test_list_item_validation.py
+++ src/functional_tests/test_list_item_validation.py
@@ -46,3 +46,22 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Purchase milk")
         self.wait_for_row_in_list_table("2: Make tea")
+
+    def test_cannot_add_duplicate_items(self):
+        # Edith goes to the home page and starts a new list
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys("Buy wellies")
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy wellies")
+
+        # She accidentally tries to enter a duplicate item
+        self.get_item_input_box().send_keys("Buy wellies")
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # She sees a helpful error message
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element(By.CSS_SELECTOR, ".invalid-feedback").text,
+                "You've already got this in your list",
+            )
+        )
