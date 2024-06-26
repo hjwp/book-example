a2fe4ded1a690a93cbb280daa08694a2d51f7cc7
Harry <hjwp2@cantab.net>
2024-06-26 17:52:36 +0100

    Revert Spike. --ch19l019--

--- src/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from accounts import views
-
-urlpatterns = [
-    path("send_login_email", views.send_login_email, name="send_login_email"),
-    path("login", views.login, name="login"),
-    path("logout", views.logout, name="logout"),
-]
