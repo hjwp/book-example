0a973cfd2da969f2de1d1bf51f88e2158bfd2699
Harry <hjwp2@cantab.net>
2024-06-26 23:05:09 +0100

    Test email is pk. --ch18l032--

--- src/accounts/tests/test_models.py
+++ src/accounts/tests/test_models.py
@@ -8,3 +8,7 @@ class UserModelTest(TestCase):
     def test_user_is_valid_with_email_only(self):
         user = User(email="a@b.com")
         user.full_clean()  # should not raise
+
+    def test_email_is_primary_key(self):
+        user = User(email="a@b.com")
+        self.assertEqual(user.pk, "a@b.com")
