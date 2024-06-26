09f854f8b908f521dbf28cd8b65bf41ae9f8cf60
Harry <hjwp2@cantab.net>
2024-06-26 22:16:14 +0100

    First test for custom user model. --ch18l023--

---
+++ src/accounts/tests/test_models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+User = get_user_model()
+
+
+class UserModelTest(TestCase):
+    def test_user_is_valid_with_email_only(self):
+        user = User(email="a@b.com")
+        user.full_clean()  # should not raise
