f990f8d4f39f50ad3aa78ab5438b376fde91c581
Harry <hjwp2@cantab.net>
2024-06-26 16:35:22 +0100

    SPIKE: view to send login email. --ch19l005--

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,22 @@
+import sys
+import uuid
+
+from django.core.mail import send_mail
 from django.shortcuts import render
 
-# Create your views here.
+from accounts.models import Token
+
+
+def send_login_email(request):
+    email = request.POST["email"]
+    uid = str(uuid.uuid4())
+    Token.objects.create(email=email, uid=uid)
+    print("saving uid", uid, "for email", email, file=sys.stderr)
+    url = request.build_absolute_uri(f"/accounts/login?uid={uid}")
+    send_mail(
+        "Your login link for Superlists",
+        f"Use this link to log in:\n\n{url}",
+        "noreply@superlists",
+        [email],
+    )
+    return render(request, "login_email_sent.html")
