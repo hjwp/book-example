2c548b41d3f9121e9120c38b8376c4340da124a9
Harry <hjwp2@cantab.net>
2024-06-26 22:49:06 +0100

    Add accounts and auth_user_model to settings.py --ch18l026--

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "lists",
+    "accounts",
 ]
+
+AUTH_USER_MODEL = "accounts.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
