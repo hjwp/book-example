5e650ebfd501fda8b4c3d034ffdbbc0929580972
Harry <hjwp2@cantab.net>
2024-06-26 17:02:07 +0100

    SPIKE: login+logout to urls.py. --ch18l014--

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -4,4 +4,6 @@ from accounts import views
 
 urlpatterns = [
     path("send_email", views.send_login_email, name="send_login_email"),
+    path("login", views.login, name="login"),
+    path("logout", views.logout, name="logout"),
 ]
