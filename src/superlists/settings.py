dcb91d286edd9a39f4cbcfa5708587b1daaedacf
Harry <hjwp2@cantab.net>
2024-06-26 16:44:05 +0100

    SPIKE: settings for email. --ch18l007--

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -134,3 +134,9 @@ LOGGING = {
         "root": {"handlers": ["console"], "level": "INFO"},
     },
 }
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "obeythetestinggoat@gmail.com"
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
