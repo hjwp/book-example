c362cc0d58537d7d84e08c3644814c3e63ad1357
Csanad Beres <qed.csberes@gmail.com>
2024-06-29 22:17:20 +0200

    add unique constraint to Item per List --ch15l005--

--- src/lists/models.py
+++ src/lists/models.py
@@ -8,5 +8,8 @@ class List(models.Model):
 
 
 class Item(models.Model):
-    text = models.TextField(default="", unique=True)
+    text = models.TextField(default="")
     list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("list", "text")
