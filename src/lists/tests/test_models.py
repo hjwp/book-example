e79a88636d29e068d37ad0d98d81503f570680c4
Csanad Beres <qed.csberes@gmail.com>
2024-06-29 22:23:51 +0200

    simplify ListAndItemModelsTest --ch15l006--

--- src/lists/tests/test_models.py
+++ src/lists/tests/test_models.py
@@ -6,31 +6,16 @@ from lists.models import Item, List
 
 
 class ListAndItemModelsTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        mylist = List()
-        mylist.save()
-        first_item = Item()
-        first_item.text = "The first (ever) list item"
-        first_item.list = mylist
-        first_item.save()
+    def test_default_text(self):
+        item = Item()
+        self.assertEqual(item.text, "")
 
-        second_item = Item()
-        second_item.text = "Item the second"
-        second_item.list = mylist
-        second_item.save()
-
-        saved_list = List.objects.get()
-        self.assertEqual(saved_list, mylist)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "The first (ever) list item")
-        self.assertEqual(first_saved_item.list, mylist)
-        self.assertEqual(second_saved_item.text, "Item the second")
-        self.assertEqual(second_saved_item.list, mylist)
+    def test_item_is_related_to_list(self):
+        mylist = List.objects.create()
+        item = Item()
+        item.list = mylist
+        item.save()
+        self.assertIn(item, mylist.item_set.all())
 
     def test_cannot_save_null_list_items(self):
         mylist = List.objects.create()
