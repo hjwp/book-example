021e0b1ed1a46717ec70d1bd1072691e26cf095e
Csanad Beres <qed.csberes@gmail.com>
2024-06-30 22:03:11 +0200

    test duplicate item validation errors end up on lists page --ch15l009--

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -119,3 +119,16 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        Item.objects.create(list=list1, text="textey")
+        response = self.client.post(
+            f"/lists/{list1.id}/",
+            data={"text": "textey"},
+        )
+
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, "list.html")
+        self.assertEqual(Item.objects.all().count(), 1)
