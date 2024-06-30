a6036a6bce52148ac85a857f1b89caeb2d4306ad
Csanad Beres <qed.csberes@gmail.com>
2024-06-30 22:24:45 +0200

    skip test_duplicate_item_validation_errors_end_up_on_lists_page --ch16l010--

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -1,3 +1,5 @@
+from unittest import skip
+
 import lxml.html
 from django.test import TestCase
 from django.utils import html
@@ -119,6 +121,7 @@ class ListViewTest(TestCase):
         response = self.post_invalid_input()
         self.assertContains(response, html.escape(EMPTY_ITEM_ERROR))
 
+    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         Item.objects.create(list=list1, text="textey")
