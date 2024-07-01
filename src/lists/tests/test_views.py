b9abc9876e6ecd533edc580a25ab92acc8ce42f0
Csanad Beres <qed.csberes@gmail.com>
2024-07-01 18:27:11 +0200

    remove skip from test_duplicate_item_validation_errors_end_up_on_lists_page --ch15l014--

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -1,8 +1,12 @@
-from unittest import skip
 from django.test import TestCase
 from django.utils import html
 
-from lists.forms import EMPTY_ITEM_ERROR, ItemForm
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR,
+    EMPTY_ITEM_ERROR,
+    ExistingListItemForm,
+    ItemForm,
+)
 from lists.models import Item, List
 
 
@@ -121,7 +125,6 @@ class ListViewTest(TestCase):
         response = self.post_invalid_input()
         self.assertContains(response, html.escape(EMPTY_ITEM_ERROR))
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         Item.objects.create(list=list1, text="textey")
@@ -131,7 +134,7 @@ class ListViewTest(TestCase):
             data={"text": "textey"},
         )
 
-        expected_error = html.escape("You've already got this in your list")
+        expected_error = html.escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, "list.html")
         self.assertEqual(Item.objects.all().count(), 1)
