bbae7eb1345addb445625b37cc113e9503ef0715
Csanad Beres <qed.csberes@gmail.com>
2024-07-02 02:59:10 +0200

    test form save --ch15l017--

--- src/lists/tests/test_forms.py
+++ src/lists/tests/test_forms.py
@@ -20,6 +20,12 @@ class ItemFormTest(TestCase):
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors["text"], [EMPTY_ITEM_ERROR])
 
+    def test_form_save(self):
+        mylist = List.objects.create()
+        form = ExistingListItemForm(for_list=mylist, data={"text": "hi"})
+        new_item = form.save()
+        self.assertEqual(new_item, Item.objects.all()[0])
+
     def test_form_save_handles_saving_to_a_list(self):
         mylist = List.objects.create()
         form = ItemForm(data={"text": "do me"})
