1d70b59ad4acc083ab189add2291231ebd2cc3b3
Harry <hjwp2@cantab.net>
2024-07-04 06:19:07 +0100

    tests that we use the token when sending login email. --ch19l021--

--- src/accounts/tests/test_views.py
+++ src/accounts/tests/test_views.py
@@ -2,6 +2,8 @@ from unittest import mock
 
 from django.test import TestCase
 
+from accounts.models import Token
+
 
 class SendLoginEmailViewTest(TestCase):
     def test_redirects_to_home_page(self):
@@ -35,6 +37,24 @@ class SendLoginEmailViewTest(TestCase):
         self.assertEqual(subject, "Your login link for Superlists")
         self.assertEqual(from_email, "noreply@superlists")
         self.assertEqual(to_list, ["edith@example.com"])
+
+    def test_creates_token_associated_with_email(self):
+        self.client.post(
+            "/accounts/send_login_email", data={"email": "edith@example.com"}
+        )
+        token = Token.objects.get()
+        self.assertEqual(token.email, "edith@example.com")
+
+    @mock.patch("accounts.views.send_mail")
+    def test_sends_link_to_login_using_token_uid(self, mock_send_mail):
+        self.client.post(
+            "/accounts/send_login_email", data={"email": "edith@example.com"}
+        )
+
+        token = Token.objects.get()
+        expected_url = f"http://testserver/accounts/login?token={token.uid}"
+        (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+        self.assertIn(expected_url, body)
 
 
 class LoginViewTest(TestCase):
