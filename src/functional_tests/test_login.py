3190ca48ca880f0581a348c37c29071ddb8b158a
Harry <hjwp2@cantab.net>
2024-07-04 06:19:11 +0100

    extend ft to test log out. --ch19l048--

--- src/functional_tests/test_login.py
+++ src/functional_tests/test_login.py
@@ -50,3 +50,13 @@ class LoginTest(FunctionalTest):
         )
         navbar = self.browser.find_element(By.CSS_SELECTOR, ".navbar")
         self.assertIn(TEST_EMAIL, navbar.text)
+
+        # Now she logs out
+        self.browser.find_element(By.CSS_SELECTOR, "#id_logout").click()
+
+        # She is logged out
+        self.wait_for(
+            lambda: self.browser.find_element(By.CSS_SELECTOR, "input[name=email]")
+        )
+        navbar = self.browser.find_element(By.CSS_SELECTOR, ".navbar")
+        self.assertNotIn(TEST_EMAIL, navbar.text)
