a671710bf750376ac8faedfc1fa2291a3449250c
Harry <hjwp2@cantab.net>
2024-07-04 06:19:10 +0100

    final test for login, move patch to class level. --ch19l044--

--- src/accounts/tests/test_views.py
+++ src/accounts/tests/test_views.py
@@ -57,12 +57,12 @@ class SendLoginEmailViewTest(TestCase):
         self.assertIn(expected_url, body)
 
 
+@mock.patch("accounts.views.auth")
 class LoginViewTest(TestCase):
-    def test_redirects_to_home_page(self):
+    def test_redirects_to_home_page(self, mock_auth):
         response = self.client.get("/accounts/login?token=abcd123")
         self.assertRedirects(response, "/")
 
-    @mock.patch("accounts.views.auth")
     def test_calls_authenticate_with_uid_from_get_request(self, mock_auth):
         self.client.get("/accounts/login?token=abcd123")
         self.assertEqual(
@@ -70,10 +70,14 @@ class LoginViewTest(TestCase):
             mock.call(uid="abcd123"),
         )
 
-    @mock.patch("accounts.views.auth")
     def test_calls_auth_login_with_user_if_there_is_one(self, mock_auth):
         response = self.client.get("/accounts/login?token=abcd123")
         self.assertEqual(
             mock_auth.login.call_args,
             mock.call(response.wsgi_request, mock_auth.authenticate.return_value),
         )
+
+    def test_does_not_login_if_user_is_not_authenticated(self, mock_auth):
+        mock_auth.authenticate.return_value = None
+        self.client.get("/accounts/login?token=abcd123")
+        self.assertEqual(mock_auth.login.called, False)
