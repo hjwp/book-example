bff90661f303a61bbcf966e91af754cbbc442728
Harry <hjwp2@cantab.net>
2024-07-04 06:19:09 +0100

    Start on mocky tests for login view. --ch19l037--

--- src/accounts/tests/test_views.py
+++ src/accounts/tests/test_views.py
@@ -61,3 +61,11 @@ class LoginViewTest(TestCase):
     def test_redirects_to_home_page(self):
         response = self.client.get("/accounts/login?token=abcd123")
         self.assertRedirects(response, "/")
+
+    @mock.patch("accounts.views.auth")
+    def test_calls_authenticate_with_uid_from_get_request(self, mock_auth):
+        self.client.get("/accounts/login?token=abcd123")
+        self.assertEqual(
+            mock_auth.authenticate.call_args,
+            mock.call(uid="abcd123"),
+        )
