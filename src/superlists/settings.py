811678d5e7afea6986aff98c3577e583aab015ff
Harry <hjwp2@cantab.net>
2024-07-04 06:19:09 +0100

    add auth backend to settings. --ch20l001--

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
 ]
 
 AUTH_USER_MODEL = "accounts.User"
+AUTHENTICATION_BACKENDS = [
+    "accounts.authentication.PasswordlessAuthenticationBackend",
+]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
