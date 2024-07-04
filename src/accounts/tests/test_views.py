957eca5ec050d9ec855cf0024bc6502dc41232fe
Harry <hjwp2@cantab.net>
2024-07-04 06:19:03 +0100

    Mock out send_mail manually. --ch20l005--

--- src/accounts/tests/test_views.py
+++ src/accounts/tests/test_views.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+import accounts.views
+
 
 class SendLoginEmailViewTest(TestCase):
     def test_redirects_to_home_page(self):
@@ -7,3 +9,24 @@ class SendLoginEmailViewTest(TestCase):
             "/accounts/send_login_email", data={"email": "edith@example.com"}
         )
         self.assertRedirects(response, "/")
+
+    def test_sends_mail_to_address_from_post(self):
+        self.send_mail_called = False
+
+        def fake_send_mail(subject, body, from_email, to_list):
+            self.send_mail_called = True
+            self.subject = subject
+            self.body = body
+            self.from_email = from_email
+            self.to_list = to_list
+
+        accounts.views.send_mail = fake_send_mail
+
+        self.client.post(
+            "/accounts/send_login_email", data={"email": "edith@example.com"}
+        )
+
+        self.assertTrue(self.send_mail_called)
+        self.assertEqual(self.subject, "Your login link for Superlists")
+        self.assertEqual(self.from_email, "noreply@superlists")
+        self.assertEqual(self.to_list, ["edith@example.com"])
