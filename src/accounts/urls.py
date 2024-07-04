d19dbb66bca7295943232695b89347bcfc4504e8
Harry <hjwp2@cantab.net>
2024-07-04 06:19:11 +0100

    log out url, view imported from contrib. --ch20l026--

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,7 +1,10 @@
-from django.urls import include, path
+from django.contrib.auth import views as auth_views
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("send_login_email", views.send_login_email, name="send_login_email"),
     path("login", views.login, name="login"),
+    path("logout", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
 ]
