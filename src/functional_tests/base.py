8892d660a61048cb94eb3fc685039b16ab331ee3
Harry <hjwp2@cantab.net>
2024-07-20 23:03:46 +0100

    Replace explict waits with an imagined decorator. --ch20l005--

--- src/functional_tests/base.py
+++ src/functional_tests/base.py
@@ -28,32 +28,22 @@ class FunctionalTest(StaticLiveServerTestCase):
                     raise
                 time.sleep(0.5)
 
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element(By.ID, "id_list_table")
-                rows = table.find_elements(By.TAG_NAME, "tr")
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException):
-                if time.time() - start_time > MAX_WAIT:
-                    raise
-                time.sleep(0.5)
-
     def get_item_input_box(self):
         return self.browser.find_element(By.ID, "id_text")
 
+    @wait
+    def wait_for_row_in_list_table(self, row_text):
+        rows = self.browser.find_elements(By.CSS_SELECTOR, "#id_list_table tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
+    @wait
     def wait_to_be_logged_in(self, email):
-        self.wait_for(
-            lambda: self.browser.find_element(By.CSS_SELECTOR, "#id_logout"),
-        )
+        self.browser.find_element(By.CSS_SELECTOR, "#id_logout")
         navbar = self.browser.find_element(By.CSS_SELECTOR, ".navbar")
         self.assertIn(email, navbar.text)
 
+    @wait
     def wait_to_be_logged_out(self, email):
-        self.wait_for(
-            lambda: self.browser.find_element(By.CSS_SELECTOR, "input[name=email]")
-        )
+        self.browser.find_element(By.CSS_SELECTOR, "input[name=email]")
         navbar = self.browser.find_element(By.CSS_SELECTOR, ".navbar")
         self.assertNotIn(email, navbar.text)
