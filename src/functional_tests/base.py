4ebca0a534fe0e48f368a1a3176aa749f8150565
Harry <hjwp2@cantab.net>
2024-07-20 23:03:46 +0100

    A first attempt at a decorator to do a wait on any fn. --ch20l006--

--- src/functional_tests/base.py
+++ src/functional_tests/base.py
@@ -8,6 +8,19 @@ from selenium.common.exceptions import WebDriverException
 MAX_WAIT = 5
 
 
+def wait(fn):
+    def modified_fn():
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
+
+
 class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
