065828ba79ddce810f1f31f201e3c91f36df3420
Harry <hjwp2@cantab.net>
2024-07-20 23:03:45 +0100

    Helper method for checking login/logout status in ft base. --ch20l002--

--- src/functional_tests/base.py
+++ src/functional_tests/base.py
@@ -43,3 +43,15 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def get_item_input_box(self):
         return self.browser.find_element(By.ID, "id_text")
+
+    def wait_to_be_logged_in(self, email):
+        self.wait_for(lambda: self.browser.find_element(By.LINK_TEXT, "Log out"))
+        navbar = self.browser.find_element(By.CSS_SELECTOR, ".navbar")
+        self.assertIn(email, navbar.text)
+
+    def wait_to_be_logged_out(self, email):
+        self.wait_for(
+            lambda: self.browser.find_element(By.CSS_SELECTOR, "input[name=email]")
+        )
+        navbar = self.browser.find_element(By.CSS_SELECTOR, ".navbar")
+        self.assertNotIn(email, navbar.text)
