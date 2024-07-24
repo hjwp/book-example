3221386906b2ade00d2517faccc5614449d11f3e
Harry <hjwp2@cantab.net>
2024-07-24 10:56:16 +0200

    URL for my lists. --ch19l004--

--- lists/urls.py
+++ lists/urls.py
@@ -4,4 +4,5 @@ from lists import views
 urlpatterns = [
     url(r"^new$", views.new_list, name="new_list"),
     url(r"^(\d+)/$", views.view_list, name="view_list"),
+    url(r"^users/(.+)/$", views.my_lists, name="my_lists"),
 ]
