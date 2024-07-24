05f04189d22de1cce6fceda67c0803deb5dd5807
Harry <hjwp2@cantab.net>
2024-07-24 10:55:58 +0200

    Start on first real version of my lists FT. --ch22l001--

--- src/functional_tests/test_my_lists.py
+++ src/functional_tests/test_my_lists.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from selenium.webdriver.common.by import By
 
 from .base import FunctionalTest
 from .management.commands.create_session import create_pre_authenticated_session
@@ -24,11 +25,24 @@ class MyListsTest(FunctionalTest):
         )
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
-        email = "edith@example.com"
-        self.browser.get(self.live_server_url)
-        self.wait_to_be_logged_out(email)
-
         # Edith is a logged-in user
-        self.create_pre_authenticated_session(email)
+        self.create_pre_authenticated_session("edith@example.com")
+
+        # She goes to the home page and starts a list
         self.browser.get(self.live_server_url)
-        self.wait_to_be_logged_in(email)
+        self.add_list_item("Reticulate splines")
+        self.add_list_item("Immanentize eschaton")
+        first_list_url = self.browser.current_url
+
+        # She notices a "My lists" link, for the first time.
+        self.browser.find_element(By.LINK_TEXT, "My lists").click()
+
+        # She sees that her list is in there, named according to its
+        # first list item
+        self.wait_for(
+            lambda: self.browser.find_element(By.LINK_TEXT, "Reticulate splines")
+        )
+        self.browser.find_element(By.LINK_TEXT, "Reticulate splines").click()
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.current_url, first_list_url)
+        )
