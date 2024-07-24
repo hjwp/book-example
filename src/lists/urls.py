8576b9425d717513f721cddd8cc20226da54bbd9
Harry <hjwp2@cantab.net>
2024-07-24 10:56:16 +0200

    URL for my lists. --ch24l008--

--- src/lists/urls.py
+++ src/lists/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
 from lists import views
 
 urlpatterns = [
     path("new", views.new_list, name="new_list"),
     path("<int:list_id>/", views.view_list, name="view_list"),
+    path("users/<str:email>/", views.my_lists, name="my_lists"),
 ]
