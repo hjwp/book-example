8bce77fd181e358818c51e0b7550661b84ae2da4
Harry <hjwp2@cantab.net>
2024-07-24 10:55:58 +0200

    Flesh out ft to try a second list. --ch22l002--

--- src/functional_tests/test_my_lists.py
+++ src/functional_tests/test_my_lists.py
@@ -46,3 +46,25 @@ class MyListsTest(FunctionalTest):
         self.wait_for(
             lambda: self.assertEqual(self.browser.current_url, first_list_url)
         )
+
+        # She decides to start another list, just to see
+        self.browser.get(self.live_server_url)
+        self.add_list_item("Click cows")
+        second_list_url = self.browser.current_url
+
+        # Under "my lists", her new list appears
+        self.browser.find_element(By.LINK_TEXT, "My lists").click()
+        self.wait_for(lambda: self.browser.find_element(By.LINK_TEXT, "Click cows"))
+        self.browser.find_element(By.LINK_TEXT, "Click cows").click()
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.current_url, second_list_url)
+        )
+
+        # She logs out.  The "My lists" option disappears
+        self.browser.find_element(By.LINK_TEXT, "Log out").click()
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_elements(By.LINK_TEXT, "My lists"),
+                [],
+            )
+        )
