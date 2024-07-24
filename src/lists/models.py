e5592655eb0d27b4b6fdbd347b711a7f0a7592ed
Harry <hjwp2@cantab.net>
2024-07-24 10:56:18 +0200

    optional owner field on list model. --ch24l021--

--- src/lists/models.py
+++ src/lists/models.py
@@ -1,8 +1,13 @@
+from django.conf import settings
 from django.db import models
 from django.urls import reverse
 
 
 class List(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.CASCADE
+    )
+
     def get_absolute_url(self):
         return reverse("view_list", args=[self.id])
 
