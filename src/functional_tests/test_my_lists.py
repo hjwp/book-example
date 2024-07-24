03b1adb6e8aac41e5ea9d9a5d5c288c652cde7b9
Harry <hjwp2@cantab.net>
2024-07-24 10:55:58 +0200

    Start on first real version of my lists FT. --ch19l001-1--

--- src/functional_tests/test_my_lists.py
+++ src/functional_tests/test_my_lists.py
@@ -26,11 +26,24 @@ class MyListsTest(FunctionalTest):
         )
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
-        email = "edith@example.com"
-        self.browser.get(self.live_server_url)
-        self.wait_to_be_logged_out(email)
-
         # Edith is a logged-in user
-        self.create_pre_authenticated_session(email)
+        self.create_pre_authenticated_session("edith@example.com")
+
+        # She goes to the home page and starts a list
         self.browser.get(self.live_server_url)
-        self.wait_to_be_logged_in(email)
+        self.add_list_item("Reticulate splines")
+        self.add_list_item("Immanentize eschaton")
+        first_list_url = self.browser.current_url
+
+        # She notices a "My lists" link, for the first time.
+        self.browser.find_element_by_link_text("My lists").click()
+
+        # She sees that her list is in there, named according to its
+        # first list item
+        self.wait_for(
+            lambda: self.browser.find_element_by_link_text("Reticulate splines")
+        )
+        self.browser.find_element_by_link_text("Reticulate splines").click()
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.current_url, first_list_url)
+        )
