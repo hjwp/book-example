1020d064ed6b7a9c1000bf66a5dee421c6853bb8
Harry <hjwp2@cantab.net>
2024-07-24 10:56:17 +0200

    test passes owner to my_lists template. --ch24l011--

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -2,6 +2,7 @@ import lxml.html
 from django.test import TestCase
 from django.utils import html
 
+from accounts.models import User
 from lists.forms import (
     DUPLICATE_ITEM_ERROR,
     EMPTY_ITEM_ERROR,
@@ -150,3 +151,9 @@ class MyListsTest(TestCase):
     def test_my_lists_url_renders_my_lists_template(self):
         response = self.client.get("/lists/users/a@b.com/")
         self.assertTemplateUsed(response, "my_lists.html")
+
+    def test_passes_correct_owner_to_template(self):
+        User.objects.create(email="wrong@owner.com")
+        correct_user = User.objects.create(email="a@b.com")
+        response = self.client.get("/lists/users/a@b.com/")
+        self.assertEqual(response.context["owner"], correct_user)
