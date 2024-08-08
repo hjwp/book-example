93a0aa6b82e9d985b4c571704c11376ee5ee89d5
Harry <hjwp2@cantab.net>
2024-08-08 01:41:07 +0100

    String repr test. --ch15l021--

--- src/lists/tests/test_models.py
+++ src/lists/tests/test_models.py
@@ -46,6 +46,10 @@ class ItemModelTest(TestCase):
             [item1, item2, item3],
         )
 
+    def test_string_representation(self):
+        item = Item(text="some text")
+        self.assertEqual(str(item), "some text")
+
 
 class ListModelTest(TestCase):
     def test_get_absolute_url(self):
