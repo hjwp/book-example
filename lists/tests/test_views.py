0e123bf9e45c7b02ca9ae6ab68144921458ab7cb
Harry <hjwp2@cantab.net>
2024-08-14 17:58:55 +0100

    switch out integrated test to use new view. --ch20l033--

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -57,7 +57,6 @@ class NewListViewIntegratedTest(TestCase):
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
-    @unittest.skip
     def test_list_owner_is_saved_if_user_is_authenticated(self):
         user = User.objects.create(email="a@b.com")
         self.client.force_login(user)
