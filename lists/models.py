303334e23e7a9f5a8e0d282fdb7327040ee71159
Harry <hjwp2@cantab.net>
2024-08-14 17:58:55 +0100

    return newly created list in List.create_new --ch20l039-3--

--- lists/models.py
+++ lists/models.py
@@ -13,6 +13,7 @@ class List(models.Model):
     def create_new(first_item_text, owner=None):
         list_ = List.objects.create(owner=owner)
         Item.objects.create(text=first_item_text, list=list_)
+        return list_
 
 
 class Item(models.Model):
