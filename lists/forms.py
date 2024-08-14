955c1ec13cd7c0a52ce9b9acc3301de5165650c0
Harry <hjwp2@cantab.net>
2024-08-14 17:58:53 +0100

    add import into forms. --ch20l023--

--- lists/forms.py
+++ lists/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from lists.models import Item
+from lists.models import Item, List
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 DUPLICATE_ITEM_ERROR = "You've already got this in your list"
