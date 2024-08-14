a9b7d2b45744cb527b8dc8e5bb3bc8ff4e58edc0
Harry <hjwp2@cantab.net>
2024-08-14 17:58:53 +0100

    First isolated test for form save. --ch20l021--

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,3 +1,5 @@
+import unittest
+from unittest.mock import patch, Mock
 from django.test import TestCase
 
 from lists.forms import (
@@ -5,6 +7,7 @@ from lists.forms import (
     EMPTY_ITEM_ERROR,
     ExistingListItemForm,
     ItemForm,
+    NewListForm,
 )
 from lists.models import Item, List
 
@@ -27,6 +30,18 @@ class ItemFormTest(TestCase):
         self.assertEqual(new_item, Item.objects.first())
         self.assertEqual(new_item.text, "do me")
         self.assertEqual(new_item.list, list_)
+
+
+class NewListFormTest(unittest.TestCase):
+    @patch("lists.forms.List.create_new")
+    def test_save_creates_new_list_from_post_data_if_user_not_authenticated(
+        self, mock_List_create_new
+    ):
+        user = Mock(is_authenticated=False)
+        form = NewListForm(data={"text": "new item text"})
+        form.is_valid()
+        form.save(owner=user)
+        mock_List_create_new.assert_called_once_with(first_item_text="new item text")
 
 
 class ExistingListItemFormTest(TestCase):
