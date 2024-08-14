3e0d4fd9b0b144163d02bad8127b5e0159a545c5
Harry <hjwp2@cantab.net>
2024-08-14 17:58:53 +0100

    second isolated test for forms authenticated user case. --ch20l022--

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -43,6 +43,18 @@ class NewListFormTest(unittest.TestCase):
         form.save(owner=user)
         mock_List_create_new.assert_called_once_with(first_item_text="new item text")
 
+    @patch("lists.forms.List.create_new")
+    def test_save_creates_new_list_with_owner_if_user_authenticated(
+        self, mock_List_create_new
+    ):
+        user = Mock(is_authenticated=True)
+        form = NewListForm(data={"text": "new item text"})
+        form.is_valid()
+        form.save(owner=user)
+        mock_List_create_new.assert_called_once_with(
+            first_item_text="new item text", owner=user
+        )
+
 
 class ExistingListItemFormTest(TestCase):
     def test_form_renders_item_text_input(self):
