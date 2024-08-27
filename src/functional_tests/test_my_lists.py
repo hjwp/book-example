356c413b5e917df1fb8cceceded0f20fa978bd88
Harry <hjwp2@cantab.net>
2024-08-27 05:11:13 +0100

    Move things around to make create-pre-auth available on base. --ch24l002--

--- src/functional_tests/test_my_lists.py
+++ src/functional_tests/test_my_lists.py
@@ -1,29 +1,9 @@
-from django.conf import settings
 from selenium.webdriver.common.by import By
 
 from .base import FunctionalTest
-from .management.commands.create_session import create_pre_authenticated_session
-from .server_tools import create_session_on_server
 
 
 class MyListsTest(FunctionalTest):
-    def create_pre_authenticated_session(self, email):
-        if self.test_server:
-            session_key = create_session_on_server(self.test_server, email)
-        else:
-            session_key = create_pre_authenticated_session(email)
-
-        ## to set a cookie we need to first visit the domain.
-        ## 404 pages load the quickest!
-        self.browser.get(self.live_server_url + "/404_no_such_url/")
-        self.browser.add_cookie(
-            dict(
-                name=settings.SESSION_COOKIE_NAME,
-                value=session_key,
-                path="/",
-            )
-        )
-
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
         # Edith is a logged-in user
         self.create_pre_authenticated_session("edith@example.com")
