998e9616b6d699367a1624b2a24d720a295e4962
Harry <hjwp2@cantab.net>
2024-08-27 05:26:18 +0100

    MyLists page object. --ch24l011--

---
+++ src/functional_tests/my_lists_page.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+
+
+class MyListsPage:
+    def __init__(self, test):
+        self.test = test
+
+    def go_to_my_lists_page(self):
+        self.test.browser.get(self.test.live_server_url)
+        self.test.browser.find_element(By.LINK_TEXT, "My lists").click()
+        self.test.wait_for(
+            lambda: self.test.assertEqual(
+                self.test.browser.find_element(By.TAG_NAME, "h1").text,
+                "My Lists",
+            )
+        )
+        return self
