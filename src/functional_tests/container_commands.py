d685a620ab9eb2c2af205b9749660af9d8b7231a
Harry <hjwp2@cantab.net>
2025-01-22 15:43:35 +0000

    First cut of container commands. --ch23l018--

---
+++ src/functional_tests/container_commands.py
@@ -0,0 +1,48 @@
+import subprocess
+
+USER = "elspeth"
+
+
+def create_session_on_server(host, email):
+    return _exec_in_container(
+        host, ["/venv/bin/python", "/src/manage.py", "create_session", email]
+    )
+
+
+def _exec_in_container(host, commands):
+    if "localhost" in host:
+        return _exec_in_container_locally(commands)
+    else:
+        return _exec_in_container_on_server(host, commands)
+
+
+def _exec_in_container_locally(commands):
+    print(f"Running {commands} on inside local docker container")
+    return _run_commands(["docker", "exec", _get_container_id()] + commands)
+
+
+def _exec_in_container_on_server(host, commands):
+    print(f"Running {commands!r} on {host} inside docker container")
+    return _run_commands(
+        ["ssh", f"{USER}@{host}", "docker", "exec", "superlists"] + commands
+    )
+
+
+def _get_container_id():
+    return subprocess.check_output(
+        ["docker", "ps", "-q", "--filter", "ancestor=superlists"]
+    ).strip()
+
+
+def _run_commands(commands):
+    process = subprocess.run(
+        commands,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        check=False,
+    )
+    result = process.stdout.decode()
+    if process.returncode != 0:
+        raise Exception(result)
+    print(f"Result: {result!r}")
+    return result.strip()
