4e9c9014eec40cc132b8e65b11b70fec37f37862
Harry <hjwp2@cantab.net>
2025-01-29 20:21:54 +0000

    use helper fn in 4 other fts. --ch24l004-1--

--- src/functional_tests/test_simple_list_creation.py
+++ src/functional_tests/test_simple_list_creation.py
@@ -1,6 +1,7 @@
-from .base import FunctionalTest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
+from .base import FunctionalTest
 
 
 class NewVisitorTest(FunctionalTest):
@@ -30,9 +31,7 @@ class NewVisitorTest(FunctionalTest):
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
         # (Edith is very methodical)
-        inputbox = self.get_item_input_box()
-        inputbox.send_keys("Use peacock feathers to make a fly")
-        inputbox.send_keys(Keys.ENTER)
+        self.add_list_item("Use peacock feathers to make a fly")
 
         # The page updates again, and now shows both items on her list
         self.wait_for_row_in_list_table("1: Buy peacock feathers")
@@ -43,10 +42,7 @@ class NewVisitorTest(FunctionalTest):
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
         self.browser.get(self.live_server_url)
-        inputbox = self.get_item_input_box()
-        inputbox.send_keys("Buy peacock feathers")
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        self.add_list_item("Buy peacock feathers")
 
         # She notices that her list has a unique URL
         edith_list_url = self.browser.current_url
@@ -67,10 +63,7 @@ class NewVisitorTest(FunctionalTest):
 
         # Francis starts a new list by entering a new item. He
         # is less interesting than Edith...
-        inputbox = self.get_item_input_box()
-        inputbox.send_keys("Buy milk")
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table("1: Buy milk")
+        self.add_list_item("Buy milk")
 
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
