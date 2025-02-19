0fa0a3295af9a2f015ba94e4d6db4944703282c4
Harry <hjwp2@cantab.net>
2025-02-19 15:20:39 +0000

    enable debug logging in our debug-ci script. --ch25l005--

--- infra/debug-ci.py
+++ infra/debug-ci.py
@@ -1,4 +1,13 @@
+import subprocess
+
 from selenium import webdriver
 
+options = webdriver.FirefoxOptions()
+options.log.level = "trace"
+
+service = webdriver.FirefoxService(
+    log_output=subprocess.STDOUT, service_args=["--log", "trace"]
+)
+
 # just try to open a selenium session
-webdriver.Firefox().quit()
+webdriver.Firefox(options=options, service=service).quit()
