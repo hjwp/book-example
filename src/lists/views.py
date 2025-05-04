46cb2f52921893e4d1bad02b9a3b5ec625368d6d
Harry <hjwp2@cantab.net>
2025-05-04 18:34:21 +0200

    Last rename in views.py. --ch14l018--

--- src/lists/views.py
+++ src/lists/views.py
@@ -28,7 +28,7 @@ def view_list(request, list_id):
 
     if request.method == "POST":
         try:
-            item = Item(text=request.POST["item_text"], list=our_list)
+            item = Item(text=request.POST["text"], list=our_list)
             item.full_clean()
             item.save()
             return redirect(our_list)
