632890dda5e44c11f49fda3893139cdcf46dfaba
Harry <hjwp2@cantab.net>
2025-05-04 18:34:05 +0200

    First rename item_text -> text in views.py. --ch14l017--

--- src/lists/views.py
+++ src/lists/views.py
@@ -11,7 +11,7 @@ def home_page(request):
 
 def new_list(request):
     nulist = List.objects.create()
-    item = Item(text=request.POST["item_text"], list=nulist)
+    item = Item(text=request.POST["text"], list=nulist)
     try:
         item.full_clean()
         item.save()
