69f0fb74878013dc5e9866cc5b5f24bb8a1bab5d
Harry <hjwp2@cantab.net>
2025-05-07 17:34:23 +0100

    Restore widget config. --ch15l007--

--- src/lists/forms.py
+++ src/lists/forms.py
@@ -7,12 +7,11 @@ class ItemForm(forms.models.ModelForm):
     class Meta:
         model = Item
         fields = ("text",)
-
-    # item_text = forms.CharField(
-    #     widget=forms.widgets.TextInput(
-    #         attrs={
-    #             "placeholder": "Enter a to-do item",
-    #             "class": "form-control form-control-lg",
-    #         }
-    #     ),
-    # )
+        widgets = {
+            "text": forms.widgets.TextInput(
+                attrs={
+                    "placeholder": "Enter a to-do item",
+                    "class": "form-control form-control-lg",
+                }
+            ),
+        }
