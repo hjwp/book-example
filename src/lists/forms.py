38b11e316c614098175ab2ed88f7324c2cc08f59
Harry <hjwp2@cantab.net>
2025-05-07 17:31:22 +0100

    Switch to modelform, comment out custom widget. --ch15l006--

--- src/lists/forms.py
+++ src/lists/forms.py
@@ -1,12 +1,18 @@
 from django import forms
 
+from lists.models import Item
 
-class ItemForm(forms.Form):
-    item_text = forms.CharField(
-        widget=forms.widgets.TextInput(
-            attrs={
-                "placeholder": "Enter a to-do item",
-                "class": "form-control form-control-lg",
-            }
-        ),
-    )
+
+class ItemForm(forms.models.ModelForm):
+    class Meta:
+        model = Item
+        fields = ("text",)
+
+    # item_text = forms.CharField(
+    #     widget=forms.widgets.TextInput(
+    #         attrs={
+    #             "placeholder": "Enter a to-do item",
+    #             "class": "form-control form-control-lg",
+    #         }
+    #     ),
+    # )
