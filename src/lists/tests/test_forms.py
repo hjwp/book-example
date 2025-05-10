d468bb25e094db4bfc03ffc415b6ef2f2a0b00d2
Harry <hjwp2@cantab.net>
2025-05-10 22:43:40 +0100

    fixup an is_valid in ItemFormTest

--- src/lists/tests/test_forms.py
+++ src/lists/tests/test_forms.py
@@ -18,6 +18,7 @@ class ItemFormTest(TestCase):
     def test_form_save_handles_saving_to_a_list(self):
         mylist = List.objects.create()
         form = ItemForm(data={"text": "do me"})
+        self.assertTrue(form.is_valid())
         new_item = form.save(for_list=mylist)
         self.assertEqual(new_item, Item.objects.get())
         self.assertEqual(new_item.text, "do me")
