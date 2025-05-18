85bd3277a4af24616564b06fb43c41065289f912
Harry <hjwp2@cantab.net>
2025-05-18 17:08:45 +0100

    Test for POST request. --ch05l008--

--- lists/tests.py
+++ lists/tests.py
@@ -10,3 +10,7 @@ class HomePageTest(TestCase):
         response = self.client.get("/")
         self.assertContains(response, '<form method="POST">')
         self.assertContains(response, '<input name="item_text"')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertContains(response, "A new list item")
