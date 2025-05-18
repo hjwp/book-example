e37f84b2bdd3019ffcff839657bb47106d8a53ec
Harry <hjwp2@cantab.net>
2025-05-18 08:57:15 +0100

    Add extra smoke test for homepage content --ch04l008--

--- lists/tests.py
+++ lists/tests.py
@@ -5,3 +5,7 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
+
+    def test_renders_homepage_content(self):
+        response = self.client.get("/")
+        self.assertContains(response, "To-Do")
