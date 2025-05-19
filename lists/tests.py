176a072a3ae91da4ce8fdd13132020aa3a78089e
Harry <hjwp2@cantab.net>
2025-05-19 20:31:00 +0100

    Adjust html unit tests to use html=True. --ch08l011-1--

--- lists/tests.py
+++ lists/tests.py
@@ -11,7 +11,11 @@ class HomePageTest(TestCase):
     def test_renders_input_form(self):
         response = self.client.get("/")
         self.assertContains(response, '<form method="POST" action="/lists/new">')
-        self.assertContains(response, '<input name="item_text"')
+        self.assertContains(
+            response,
+            '<input name="item_text" id="id_new_item" placeholder="Enter a to-do item" />',
+            html=True,
+        )
 
 
 class NewListTest(TestCase):
@@ -40,7 +44,11 @@ class ListViewTest(TestCase):
             response,
             f'<form method="POST" action="/lists/{mylist.id}/add_item">',
         )
-        self.assertContains(response, '<input name="item_text"')
+        self.assertContains(
+            response,
+            '<input name="item_text" id="id_new_item" placeholder="Enter a to-do item" />',
+            html=True,
+        )
 
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
