2e8d79c61621becadbfa6bbc72dd62b831238fb4
Harry <hjwp2@cantab.net>
2025-05-20 16:25:43 +0100

    Restore use of form in home_page() view. --ch15l025--

--- src/lists/views.py
+++ src/lists/views.py
@@ -6,8 +6,7 @@ from lists.models import Item, List
 
 
 def home_page(request):
-    # return render(request, "home.html", {"form": ItemForm()})
-    return render(request, "home.html")
+    return render(request, "home.html", {"form": ItemForm()})
 
 
 def new_list(request):
