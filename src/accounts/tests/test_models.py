dad9297e7334b77975f893bf236a0684427ed755
Harry <hjwp2@cantab.net>
2025-05-30 01:15:30 +0100

    Test our model is valid django default auth model --ch19l026-1--

--- src/accounts/tests/test_models.py
+++ src/accounts/tests/test_models.py
@@ -1,9 +1,13 @@
+from django.contrib import auth
 from django.test import TestCase
 
 from accounts.models import User
 
 
 class UserModelTest(TestCase):
+    def test_model_is_configured_for_django_auth(self):
+        self.assertEqual(auth.get_user_model(), User)
+
     def test_user_is_valid_with_email_only(self):
         user = User(email="a@b.com")
         user.full_clean()  # should not raise
