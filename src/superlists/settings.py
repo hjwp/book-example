88b03ebb419d18f823ab36c6a4b7cff2f617499c
Harry <hjwp2@cantab.net>
2025-05-30 01:19:45 +0100

    Add AUTH_USER_MODEL to settings.py. --ch19l026-2--

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     "lists",
 ]
 
+AUTH_USER_MODEL = "accounts.User"
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
