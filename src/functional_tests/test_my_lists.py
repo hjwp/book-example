614235ed973625942700e460a653d2bab0f320b7
Harry <hjwp2@cantab.net>
2025-06-26 22:33:37 +0100

    Revert "add a deliberate test failure to check screenshotting." --ch25l010-1--
    This reverts commit 9d216e8991433e8dd6eef78c6688753f11d1fbe7.

--- src/functional_tests/test_my_lists.py
+++ src/functional_tests/test_my_lists.py
@@ -34,8 +34,6 @@ class MyListsTest(FunctionalTest):
         self.add_list_item("Immanentize eschaton")
         first_list_url = self.browser.current_url
 
-        self.fail('testey')
-
         # She notices a "My lists" link, for the first time.
         self.browser.find_element(By.LINK_TEXT, "My lists").click()
 
